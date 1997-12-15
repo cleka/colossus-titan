44c98fb2110d564a8c620fcfe1fc8b26083d39bc
David Ripton <dripton@ripton.net>
1997-12-15 23:35:02 +0000

    Changes made Monday 12-15.    Changed handle_event to use various mouse_x events instead.    Changed mouse_move to mouse_drag and got Chit dragging working.    Added a bunch of repaint() calls so that selections appear       without having to force repaints externally.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@3 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/BattleMap.java
+++ Colossus/BattleMap.java
@@ -4,7 +4,7 @@
 import java.awt.*;
 
 
-class Hex implements Shape
+class Hex extends Canvas implements Shape
 {
     protected boolean selected;
     private int[] x_vertex = new int[6];
@@ -20,23 +20,29 @@ class Hex implements Shape
         x_vertex[1] = cx + 2 * scale;
         y_vertex[1] = cy;
         x_vertex[2] = cx + 3 * scale;
-        y_vertex[2] = cy + (int)java.lang.Math.round(java.lang.Math.sqrt(3.0) 
+        y_vertex[2] = cy + (int)java.lang.Math.round(java.lang.Math.sqrt(3.0)
                        * scale);
         x_vertex[3] = cx + 2 * scale;
-        y_vertex[3] = cy + (int)java.lang.Math.round(2 * java.lang.Math.sqrt(3.0) 
+        y_vertex[3] = cy + (int)java.lang.Math.round(2 * java.lang.Math.sqrt(3.0)
                        * scale);
         x_vertex[4] = cx;
-        y_vertex[4] = cy + (int)java.lang.Math.round(2 * java.lang.Math.sqrt(3.0) 
+        y_vertex[4] = cy + (int)java.lang.Math.round(2 * java.lang.Math.sqrt(3.0)
                        * scale);
         x_vertex[5] = cx - 1 * scale;
-        y_vertex[5] = cy + (int)java.lang.Math.round(java.lang.Math.sqrt(3.0) 
+        y_vertex[5] = cy + (int)java.lang.Math.round(java.lang.Math.sqrt(3.0)
                        * scale);
-        
+
         p = new Polygon(x_vertex, y_vertex, 6);
     }
 
 
     public void paint(Graphics g)
+    {
+        update(g);
+    }
+    
+
+    public void update(Graphics g)
     {
         System.out.println("painting a Hex");
         if (selected == true)
@@ -47,6 +53,8 @@ class Hex implements Shape
         }
         else
         {
+            g.setColor(java.awt.Color.white);
+	    g.fillPolygon(p);
             g.setColor(java.awt.Color.black);
             g.drawPolygon(p);
         }
@@ -59,6 +67,7 @@ class Hex implements Shape
         if (getBounds().contains(point))
         {
             selected = !selected;
+            repaint();
         }
     }
 
@@ -77,7 +86,7 @@ class Hex implements Shape
 
 
 
-class Chit implements Shape
+class Chit extends Canvas implements Shape, java.awt.image.ImageObserver
 {
     protected boolean selected;
     private Rectangle rect;
@@ -87,13 +96,19 @@ class Chit implements Shape
     {
         selected = false;
         rect = new Rectangle(cx, cy, scale, scale);
-        Toolkit.getDefaultToolkit().getImage(image_filename);
+        image = Toolkit.getDefaultToolkit().getImage(image_filename);
     }
 
 
-    void paint(Graphics g)
-    {    
-          //g.drawImage(image, 0, 0, this);
+    public void paint(Graphics g)
+    {
+        update(g);
+    }
+
+    public void update(Graphics g)
+    {
+//          g.drawImage(image, 0, 0, this);
+
           if (selected)
           {
               g.setColor(java.awt.Color.yellow);
@@ -103,9 +118,10 @@ class Chit implements Shape
               g.setColor(java.awt.Color.blue);
           }
           g.fillRect(rect.x,rect.y,rect.width,rect.height);
+
     }
-    
-    
+
+
     boolean select(Point point)
     {
         if (rect.contains(point))
@@ -116,23 +132,30 @@ class Chit implements Shape
         {
             selected = false;
         }
-        
+
+        repaint();
+
         return selected;
     }
 
 
     void move(Point point)
     {
-        Rectangle tmprect = rect;
         rect.setLocation(point);
-        tmprect = tmprect.union(rect);
-        // set invalid rectangle
+        repaint();
     }
-    
 
+    // Required by interface Shape
     public Rectangle getBounds()
     {
         return (Rectangle) rect;
+    }
+
+    // Required by interface ImageObserver
+    public boolean imageUpdate(Image img, int infoflags, int x,
+        int y, int width, int height)
+    {
+        return false;
     }
 
 }
@@ -157,7 +180,7 @@ class HexMap extends Canvas
     HexMap(int cx, int cy, int scale)
     {
         System.out.println("Creating a HexMap.");
-    
+
         for (int i = 0; i < 6; i++)
         {
             for (int j = 0; j < 6; j++)
@@ -166,26 +189,31 @@ class HexMap extends Canvas
                 {
                     h[i][j] = new Hex
                         ((int)java.lang.Math.round(cx + 3 * i * scale),
-                        (int)java.lang.Math.round(cy + (2 * j + i % 2) * 
+                        (int)java.lang.Math.round(cy + (2 * j + i % 2) *
                         java.lang.Math.sqrt(3.0) * scale), scale);
                 }
             }
         }
-    
-        chits[0] = new Chit(100, 100, 60, "ogre.gif");
-        chits[1] = new Chit(150, 150, 60, "lion.gif");
-        chits[2] = new Chit(200, 200, 60, "angel.gif");
-        chits[3] = new Chit(250, 250, 60, "gargoyle.gif");
-        chits[4] = new Chit(300, 300, 60, "behemoth.gif");
-        chits[5] = new Chit(350, 350, 60, "serpent.gif");
-        chits[6] = new Chit(400, 400, 60, "troll.gif");
-        chits[7] = new Chit(450, 450, 60, "giant.gif");
+
+        chits[0] = new Chit(100, 100, 60, "Ogre.gif");
+        chits[1] = new Chit(150, 150, 60, "Lion.gif");
+        chits[2] = new Chit(200, 200, 60, "Angel.gif");
+        chits[3] = new Chit(250, 250, 60, "Gargoyle.gif");
+        chits[4] = new Chit(300, 300, 60, "Behemoth.gif");
+        chits[5] = new Chit(350, 350, 60, "Serpent.gif");
+        chits[6] = new Chit(400, 400, 60, "Troll.gif");
+        chits[7] = new Chit(450, 450, 60, "Giant.gif");
 
         tracking = -1;
     }
-    
-    
+
+
     public void paint(Graphics g)
+    {
+        update(g);
+    }
+
+    public void update(Graphics g)
     {
         for (int i = 0; i < 6; i++)
         {
@@ -196,29 +224,27 @@ class HexMap extends Canvas
                     System.out.println("drawing h[" + i + "][" + j + "]");
                     h[i][j].paint(g);
                 }
-            }        
+            }
         }
 
         // Draw chits from low end of array (top of Z-order), de-
-        // invalidating as we draw each chit so that element 0 is 
-        // on top even though it's drawn first.  This should 
+        // invalidating as we draw each chit so that element 0 is
+        // on top even though it's drawn first.  This should
         // eliminate flicker caused by back objects being briefly
         // drawn then overlaid by front objects.
-    
-        for (int i=0; i < chits.length; i++)
+
+        for (int i = 0; i < chits.length; i++)
         {
-            Rectangle rectChit = new Rectangle(chits[i].getBounds());
-            
             System.out.println("Drawing chits[" + i + "]");
             chits[i].paint(g);
-                
-            // Now remove the area just painted by the chit from
-            // what's left of the invalid region.
-        }    
+            // Rectangle rectClip = new Rectangle(g.getClipBounds());
+            // rectClip.add(chits[i].getBounds());
+            // g.setClip(rectClip);
+        }
     }
 
-    
-    
+
+
     void select(Point point)
     {
         Rectangle rect;
@@ -243,34 +269,14 @@ class HexMap extends Canvas
     {
         return new Dimension(200, 200);
     }
-    
+
     public Dimension preferredSize()
     {
         return new Dimension(700,700);
     }
- 
-    public boolean handleEvent(Event event)
-    {
-        //System.out.println("HexMap got event " + event.toString());
 
-        if (event.id == event.MOUSE_DOWN)
-        {
-            mouseDown(event.x,event.y);
-        }
-        else if (event.id == event.MOUSE_UP)
-        {
-            mouseUp();
-        }
-        else if (event.id == event.MOUSE_MOVE)
-        {
-            mouseMove(event.x,event.y);
-        }
 
-        return super.handleEvent(event);
-    }
-
-  
-    void mouseDown(int x, int y)
+    public boolean mouseDown(Event event, int x, int y)
     {
         Point point = new Point(x,y);
         for (int i=0; i < chits.length; i++)
@@ -280,7 +286,7 @@ class HexMap extends Canvas
                 System.out.println("Selected chits[" + i +"]");
                 // Move selected chit to top of Z-order
                 tracking = 0;
-    
+
                 // Don't swap if it's already on top.
                 if (i != 0)
                 {
@@ -290,34 +296,39 @@ class HexMap extends Canvas
 		        chits[j] = chits[j - 1];
                     }
                     chits[0] = tmpchit;
+                    repaint();
                 }
-    
-                return;
+
+                return false;
             }
         }
-    
+
         // No hits on chits, so check map.
         select(point);
+        repaint();
+        return false;
     }
-    
-    
-    void mouseUp()
+
+
+    public boolean mouseUp(Event event, int x, int y)
     {
         tracking = -1;
+        return false;
     }
-    
-    
-    void mouseMove(int x, int y)
+
+
+    public boolean mouseDrag(Event event, int x, int y)
     {
         Point point = new Point(x,y);
-        if (tracking != -1) 
+        if (tracking != -1)
         {
             chits[tracking].move(point);
+            repaint();
         }
+        return false;
     }
-    
+
 }
-    
 
 
 public class BattleMap extends Frame
@@ -327,12 +338,12 @@ public class BattleMap extends Frame
     public BattleMap()
     {
         super("BattleMap");
-    
+
         map = new HexMap(80, 20, 25);
         add("Center", map);
         validate();
     }
-    
+
     public boolean handleEvent(Event event)
     {
         //System.out.println("BattleMap got event " + event.toString());
@@ -342,11 +353,16 @@ public class BattleMap extends Frame
         }
         return super.handleEvent(event);
     }
-     
+
     public void paint(Graphics g)
     {
-        map.paint(g);
-    }        
+        update(g);
+    }
+
+    public void update(Graphics g)
+    {
+        //map.update(g);
+    }
 
     public static void main(String args[])
     {
