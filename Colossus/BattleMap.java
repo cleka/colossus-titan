4d4dd208b14f37e9f0558d194fe53b9a743a0f7a
David Ripton <dripton@ripton.net>
1997-12-16 22:50:08 +0000

    Changes of 12/16/97:    Added many more chits.    Got chit display working.    Currently only HexMap is hooked into the event scheme,       so the only repaints that work are the ones started       from within HexMap.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/BattleMap.java
+++ Colossus/BattleMap.java
@@ -1,5 +1,4 @@
-/** Attempted port of hexmap from MFC to Java
-    dripton  12/10/97 */
+/** Attempted port of hexmap from MFC to Java dripton  12/10/97 */
 
 import java.awt.*;
 
@@ -10,6 +9,7 @@ class Hex extends Canvas implements Shape
     private int[] x_vertex = new int[6];
     private int[] y_vertex = new int[6];
     private Polygon p;
+    private Rectangle rectBound;
 
     Hex(int cx, int cy, int scale)
     {
@@ -33,6 +33,8 @@ class Hex extends Canvas implements Shape
                        * scale);
 
         p = new Polygon(x_vertex, y_vertex, 6);
+        rectBound = new Rectangle(x_vertex[5], y_vertex[0], x_vertex[2] - 
+            x_vertex[5], y_vertex[3] - y_vertex[0]);
     }
 
 
@@ -45,10 +47,11 @@ class Hex extends Canvas implements Shape
     public void update(Graphics g)
     {
         System.out.println("painting a Hex");
-        if (selected == true)
+        if (selected)
         {
             g.setColor(java.awt.Color.red);
             g.fillPolygon(p);
+            g.setColor(java.awt.Color.black);
             g.drawPolygon(p);
         }
         else
@@ -61,27 +64,39 @@ class Hex extends Canvas implements Shape
     }
 
 
-    void select(Point point)
+    boolean select(Point point)
     {
-        // This only checks the bounding rectangle; need to fix.
-        if (getBounds().contains(point))
+        if (p.contains(point))
         {
             selected = !selected;
             repaint();
+	    return true;
         }
+	return false;
     }
 
 
     public Rectangle getBounds()
     {
-        Rectangle rect = new Rectangle();
-        rect.x = x_vertex[5];
-        rect.y = y_vertex[0];
-        rect.width  = x_vertex[2] - rect.x;
-        rect.height = y_vertex[3] - rect.y;
-        return rect;
+        return rectBound;
     }
 
+    public boolean contains(Point point)
+    {
+        return (p.contains(point));
+    }
+    
+    public Dimension minimumsize()
+    {
+        return new Dimension(x_vertex[2] - x_vertex[5], y_vertex[3] 
+	    - y_vertex[0]);
+    }
+
+    public Dimension preferredSize()
+    {
+        return new Dimension(x_vertex[2] - x_vertex[5], y_vertex[3] 
+	    - y_vertex[0]);
+    }
 }
 
 
@@ -107,18 +122,10 @@ class Chit extends Canvas implements Shape, java.awt.image.ImageObserver
 
     public void update(Graphics g)
     {
-//          g.drawImage(image, 0, 0, this);
-
-          if (selected)
-          {
-              g.setColor(java.awt.Color.yellow);
-          }
-          else
-          {
-              g.setColor(java.awt.Color.blue);
-          }
-          g.fillRect(rect.x,rect.y,rect.width,rect.height);
-
+        if (g.drawImage(image, rect.x, rect.y, this) == false)
+        {
+            System.out.println("image started drawing but isn't done");
+        }
     }
 
 
@@ -127,22 +134,20 @@ class Chit extends Canvas implements Shape, java.awt.image.ImageObserver
         if (rect.contains(point))
         {
             selected = true;
+            repaint();
         }
         else
         {
             selected = false;
         }
-
-        repaint();
-
         return selected;
     }
 
 
     void move(Point point)
     {
+        // Repainting needs to happen at a higher level.
         rect.setLocation(point);
-        repaint();
     }
 
     // Required by interface Shape
@@ -155,7 +160,25 @@ class Chit extends Canvas implements Shape, java.awt.image.ImageObserver
     public boolean imageUpdate(Image img, int infoflags, int x,
         int y, int width, int height)
     {
-        return false;
+        // If image is done loading, forget about it.
+        if (infoflags >= 32)
+        {
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
+    }
+    
+    public Dimension minimumsize()
+    {
+        return new Dimension(rect.width, rect.height);
+    }
+
+    public Dimension preferredSize()
+    {
+        return new Dimension(rect.width, rect.height);
     }
 
 }
@@ -165,8 +188,9 @@ class Chit extends Canvas implements Shape, java.awt.image.ImageObserver
 class HexMap extends Canvas
 {
     private Hex[][] h = new Hex[6][6];
-    private Chit[] chits = new Chit[8];
+    private Chit[] chits = new Chit[22];
     private int tracking;
+    Rectangle rectClip = new Rectangle();
     final private boolean[][] show =
     {
         {false,false,true,true,true,false},
@@ -180,12 +204,13 @@ class HexMap extends Canvas
     HexMap(int cx, int cy, int scale)
     {
         System.out.println("Creating a HexMap.");
+	tracking = -1;
 
         for (int i = 0; i < 6; i++)
         {
             for (int j = 0; j < 6; j++)
             {
-                if (show[i][j] == true)
+                if (show[i][j])
                 {
                     h[i][j] = new Hex
                         ((int)java.lang.Math.round(cx + 3 * i * scale),
@@ -195,16 +220,28 @@ class HexMap extends Canvas
             }
         }
 
-        chits[0] = new Chit(100, 100, 60, "Ogre.gif");
-        chits[1] = new Chit(150, 150, 60, "Lion.gif");
-        chits[2] = new Chit(200, 200, 60, "Angel.gif");
-        chits[3] = new Chit(250, 250, 60, "Gargoyle.gif");
-        chits[4] = new Chit(300, 300, 60, "Behemoth.gif");
-        chits[5] = new Chit(350, 350, 60, "Serpent.gif");
-        chits[6] = new Chit(400, 400, 60, "Troll.gif");
-        chits[7] = new Chit(450, 450, 60, "Giant.gif");
-
-        tracking = -1;
+        chits[0] = new Chit(100, 100, 60, "Angel.gif");
+        chits[1] = new Chit(120, 120, 60, "Archange.gif");
+        chits[2] = new Chit(140, 140, 60, "Behemoth.gif");
+        chits[3] = new Chit(160, 160, 60, "Centaur.gif");
+        chits[4] = new Chit(180, 180, 60, "Colossus.gif");
+        chits[5] = new Chit(200, 200, 60, "Cyclops.gif");
+        chits[6] = new Chit(220, 220, 60, "Gargoyle.gif");
+        chits[7] = new Chit(240, 240, 60, "Giant.gif");
+        chits[8] = new Chit(260, 260, 60, "Gorgon.gif");
+        chits[9] = new Chit(280, 280, 60, "Griffon.gif");
+        chits[10] = new Chit(300, 300, 60, "Guardian.gif");
+        chits[11] = new Chit(320, 320, 60, "Hydra.gif");
+        chits[12] = new Chit(340, 340, 60, "Lion.gif");
+        chits[13] = new Chit(360, 360, 60, "Minotaur.gif");
+        chits[14] = new Chit(380, 380, 60, "Ogre.gif");
+        chits[15] = new Chit(400, 400, 60, "Ranger.gif");
+        chits[16] = new Chit(420, 420, 60, "Serpent.gif");
+        chits[17] = new Chit(460, 460, 60, "Titan.gif");
+        chits[18] = new Chit(480, 480, 60, "Troll.gif");
+        chits[19] = new Chit(500, 500, 60, "Unicorn.gif");
+        chits[20] = new Chit(520, 520, 60, "Warbear.gif");
+        chits[21] = new Chit(540, 540, 60, "Warlock.gif");
     }
 
 
@@ -215,55 +252,35 @@ class HexMap extends Canvas
 
     public void update(Graphics g)
     {
+        rectClip = g.getClipBounds();
+        System.out.println("rectClip: " + rectClip.x + " " + rectClip.y 
+	    + " " + rectClip.width + " " + rectClip.height);
+
         for (int i = 0; i < 6; i++)
         {
             for (int j = 0; j < 6; j++)
             {
-                if (show[i][j])   // need to do only if in invalid rectangle
+                if (show[i][j] && rectClip.intersects(h[i][j].getBounds()))
                 {
                     System.out.println("drawing h[" + i + "][" + j + "]");
-                    h[i][j].paint(g);
+                    h[i][j].update(g);
                 }
             }
         }
 
-        // Draw chits from low end of array (top of Z-order), de-
-        // invalidating as we draw each chit so that element 0 is
-        // on top even though it's drawn first.  This should
-        // eliminate flicker caused by back objects being briefly
-        // drawn then overlaid by front objects.
-
-        for (int i = 0; i < chits.length; i++)
+        // Draw chits from back to front.
+        for (int i = chits.length - 1; i >= 0; i--)
         {
-            System.out.println("Drawing chits[" + i + "]");
-            chits[i].paint(g);
-            // Rectangle rectClip = new Rectangle(g.getClipBounds());
-            // rectClip.add(chits[i].getBounds());
-            // g.setClip(rectClip);
-        }
-    }
-
-
-
-    void select(Point point)
-    {
-        Rectangle rect;
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j < 6; j++)
-            {
-                if (show[i][j])
-                {
-                    rect = h[i][j].getBounds();
-                    if (rect.contains(point))
-                    {
-                        System.out.println("Calling select for h[" + i + "][" + j +"]");
-                        h[i][j].select(point);
-                    }
-                }
+	    if (rectClip.intersects(chits[i].getBounds()))
+	    {
+                System.out.println("Drawing chits[" + i + "]");
+                chits[i].update(g);
             }
         }
     }
+
+
+
 
     public Dimension minimumsize()
     {
@@ -296,16 +313,31 @@ class HexMap extends Canvas
 		        chits[j] = chits[j - 1];
                     }
                     chits[0] = tmpchit;
-                    repaint();
+                    chits[0].repaint();
+		    // TODO: Make this work without a global repaint.
+		    repaint();
                 }
-
                 return false;
             }
         }
 
         // No hits on chits, so check map.
-        select(point);
-        repaint();
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                if (show[i][j] && h[i][j].contains(point))
+                {
+                    System.out.println("Calling select for h[" + i + "]["
+                        + j +"]");
+                    h[i][j].select(point);
+                    // TODO: Make this work without a global repaint.
+                    repaint();
+		    return false;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -322,6 +354,9 @@ class HexMap extends Canvas
         Point point = new Point(x,y);
         if (tracking != -1)
         {
+            // TODO: Set clip so that the chit's old 
+            // location gets erased, so we don't need
+            // a global repaint.
             chits[tracking].move(point);
             repaint();
         }
@@ -354,14 +389,14 @@ public class BattleMap extends Frame
         return super.handleEvent(event);
     }
 
+    public void update(Graphics g)
+    {
+        System.out.println("Called BattleMap.update()");
+    }
+    
     public void paint(Graphics g)
     {
         update(g);
-    }
-
-    public void update(Graphics g)
-    {
-        //map.update(g);
     }
 
     public static void main(String args[])
