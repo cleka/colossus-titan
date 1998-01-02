cf9941ac7a713a4e46493a089f116c8866b81816
David Ripton <dripton@ripton.net>
1998-01-02 14:24:03 +0000

    Added hex exits info. Began code to draw hex exits.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@16 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/MasterBoard.java
+++ Colossus/MasterBoard.java
@@ -99,212 +99,472 @@ public class MasterBoard extends Frame implements MouseListener,
         // Add terrain types, id labels, and exits to hexes
         h[0][3].terrain='S';
         h[0][3].label=132;
+        h[0][3].exitType[1]=4;
+         
         h[0][4].terrain='P';
         h[0][4].label=133;
+        h[0][4].exitType[0]=4;
 
         h[1][2].terrain='B';
         h[1][2].label=130;
+        h[1][2].exitType[1]=4;
+
         h[1][3].terrain='M';
         h[1][3].label=131;
+        h[1][3].exitType[0]=4;
+        h[1][3].exitType[2]=2;
+
         h[1][4].terrain='B';
         h[1][4].label=134;
+        h[1][4].exitType[1]=2;
+        h[1][4].exitType[5]=4;
+
         h[1][5].terrain='J';
         h[1][5].label=135;
+        h[1][5].exitType[0]=4;
 
         h[2][1].terrain='D';
         h[2][1].label=128;
+        h[2][1].exitType[1]=4;
+
         h[2][2].terrain='P';
         h[2][2].label=129;
+        h[2][2].exitType[0]=4;
+        h[2][2].exitType[2]=2;
+
         h[2][3].terrain='H';
         h[2][3].label=32;
+        h[2][3].exitType[3]=4;
+        h[2][3].exitType[5]=1;
+
         h[2][4].terrain='J';
         h[2][4].label=33;
+        h[2][4].exitType[2]=4;
+        h[2][4].exitType[4]=1;
+
         h[2][5].terrain='M';
         h[2][5].label=136;
+        h[2][5].exitType[1]=2;
+        h[2][5].exitType[5]=4;
+
         h[2][6].terrain='B';
         h[2][6].label=137;
+        h[2][6].exitType[0]=4;
 
         h[3][0].terrain='M';
         h[3][0].label=126;
+        h[3][0].exitType[1]=4;
+
         h[3][1].terrain='B';
         h[3][1].label=127;
+        h[3][1].exitType[0]=4;
+        h[3][1].exitType[2]=2;
+
         h[3][2].terrain='T';
         h[3][2].label=500;
+        h[3][2].exitType[1]=3;
+        h[3][2].exitType[3]=3;
+        h[3][2].exitType[5]=3;
+
         h[3][3].terrain='B';
         h[3][3].label=31;
+        h[3][3].exitType[0]=2;
+        h[3][3].exitType[3]=4;
+
         h[3][4].terrain='P';
         h[3][4].label=34;
+        h[3][4].exitType[1]=4;
+        h[3][4].exitType[3]=2;
+
         h[3][5].terrain='T';
         h[3][5].label=600;
+        h[3][5].exitType[0]=3;
+        h[3][5].exitType[2]=3;
+        h[3][5].exitType[4]=3;
+
         h[3][6].terrain='P';
         h[3][6].label=138;
+        h[3][6].exitType[1]=2;
+        h[3][6].exitType[5]=4;
+
         h[3][7].terrain='D';
         h[3][7].label=139;
+        h[3][7].exitType[0]=4;
 
         h[4][0].terrain='J';
         h[4][0].label=125;
+        h[4][0].exitType[2]=4;
+
         h[4][1].terrain='J';
         h[4][1].label=26;
+        h[4][1].exitType[3]=4;
+        h[4][1].exitType[5]=1;
+
         h[4][2].terrain='M';
         h[4][2].label=27;
+        h[4][2].exitType[2]=4;
+        h[4][2].exitType[4]=2;
+
         h[4][3].terrain='W';
         h[4][3].label=30;
+        h[4][3].exitType[3]=2;
+        h[4][3].exitType[5]=4;
+
         h[4][4].terrain='D';
         h[4][4].label=35;
+        h[4][4].exitType[0]=2;
+        h[4][4].exitType[2]=4;
+
         h[4][5].terrain='B';
         h[4][5].label=38;
+        h[4][5].exitType[3]=4;
+        h[4][5].exitType[5]=2;
+
         h[4][6].terrain='W';
         h[4][6].label=39;
+        h[4][6].exitType[2]=4;
+        h[4][6].exitType[4]=1;
+
         h[4][7].terrain='M';
         h[4][7].label=140;
+        h[4][7].exitType[5]=4;
 
         h[5][0].terrain='P';
         h[5][0].label=124;
+        h[5][0].exitType[1]=4;
+        h[5][0].exitType[3]=2;
+
         h[5][1].terrain='W';
         h[5][1].label=25;
+        h[5][1].exitType[0]=1;
+        h[5][1].exitType[4]=4;
+
         h[5][2].terrain='S';
         h[5][2].label=28;
+        h[5][2].exitType[1]=2;
+        h[5][2].exitType[3]=4;
+
         h[5][3].terrain='P';
         h[5][3].label=29;
+        h[5][3].exitType[2]=2;
+        h[5][3].exitType[4]=4;
+
         h[5][4].terrain='M';
         h[5][4].label=36;
+        h[5][4].exitType[1]=2;
+        h[5][4].exitType[3]=4;
+
         h[5][5].terrain='H';
         h[5][5].label=37;
+        h[5][5].exitType[2]=2;
+        h[5][5].exitType[4]=4;
+
         h[5][6].terrain='J';
         h[5][6].label=40;
+        h[5][6].exitType[1]=4;
+        h[5][6].exitType[3]=1;
+
         h[5][7].terrain='B';
         h[5][7].label=141;
+        h[5][7].exitType[0]=2;
+        h[5][7].exitType[4]=4;
 
         h[6][0].terrain='B';
         h[6][0].label=123;
+        h[6][0].exitType[1]=4;
+
         h[6][1].terrain='B';
         h[6][1].label=24;
+        h[6][1].exitType[1]=2;
+        h[6][1].exitType[5]=4;
+
         h[6][2].terrain='H';
         h[6][2].label=23;
+        h[6][2].exitType[0]=4;
+        h[6][2].exitType[4]=2;
+
         h[6][3].terrain='m';
         h[6][3].label=5000;
+        h[6][3].exitType[1]=3;
+        h[6][3].exitType[3]=3;
+        h[6][3].exitType[5]=1;
+
         h[6][4].terrain='t';
         h[6][4].label=6000;
+        h[6][4].exitType[0]=3;
+        h[6][4].exitType[2]=3;
+        h[6][4].exitType[4]=1;
+
         h[6][5].terrain='S';
         h[6][5].label=42;
+        h[6][5].exitType[1]=4;
+        h[6][5].exitType[5]=2;
+
         h[6][6].terrain='M';
         h[6][6].label=41;
+        h[6][6].exitType[0]=4;
+        h[6][6].exitType[2]=2;
+
         h[6][7].terrain='S';
         h[6][7].label=142;
+        h[6][7].exitType[5]=4;
 
         h[7][0].terrain='M';
         h[7][0].label=122;
+        h[7][0].exitType[1]=4;
+        h[7][0].exitType[3]=2;
+
         h[7][1].terrain='T';
         h[7][1].label=400;
+        h[7][1].exitType[0]=3;
+        h[7][1].exitType[2]=3;
+        h[7][1].exitType[4]=3;
+
         h[7][2].terrain='M';
         h[7][2].label=22;
+        h[7][2].exitType[3]=2;
+        h[7][2].exitType[5]=4;
+
         h[7][3].terrain='t';
         h[7][3].label=4000;
+        h[7][3].exitType[0]=1;
+        h[7][3].exitType[2]=3;
+        h[7][3].exitType[4]=3;
+
         h[7][4].terrain='m';
         h[7][4].label=1000;
+        h[7][4].exitType[1]=3;
+        h[7][4].exitType[3]=3;
+        h[7][4].exitType[5]=1;
+
         h[7][5].terrain='P';
         h[7][5].label=1;
+        h[7][5].exitType[0]=2;
+        h[7][5].exitType[2]=4;
+
         h[7][6].terrain='T';
         h[7][6].label=100;
+        h[7][6].exitType[1]=3;
+        h[7][6].exitType[3]=3;
+        h[7][6].exitType[5]=1;
+
         h[7][7].terrain='P';
         h[7][7].label=101;
+        h[7][7].exitType[0]=2;
+        h[7][7].exitType[4]=4;
 
         h[8][0].terrain='S';
         h[8][0].label=121;
+        h[8][0].exitType[2]=4;
+
         h[8][1].terrain='P';
         h[8][1].label=20;
+        h[8][1].exitType[3]=4;
+        h[8][1].exitType[5]=2;
+
         h[8][2].terrain='D';
         h[8][2].label=21;
+        h[8][2].exitType[2]=2;
+        h[8][2].exitType[4]=4;
+
         h[8][3].terrain='m';
         h[8][3].label=3000;
+        h[8][3].exitType[1]=1;
+        h[8][3].exitType[3]=3;
+        h[8][3].exitType[5]=3;
+
         h[8][4].terrain='t';
         h[8][4].label=2000;
+        h[8][4].exitType[0]=3;
+        h[8][4].exitType[2]=1;
+        h[8][4].exitType[4]=3;
+
         h[8][5].terrain='W';
         h[8][5].label=2;
+        h[8][5].exitType[1]=2;
+        h[8][5].exitType[3]=4;
+
         h[8][6].terrain='B';
         h[8][6].label=3;
+        h[8][6].exitType[2]=4;
+        h[8][6].exitType[4]=2;
+
         h[8][7].terrain='B';
         h[8][7].label=102;
+        h[8][7].exitType[5]=4;
 
         h[9][0].terrain='B';
         h[9][0].label=120;
+        h[9][0].exitType[1]=4;
+        h[9][0].exitType[3]=2;
+
         h[9][1].terrain='J';
         h[9][1].label=19;
+        h[9][1].exitType[0]=1;
+        h[9][1].exitType[4]=2;
+
         h[9][2].terrain='W';
         h[9][2].label=16;
+        h[9][2].exitType[1]=4;
+        h[9][2].exitType[5]=2;
+
         h[9][3].terrain='P';
         h[9][3].label=15;
+        h[9][3].exitType[0]=4;
+        h[9][3].exitType[4]=2;
+
         h[9][4].terrain='M';
         h[9][4].label=8;
+        h[9][4].exitType[1]=4;
+        h[9][4].exitType[5]=2;
+
         h[9][5].terrain='D';
         h[9][5].label=7;
+        h[9][5].exitType[0]=4;
+        h[9][5].exitType[4]=2;
+
         h[9][6].terrain='H';
         h[9][6].label=4;
+        h[9][6].exitType[1]=4;
+        h[9][6].exitType[3]=1;
+
         h[9][7].terrain='M';
         h[9][7].label=103;
+        h[9][7].exitType[0]=2;
+        h[9][7].exitType[4]=4;
 
         h[10][0].terrain='P';
         h[10][0].label=119;
+        h[10][0].exitType[2]=4;
+
         h[10][1].terrain='H';
         h[10][1].label=18;
+        h[10][1].exitType[1]=1;
+        h[10][1].exitType[5]=4;
+
         h[10][2].terrain='B';
         h[10][2].label=17;
+        h[10][2].exitType[0]=4;
+        h[10][2].exitType[2]=2;
+
         h[10][3].terrain='S';
         h[10][3].label=14;
+        h[10][3].exitType[2]=2;
+        h[10][3].exitType[4]=4;
+
         h[10][4].terrain='H';
         h[10][4].label=9;
+        h[10][4].exitType[0]=2;
+        h[10][4].exitType[2]=4;
+
         h[10][5].terrain='P';
         h[10][5].label=6;
+        h[10][5].exitType[1]=2;
+        h[10][5].exitType[5]=4;
+
         h[10][6].terrain='J';
         h[10][6].label=5;
+        h[10][6].exitType[0]=4;
+        h[10][6].exitType[2]=1;
+
         h[10][7].terrain='J';
         h[10][7].label=104;
+        h[10][7].exitType[5]=4;
 
         h[11][0].terrain='D';
         h[11][0].label=118;
+        h[11][0].exitType[3]=4;
+
         h[11][1].terrain='M';
         h[11][1].label=117;
+        h[11][1].exitType[2]=4;
+        h[11][1].exitType[4]=2;
+
         h[11][2].terrain='T';
         h[11][2].label=300;
+        h[11][2].exitType[1]=3;
+        h[11][2].exitType[3]=3;
+        h[11][2].exitType[5]=3;
+
         h[11][3].terrain='M';
         h[11][3].label=13;
+        h[11][3].exitType[0]=2;
+        h[11][3].exitType[4]=4;
+
         h[11][4].terrain='B';
         h[11][4].label=10;
+        h[11][4].exitType[1]=4;
+        h[11][4].exitType[3]=2;
+
         h[11][5].terrain='T';
         h[11][5].label=200;
+        h[11][5].exitType[0]=3;
+        h[11][5].exitType[2]=3;
+        h[11][5].exitType[4]=3;
+
         h[11][6].terrain='B';
         h[11][6].label=106;
+        h[11][6].exitType[3]=4;
+        h[11][6].exitType[5]=2;
+
         h[11][7].terrain='P';
         h[11][7].label=105;
+        h[11][7].exitType[4]=3;
 
         h[12][1].terrain='B';
         h[12][1].label=116;
+        h[12][1].exitType[3]=4;
+
         h[12][2].terrain='P';
         h[12][2].label=115;
+        h[12][2].exitType[2]=4;
+        h[12][2].exitType[4]=2;
+
         h[12][3].terrain='J';
         h[12][3].label=12;
+        h[12][3].exitType[1]=1;
+        h[12][3].exitType[5]=4;
+
         h[12][4].terrain='W';
         h[12][4].label=11;
+        h[12][4].exitType[0]=4;
+        h[12][4].exitType[2]=1;
+
         h[12][5].terrain='M';
         h[12][5].label=108;
+        h[12][5].exitType[3]=4;
+        h[12][5].exitType[5]=2;
+
         h[12][6].terrain='D';
         h[12][6].label=107;
+        h[12][6].exitType[4]=4;
 
         h[13][2].terrain='J';
         h[13][2].label=114;
+        h[13][2].exitType[3]=4;
+
         h[13][3].terrain='B';
         h[13][3].label=113;
+        h[13][3].exitType[2]=4;
+        h[13][3].exitType[4]=2;
+
         h[13][4].terrain='P';
         h[13][4].label=110;
+        h[13][4].exitType[3]=4;
+        h[13][4].exitType[5]=2;
+
         h[13][5].terrain='B';
         h[13][5].label=109;
+        h[13][5].exitType[4]=4;
 
         h[14][3].terrain='M';
         h[14][3].label=112;
+        h[14][3].exitType[3]=4;
+
         h[14][4].terrain='S';
         h[14][4].label=111;
+        h[14][4].exitType[4]=4;
 
-        // TODO: exits
 
 
         tracker = new MediaTracker(this);
@@ -556,9 +816,8 @@ class MasterHex
     int label;
 
     // n, ne, se, s, sw, nw
-    // 0=none, 1=block, 2=arch, 3=arrow(s)
+    // 0=none, 1=block, 2=arch, 3=arrow 4=arrows
     int[] exitType = new int[6];
-    MasterHex[] exitHex = new MasterHex[6];
 
     MasterHex(int cx, int cy, int scale, boolean inverted)
     {
@@ -602,9 +861,10 @@ class MasterHex
     }
 
 
-    // TODO: Pick better colors.
+    // XXX: Pick better colors.
     public void paint(Graphics g)
     {
+        // XXX: Change this to something that keeps the basic hex color
         if (selected)
         {
             g.setColor(java.awt.Color.black);
@@ -620,13 +880,13 @@ class MasterHex
                     g.setColor(java.awt.Color.green);
                     break; 
                 case 'D':
-                    g.setColor(java.awt.Color.blue);
+                    g.setColor(java.awt.Color.orange);
                     break;
                 case 'H':
                     g.setColor(java.awt.Color.cyan);
                     break;
                 case 'J':
-                    g.setColor(java.awt.Color.orange);
+                    g.setColor(java.awt.Color.darkGray);
                     break;
                 case 'm':
                     g.setColor(java.awt.Color.red);
@@ -638,7 +898,7 @@ class MasterHex
                     g.setColor(java.awt.Color.yellow);
                     break;
                 case 'S':
-                    g.setColor(java.awt.Color.darkGray);
+                    g.setColor(java.awt.Color.blue);
                     break;
                 case 'T':
                     g.setColor(java.awt.Color.gray);
@@ -656,7 +916,7 @@ class MasterHex
             g.fillPolygon(p);
             g.setColor(java.awt.Color.black);
             g.drawPolygon(p);
-            // XXX
+            // XXX scale this
             FontMetrics fontMetrics = g.getFontMetrics();
             g.drawString("" + label, rectBound.x + rectBound.width / 2 -
                 fontMetrics.stringWidth("" + label) / 2,
@@ -664,6 +924,45 @@ class MasterHex
             g.drawString(getTerrainName(), rectBound.x + rectBound.width / 2 -
                 fontMetrics.stringWidth(getTerrainName()) / 2,
                 rectBound.y + rectBound.height * 2 / 3);
+
+            // draw exits
+            // There are up to 3 gates to draw.  Each is 1/6 of a hexside
+            // square.  The first is positioned from 1/6 to 1/3 of the way
+            // along the hexside, the second from 5/12 to 7/12, and the 
+            // third from 2/3 to 5/6.  The inner edge of each is 1/12 of a
+            // hexside inside the hexside, and the outer edge is 1/12 of a
+            // hexside outside the hexside.
+
+            for (int i = 0; i < 6; i++)
+            {
+                switch(exitType[i])
+                {
+                    case 1:
+/*
+                        x1 = xVertex[i] + (xVertex[(i + 1) % 6] - xVertex[i]) / 6;
+                        y1 = yVertex[i] + (yVertex[(i + 1) % 6] - yVertex[i]) / 6;
+                        x2 = 
+
+                        x4 = xVertex[i] + (xVertex[(i + 1) % 6] - xVertex[i]) / 3;
+                        y4 = yVertex[i] + (yVertex[(i + 1) % 6] - yVertex[i]) / 3;
+*/
+                        
+                        break;
+
+                    case 2:   // arch
+                        break;
+
+                    case 3:   // arrow
+                        break;
+
+                    case 4:   // arrows
+                        break;
+
+                    case 0:   // none
+                    default:
+                        break;
+                }
+            }
         }
     }
 
