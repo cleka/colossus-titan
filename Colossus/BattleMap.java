14eec870ae568706e11b3929be317c0c06ebbf41
David Ripton <dripton@ripton.net>
1998-01-19 22:56:39 +0000

    Tweaked scrolling and scaling, still not to perfection.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@28 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/BattleMap.java
+++ Colossus/BattleMap.java
@@ -35,11 +35,12 @@ public class BattleMap extends Frame implements MouseListener,
     private int scale;
     private int cx;
     private int cy;
+    private int chitScale;
     Dimension preferredSize;
-    Scrollbar vert;
-    Scrollbar horz; 
-    int tx = 0;
-    int ty = 0;
+    Scrollbar vBar;
+    Scrollbar hBar; 
+    int tx;
+    int ty;
     MenuBar mb;
     Menu m1;
     MenuItem mi1_1, mi1_2;
@@ -49,9 +50,11 @@ public class BattleMap extends Frame implements MouseListener,
     {
         super("BattleMap");
 
-        scale = 25;
+        scale = 17;
         cx = 3 * scale;
         cy = 3 * scale;
+        chitScale = (int) Math.round(2.4 * scale);
+
         preferredSize = new Dimension(28 * scale, 28 * scale);
         
         setSize(preferredSize);
@@ -65,10 +68,18 @@ public class BattleMap extends Frame implements MouseListener,
         needToClear = false;
         imagesLoaded = false;
 
-        horz = new Scrollbar(Scrollbar.HORIZONTAL);
-        vert = new Scrollbar(Scrollbar.VERTICAL);
-        add("East", vert);
-        add("South", horz);
+        // XXX Scale scrollbars and don't display if not needed
+        tx =  0;
+        ty =  0;
+        Dimension d = getSize();
+        vBar = new Scrollbar(Scrollbar.VERTICAL, 0, scale, 0, 
+            20 * scale - d.height);
+        hBar = new Scrollbar(Scrollbar.HORIZONTAL, 0, scale, 0,
+            20 * scale - d.width);
+        add("East", vBar);
+        add("South", hBar);
+        vBar.addAdjustmentListener(this);
+        hBar.addAdjustmentListener(this);
         
         mb = new MenuBar();
         m1 = new Menu("Size", false);
@@ -80,8 +91,6 @@ public class BattleMap extends Frame implements MouseListener,
         setMenuBar(mb);
         mi1_1.addActionListener(this);
         mi1_2.addActionListener(this);
-        vert.addAdjustmentListener(this);
-        horz.addAdjustmentListener(this);
 
         pack();
         validate();
@@ -102,30 +111,54 @@ public class BattleMap extends Frame implements MouseListener,
 
         tracker = new MediaTracker(this);
 
-        chits[0] = new Chit(100, 100, 60, "images/Angel.gif", this);
-        chits[1] = new Chit(120, 120, 60, "images/Archangel.gif", this);
-        chits[2] = new Chit(140, 140, 60, "images/Behemoth.gif", this);
-        chits[3] = new Chit(160, 160, 60, "images/Centaur.gif", this);
-        chits[4] = new Chit(180, 180, 60, "images/Colossus.gif", this);
-        chits[5] = new Chit(200, 200, 60, "images/Cyclops.gif", this);
-        chits[6] = new Chit(220, 220, 60, "images/Gargoyle.gif", this);
-        chits[7] = new Chit(240, 240, 60, "images/Giant.gif", this);
-        chits[8] = new Chit(260, 260, 60, "images/Gorgon.gif", this);
-        chits[9] = new Chit(280, 280, 60, "images/Griffon.gif", this);
-        chits[10] = new Chit(300, 300, 60, "images/Guardian.gif", this);
-        chits[11] = new Chit(320, 320, 60, "images/Hydra.gif", this);
-        chits[12] = new Chit(340, 340, 60, "images/Lion.gif", this);
-        chits[13] = new Chit(360, 360, 60, "images/Minotaur.gif", this);
-        chits[14] = new Chit(380, 380, 60, "images/Ogre.gif", this);
-        chits[15] = new Chit(400, 400, 60, "images/Ranger.gif", this);
-        chits[16] = new Chit(420, 420, 60, "images/Serpent.gif", this);
-        chits[17] = new Chit(440, 440, 60, "images/Titan.gif", this);
-        chits[18] = new Chit(460, 460, 60, "images/Troll.gif", this);
-        chits[19] = new Chit(480, 480, 60, "images/Unicorn.gif", this);
-        chits[20] = new Chit(500, 500, 60, "images/Warbear.gif", this);
-        chits[21] = new Chit(520, 520, 60, "images/Warlock.gif", this);
-        chits[22] = new Chit(540, 540, 60, "images/Dragon.gif", this);
-        chits[23] = new Chit(560, 560, 60, "images/Wyvern.gif", this);
+        chits[0] = new Chit(1 * scale, 1 * scale, chitScale, 
+            "images/Angel.gif", this);
+        chits[1] = new Chit(2 * scale, 2 * scale, chitScale,
+            "images/Archangel.gif", this);
+        chits[2] = new Chit(3 * scale, 3 * scale, chitScale,
+            "images/Behemoth.gif", this);
+        chits[3] = new Chit(4 * scale, 4 * scale, chitScale,
+            "images/Centaur.gif", this);
+        chits[4] = new Chit(5 * scale, 5 * scale, chitScale,
+            "images/Colossus.gif", this);
+        chits[5] = new Chit(6 * scale, 6 * scale, chitScale,
+            "images/Cyclops.gif", this);
+        chits[6] = new Chit(7 * scale, 7 * scale, chitScale,
+            "images/Gargoyle.gif", this);
+        chits[7] = new Chit(8 * scale, 8 * scale, chitScale,
+            "images/Giant.gif", this);
+        chits[8] = new Chit(9 * scale, 9 * scale, chitScale,
+            "images/Gorgon.gif", this);
+        chits[9] = new Chit(10 * scale, 10 * scale, chitScale,
+            "images/Griffon.gif", this);
+        chits[10] = new Chit(11 * scale, 11 * scale, chitScale,
+            "images/Guardian.gif", this);
+        chits[11] = new Chit(12 * scale, 12 * scale, chitScale,
+            "images/Hydra.gif", this);
+        chits[12] = new Chit(13 * scale, 13 * scale, chitScale,
+            "images/Lion.gif", this);
+        chits[13] = new Chit(14 * scale, 14 * scale, chitScale,
+            "images/Minotaur.gif", this);
+        chits[14] = new Chit(15 * scale, 15 * scale, chitScale,
+            "images/Ogre.gif", this);
+        chits[15] = new Chit(16 * scale, 16 * scale, chitScale,
+            "images/Ranger.gif", this);
+        chits[16] = new Chit(17 * scale, 17 * scale, chitScale,
+            "images/Serpent.gif", this);
+        chits[17] = new Chit(18 * scale, 18 * scale, chitScale,
+            "images/Titan.gif", this);
+        chits[18] = new Chit(19 * scale, 19 * scale, chitScale,
+            "images/Troll.gif", this);
+        chits[19] = new Chit(20 * scale, 20 * scale, chitScale,
+            "images/Unicorn.gif", this);
+        chits[20] = new Chit(21 * scale, 21 * scale, chitScale,
+            "images/Warbear.gif", this);
+        chits[21] = new Chit(22 * scale, 22 * scale, chitScale,
+            "images/Warlock.gif", this);
+        chits[22] = new Chit(23 * scale, 23 * scale, chitScale,
+            "images/Dragon.gif", this);
+        chits[23] = new Chit(24 * scale, 24 * scale, chitScale,
+            "images/Wyvern.gif", this);
 
         for (int i = 0; i < chits.length; i++)
         {
@@ -148,12 +181,11 @@ public class BattleMap extends Frame implements MouseListener,
 
     void rescale(int scale)
     {
+        int oldscale = this.scale;
         this.scale = scale;
+        chitScale = (int) Math.round(2.4 * scale);
         cx = 3 * scale;
         cy = 3 * scale; 
-        preferredSize.width = 28 * scale;
-        preferredSize.height = 28 * scale;
-        setSize(preferredSize);
         
         for (int i = 0; i < h.length; i++)
         {
@@ -169,7 +201,26 @@ public class BattleMap extends Frame implements MouseListener,
             }
         }
 
+        for (int i = 0; i < chits.length; i++)
+        {
+            chits[i].rescale(chitScale);
+            Rectangle rect = chits[i].getBounds();
+            Point point = new Point((int) Math.round((double) rect.x * 
+                scale / oldscale), (int) Math.round((double) rect.y * 
+                scale / oldscale));
+            chits[i].setLocation(point);
+        }
+
+        // XXX Rescale scrollbars
+
+        Dimension d = getSize();
+        vBar.setValues(ty, scale, 0, 20 * scale - d.height);
+        vBar.setBlockIncrement(scale);
+        hBar.setValues(tx, scale, 0, 20 * scale - d.width);
+        hBar.setBlockIncrement(scale);
+
         validate();
+        needToClear = true;
         repaint();
     }
 
@@ -288,13 +339,27 @@ public class BattleMap extends Frame implements MouseListener,
     public void adjustmentValueChanged(AdjustmentEvent e)
     {
         // Horizontal or vertical?
-        if (e.getAdjustable() == horz)
+        if (e.getAdjustable() == hBar)
         {
+            int oldtx = tx;
             tx = e.getValue();
+            if (tx > oldtx)
+            {
+                // Need to invalidate newly-visible region at right
+                Dimension d = getSize();
+                repaint(tx, 0, d.width - tx, d.height);
+            }
         }
         else
         {
+            int oldty = ty;
             ty = e.getValue();
+            if (ty > oldty)
+            {
+                // Need to invalidate newly-visible region at bottom
+                Dimension d = getSize();
+                repaint(0, ty, d.width, d.height - ty);
+            }
         }
 
         repaint();
@@ -316,13 +381,12 @@ public class BattleMap extends Frame implements MouseListener,
 
     public void paint(Graphics g)
     {
-        g.translate(-tx, -ty);
-
         if (!imagesLoaded)
         {
             return;
         }
 
+        g.translate(-tx, -ty);
         rectClip = g.getClipBounds();
 
         for (int i = 0; i < h.length; i++)
@@ -348,19 +412,25 @@ public class BattleMap extends Frame implements MouseListener,
 
     public void update(Graphics g)
     {
-        g.translate(-tx, -ty);
+        if (!imagesLoaded)
+        {
+            return;
+        }
 
         Dimension d = getSize();
-        rectClip = g.getClipBounds();
         
         // Create the back buffer only if we don't have a good one.
         if (gBack == null || d.width != offDimension.width || 
             d.height != offDimension.height)
         {
             offDimension = d;
-            offImage = createImage(d.width, d.height);
+            // XXX Account for scrolling
+            offImage = createImage(2 * d.width, 2 * d.height);
             gBack = offImage.getGraphics();
         }
+
+        g.translate(-tx, -ty);
+        rectClip = g.getClipBounds();
 
         // Clear the background only when chits are dragged.
         if (needToClear)
@@ -398,7 +468,7 @@ public class BattleMap extends Frame implements MouseListener,
     
     public Dimension getMinimumSize()
     {
-        return new Dimension(100, 100);
+        return new Dimension(480, 480);
     }
 
     public Dimension getPreferredSize()
@@ -412,8 +482,8 @@ public class BattleMap extends Frame implements MouseListener,
         BattleMap battlemap = new BattleMap();
     }
 
-
 }
+
 
 
 /**
@@ -469,11 +539,12 @@ class Hex
         yVertex[3] = cy + (int) Math.round(2 * SQRT3 * scale);
         xVertex[4] = cx;
         yVertex[4] = cy + (int) Math.round(2 * SQRT3 * scale);
-        xVertex[5] = cx - 1 * scale;
+        xVertex[5] = cx - scale;
         yVertex[5] = cy + (int) Math.round(SQRT3 * scale);
 
-        p.xpoints = xVertex;
-        p.ypoints = yVertex;
+        // The hit testing breaks if we just reassign the vertices
+        // of the old polygon.
+        p = new Polygon(xVertex, yVertex, 6);
 
         // Add 1 to width and height because Java rectangles come up
         // one pixel short.
