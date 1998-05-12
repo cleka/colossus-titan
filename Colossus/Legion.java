4cd9d05f3a1c6af97013efa343704a4d86210574
David Ripton <dripton@ripton.net>
1998-05-12 23:00:59 +0000

    Extended maximum legion size to 8, to deal with initial    legions before initial splits. Moved Creature class out to Creature.java
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@45 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/Legion.java
+++ Colossus/Legion.java
@@ -13,13 +13,13 @@ class Legion
     Chit chit;
     int height;
     String markerId;    // Bk03, Rd12, etc.
-    Creature [] creatures = new Creature[7];
+    Creature [] creatures = new Creature[8];  // 8 before initial splits
 
     Legion(int cx, int cy, int scale, String markerId,
         Container container, int height, Creature creature0,
         Creature creature1, Creature creature2, Creature creature3, 
         Creature creature4, Creature creature5, 
-        Creature creature6)
+        Creature creature6, Creature creature7)
     {
         String imageFilename = "images/" + markerId + ".gif";
         this.chit = new Chit(cx, cy, scale, imageFilename, container);
@@ -31,6 +31,7 @@ class Legion
         creatures[4] = creature4;
         creatures[5] = creature5;
         creatures[6] = creature6;
+        creatures[7] = creature7;
     }
 
 
@@ -56,108 +57,4 @@ class Legion
         }
         return true;
     }
-
-}
-
-
-class Creature
-{
-    String name;
-    int power;
-    int skill;
-    boolean rangeStrikes; 
-    boolean flies;
-    boolean nativeBramble;
-    boolean nativeDrift;
-    boolean nativeBog;
-    boolean nativeSandDune;
-    boolean nativeSlope;
-    boolean lord;
-
-
-    // Add various Creature archetypes as class members
-    private static final Creature[] creatureData =
-    {
-        new Creature("Angel", 6, 4, false, true, false, false, false,
-            false, false, true),
-        new Creature("Archangel", 9, 4, false, true, false, false, false,
-            false, false, true),
-        new Creature("Behemoth", 8, 3, false, false, true, false, false, 
-            false, false, false),
-        new Creature("Centaur", 3, 4, false, false, false, false, false,
-            false, false, false),
-        new Creature("Colossus", 10, 4, false, false, false, true, false,
-            false, true, false),
-        new Creature("Cyclops", 9, 2, false, false, true, false, false,
-            false, false, false),
-        new Creature("Dragon", 9, 3, true, true, false, false, false,
-            false, true, false),
-        new Creature("Gargoyle", 4, 3, false, true, true, false, false,
-            false, false, false),
-        new Creature("Giant", 7, 4, true, false, false, true, false,
-            false, false, false),
-        new Creature("Gorgon", 6, 3, true, true, true, false, false, 
-            false, false, false),
-        new Creature("Griffon", 5, 4, false, true, false, false, false,
-            true, false, false),
-        new Creature("Guardian", 12, 2, false, true, false, false, false,
-            false, false, true),
-        new Creature("Hydra", 10, 3, true, false, false, false, true,
-            true, false, false),
-        new Creature("Lion", 5, 3, false, false, false, false, false,
-            true, true, false),
-        new Creature("Minotaur", 4, 4, true, false, false, false, false,
-            false, true, false),
-        new Creature("Ogre", 6, 2, false, false, false, false, true,
-            false, true, false),
-        new Creature("Ranger", 4, 4, true, true, false, false, true,
-            false, false, false),
-        new Creature("Serpent", 18, 2, false, false, true, false, false,
-            false, false, false),
-        new Creature("Titan", 6, 4, false, false, false, false, false,
-            false, false, true),
-        new Creature("Troll", 8, 2, false, false, false, true, true,
-            false, false, false),
-        new Creature("Unicorn", 6, 4, false, false, false, false, false,
-            false, true, false),
-        new Creature("Warbear", 6, 3, false, false, false, true, false, 
-            false, false, false),
-        new Creature("Warlock", 5, 4, true, false, false, false, false,
-            false, false, true),
-        new Creature("Wyvern", 7, 3, false, true, false, false, true,
-            false, false, false),
-    };
-    
-    private static Hashtable lookup = new Hashtable(creatureData.length);
-    static
-    {
-        for (int i = 0; i < creatureData.length; i++)
-        {
-            lookup.put(creatureData[i].name, creatureData[i]);
-        }
-    }
-
-    Creature(String name, int power, int skill, boolean rangeStrikes, 
-        boolean flies, boolean nativeBramble, boolean nativeDrift, 
-        boolean nativeBog, boolean nativeSandDune, boolean nativeSlope, 
-        boolean lord)
-    {
-        this.name = name;
-        this.power = power;
-        this.skill = skill;
-        this.rangeStrikes = rangeStrikes;
-        this.flies = flies;
-        this.nativeBramble = nativeBramble;
-        this.nativeDrift = nativeDrift;
-        this.nativeBog = nativeBog;
-        this.nativeSandDune = nativeSandDune;
-        this.nativeSlope = nativeSlope;
-        this.lord = lord;
-    }
-
-    int getPointValue()
-    {
-        return power * skill;
-    }
-
 }
