e3bd534990b0ee748edc297a23ea8006794d9f01
David Ripton <dripton@ripton.net>
1998-05-13 18:32:21 +0000

    Initial revision
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@53 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/ShowLegion.java
@@ -0,0 +1,104 @@
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * Class ShowLegion displays the chits of the Creatures in a Legion
+ * @version $Id$
+ * author David Ripton
+ */
+
+class ShowLegion extends Dialog implements MouseListener
+{
+    MediaTracker tracker;
+    boolean imagesLoaded;
+    Legion legion;
+    Chit [] chits;
+
+    ShowLegion(Frame parentFrame, Legion legion, Point point)
+    {
+        super(parentFrame, "Contents of Legion " + legion.markerId, true);
+
+        int scale = 60;
+        setLocation(new Point(point.x - scale, point.y - scale));
+        setSize((21 * scale / 20) * (legion.height), 8 * scale / 5);
+        
+        setLayout(null);
+
+        pack();
+        addMouseListener(this);
+
+        this.legion = legion;
+
+        imagesLoaded = false;
+
+        chits = new Chit[legion.height];
+        for (int i = 0; i < legion.height; i++)
+        {
+            chits[i] = new Chit(i * scale + (scale / 5), scale / 2, scale, 
+                "images/" + legion.creatures[i].name + ".gif", this);
+        }
+
+        tracker = new MediaTracker(this);
+
+        for (int i = 0; i < legion.height; i++)
+        {
+            tracker.addImage(chits[i].image, 0);
+        }
+
+        try
+        {
+            tracker.waitForAll();
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("waitForAll was interrupted");
+        }
+
+        imagesLoaded = true;
+        setVisible(true);
+        repaint();
+    }
+
+
+    public void paint(Graphics g)
+    {
+        if (!imagesLoaded)
+        {
+            return;
+        }
+
+        // No need to mess around with clipping rectangles, since
+        //    this window is ephemeral.
+        for (int i = legion.height - 1; i >= 0; i--)
+        {
+            chits[i].paint(g);
+        }
+    }
+
+
+    public void mouseClicked(MouseEvent e)
+    {
+    }
+    
+    
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+
+    public void mouseExited(MouseEvent e)
+    {
+    }
+    
+
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    
+    public void mouseReleased(MouseEvent e)
+    {
+        // Exit.
+        dispose();
+    }
+}
