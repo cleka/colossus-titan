9642e13623f953b93372f2efb0f77949338e9040
David Ripton <dripton@ripton.net>
1998-05-21 19:56:13 +0000

    Added setupSplitDialog and setupMoveDialog
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@88 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/Turn.java
+++ Colossus/Turn.java
@@ -20,11 +20,16 @@ class Turn extends Dialog implements ActionListener
         this.parentFrame = parentFrame;
 
         setSize(300, 250);
-
-        // Place this window in the upper right corner.
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation(new Point(d.width - getSize().width, 0));
         
+        setupSplitDialog();
+
+        setVisible(true);
+    }
+
+    
+    void setupSplitDialog()
+    {
+        removeAll();
         setLayout(new GridLayout(0, 2));
 
         add(new Label(game.player[game.activePlayer].name + 
@@ -33,8 +38,52 @@ class Turn extends Dialog implements ActionListener
         add(button1);
         button1.addActionListener(this);
 
+        // Place this window in the upper right corner.
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(new Point(d.width - getSize().width, 0));
+
         pack();
-        setVisible(true);
+    }
+
+
+    void setupMoveDialog()
+    {
+        removeAll();
+        if (game.player[game.activePlayer].mulligansLeft > 0) 
+        {
+            setLayout(new GridLayout(0, 4));
+        }
+        else
+        {
+            setLayout(new GridLayout(0, 3));
+        }
+
+        game.player[game.activePlayer].movementRoll = 
+            (int) Math.ceil(6 * Math.random());
+
+        add(new Label(game.player[game.activePlayer].name + 
+            " : Movement Roll: " + 
+            game.player[game.activePlayer].movementRoll));
+        Button button1 = new Button("Reset Moves");
+        add(button1);
+        button1.addActionListener(this);
+
+        if (game.player[game.activePlayer].mulligansLeft > 0)
+        {
+            Button button2 = new Button("Take Mulligan");
+            add(button2);
+            button2.addActionListener(this);
+        }
+
+        Button button3 = new Button("Done with Moves");
+        add(button3);
+        button3.addActionListener(this);
+           
+        // Place this window in the upper right corner.
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(new Point(d.width - getSize().width, 0));
+
+        pack();
     }
 
     
@@ -51,6 +100,8 @@ class Turn extends Dialog implements ActionListener
                 }
             }
             game.phase++;
+
+            setupMoveDialog();
         }
 
         else if (e.getActionCommand() == "Reset Moves")
@@ -62,18 +113,19 @@ class Turn extends Dialog implements ActionListener
             game.player[game.activePlayer].legionsMoved = 0;
         }
 
-        else if (e.getActionCommand() == "Mulligan")
+        else if (e.getActionCommand() == "Take Mulligan")
         {
             if (game.player[game.activePlayer].mulligansLeft > 0)
             {
+                game.player[game.activePlayer].movementRoll = 
+                    (int) Math.ceil(6 * Math.random());
+
                 game.player[game.activePlayer].mulligansLeft--;
                 if (game.player[game.activePlayer].mulligansLeft == 0)
                 {
-                    // Remove the Mulligan button.
-
+                    // Remove the Take Mulligan button.
+                    setupMoveDialog();
                 }
-                game.player[game.activePlayer].movementRoll = 
-                    (int) Math.ceil(6 * Math.random());
             }
         }
 
@@ -95,7 +147,9 @@ class Turn extends Dialog implements ActionListener
 
         else if (e.getActionCommand() == "End Turn")
         {
-
+            game.activePlayer++;
+            game.activePlayer %= game.numPlayers;
+            game.phase = 1;
         }
     }
 }
