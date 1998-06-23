3ce5b1dd75aca80d8f7ae760b07a96f0ada70ff9
David Ripton <dripton@ripton.net>
1998-06-23 21:05:18 +0000

    Added count, getCount(), takeOne(), putOneBack() Made creatures non-final to allow count to change.  Made    all vars private and provided accessor functions.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@155 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/Creature.java
+++ Colossus/Creature.java
@@ -9,74 +9,76 @@ import java.util.*;
 
 class Creature
 {
-    String name;
-    int power;
-    int skill;
-    boolean rangeStrikes; 
-    boolean flies;
-    boolean nativeBramble;
-    boolean nativeDrift;
-    boolean nativeBog;
-    boolean nativeSandDune;
-    boolean nativeSlope;
-    boolean lord;
+    private String name;
+    private int power;
+    private int skill;
+    private boolean rangeStrikes; 
+    private boolean flies;
+    private boolean nativeBramble;
+    private boolean nativeDrift;
+    private boolean nativeBog;
+    private boolean nativeSandDune;
+    private boolean nativeSlope;
+    private boolean lord;
+    private boolean demilord;
+    private int count;
 
 
     // Add various Creature archetypes as class members
-    static final Creature angel = new Creature("Angel", 6, 4, 
-        false, true, false, false, false, false, false, true);
-    static final Creature archangel = new Creature("Archangel", 9, 4, 
-        false, true, false, false, false, false, false, true);
-    static final Creature behemoth = new Creature("Behemoth", 8, 3, 
-        false, false, true, false, false, false, false, false);
-    static final Creature centaur = new Creature("Centaur", 3, 4, 
-        false, false, false, false, false, false, false, false);
-    static final Creature colossus = new Creature("Colossus", 10, 4, 
-        false, false, false, true, false, false, true, false);
-    static final Creature cyclops = new Creature("Cyclops", 9, 2, 
-        false, false, true, false, false, false, false, false);
-    static final Creature dragon = new Creature("Dragon", 9, 3, 
-        true, true, false, false, false, false, true, false);
-    static final Creature gargoyle = new Creature("Gargoyle", 4, 3, 
-        false, true, true, false, false, false, false, false);
-    static final Creature giant = new Creature("Giant", 7, 4, 
-        true, false, false, true, false, false, false, false);
-    static final Creature gorgon = new Creature("Gorgon", 6, 3, 
-        true, true, true, false, false, false, false, false);
-    static final Creature griffon = new Creature("Griffon", 5, 4, 
-        false, true, false, false, false, true, false, false);
-    static final Creature guardian = new Creature("Guardian", 12, 2, 
-        false, true, false, false, false, false, false, true);
-    static final Creature hydra = new Creature("Hydra", 10, 3, 
-        true, false, false, false, true, true, false, false);
-    static final Creature lion = new Creature("Lion", 5, 3, 
-        false, false, false, false, false, true, true, false);
-    static final Creature minotaur = new Creature("Minotaur", 4, 4, 
-        true, false, false, false, false, false, true, false);
-    static final Creature ogre = new Creature("Ogre", 6, 2, 
-        false, false, false, false, true, false, true, false);
-    static final Creature ranger = new Creature("Ranger", 4, 4, 
-        true, true, false, false, true, false, false, false);
-    static final Creature serpent = new Creature("Serpent", 18, 2, 
-        false, false, true, false, false, false, false, false);
-    static final Creature titan = new Creature("Titan", 6, 4, 
-        false, false, false, false, false, false, false, true);
-    static final Creature troll = new Creature("Troll", 8, 2, 
-        false, false, false, true, true, false, false, false);
-    static final Creature unicorn = new Creature("Unicorn", 6, 4, 
-        false, false, false, false, false, false, true, false);
-    static final Creature warbear = new Creature("Warbear", 6, 3, 
-        false, false, false, true, false, false, false, false);
-    static final Creature warlock = new Creature("Warlock", 5, 4, 
-        true, false, false, false, false, false, false, true);
-    static final Creature wyvern = new Creature("Wyvern", 7, 3, 
-        false, true, false, false, true, false, false, false);
+    static Creature angel = new Creature("Angel", 6, 4, false, true, false, 
+        false, false, false, false, true, false, 18);
+    static Creature archangel = new Creature("Archangel", 9, 4, false, true, 
+        false, false, false, false, false, true, false, 6);
+    static Creature behemoth = new Creature("Behemoth", 8, 3, false, false, 
+        true, false, false, false, false, false, false, 18);
+    static Creature centaur = new Creature("Centaur", 3, 4, false, false, 
+        false, false, false, false, false, false, false, 25);
+    static Creature colossus = new Creature("Colossus", 10, 4, false, false, 
+        false, true, false, false, true, false, false, 10);
+    static Creature cyclops = new Creature("Cyclops", 9, 2, false, false, true,
+        false, false, false, false, false, false, 28);
+    static Creature dragon = new Creature("Dragon", 9, 3, true, true, false, 
+        false, false, false, true, false, false, 18);
+    static Creature gargoyle = new Creature("Gargoyle", 4, 3, false, true, 
+        true, false, false, false, false, false, false, 21);
+    static Creature giant = new Creature("Giant", 7, 4, true, false, false, 
+        true, false, false, false, false, false, 18);
+    static Creature gorgon = new Creature("Gorgon", 6, 3, true, true, true, 
+        false, false, false, false, false, false, 25);
+    static Creature griffon = new Creature("Griffon", 5, 4, false, true, false, 
+        false, false, true, false, false, false, 18);
+    static Creature guardian = new Creature("Guardian", 12, 2, false, true, 
+        false, false, false, false, false, false, true, 6);
+    static Creature hydra = new Creature("Hydra", 10, 3, true, false, false, 
+        false, true, true, false, false, false, 10);
+    static Creature lion = new Creature("Lion", 5, 3, false, false, false, 
+        false, false, true, true, false, false, 28);
+    static Creature minotaur = new Creature("Minotaur", 4, 4, true, false, 
+        false, false, false, false, true, false, false, 21);
+    static Creature ogre = new Creature("Ogre", 6, 2, false, false, false, 
+        false, true, false, true, false, false, 25);
+    static Creature ranger = new Creature("Ranger", 4, 4, true, true, false, 
+        false, true, false, false, false, false, 28);
+    static Creature serpent = new Creature("Serpent", 18, 2, false, false, 
+        true, false, false, false, false, false, false, 10);
+    static Creature titan = new Creature("Titan", 6, 4, false, false, false, 
+        false, false, false, false, true, false, 6);
+    static Creature troll = new Creature("Troll", 8, 2, false, false, false, 
+        true, true, false, false, false, false, 28);
+    static Creature unicorn = new Creature("Unicorn", 6, 4, false, false, 
+        false, false, false, false, true, false, false, 12);
+    static Creature warbear = new Creature("Warbear", 6, 3, false, false, 
+        false, true, false, false, false, false, false, 21);
+    static Creature warlock = new Creature("Warlock", 5, 4, true, false, false, 
+        false, false, false, false, false, true, 6);
+    static Creature wyvern = new Creature("Wyvern", 7, 3, false, true, false, 
+        false, true, false, false, false, false, 18);
     
 
     Creature(String name, int power, int skill, boolean rangeStrikes, 
         boolean flies, boolean nativeBramble, boolean nativeDrift, 
         boolean nativeBog, boolean nativeSandDune, boolean nativeSlope, 
-        boolean lord)
+        boolean lord, boolean demilord, int count)
     {
         this.name = name;
         this.power = power;
@@ -89,11 +91,109 @@ class Creature
         this.nativeSandDune = nativeSandDune;
         this.nativeSlope = nativeSlope;
         this.lord = lord;
+        this.demilord = demilord;
+        this.count = count;
     }
+
 
     int getPointValue()
     {
         return power * skill;
     }
 
+
+    int getCount()
+    {
+        return count;
+    }
+
+
+    void takeOne()
+    {
+        count--;
+    }
+
+
+    void putOneBack()
+    {
+        count++;
+    }
+
+
+    boolean isLord()
+    {
+        return lord;
+    }
+
+
+    boolean isImmortal()
+    {
+        return (lord || demilord);
+    }
+
+
+    String getName()
+    {
+        return name;
+    }
+
+
+    String getImageName()
+    {
+        return "images/" + name + ".gif";
+    }
+
+
+    int getPower()
+    {
+        return power;
+    }
+    
+    
+    int getSkill()
+    {
+        return skill;
+    }
+    
+    
+    boolean rangeStrikes()
+    {
+        return rangeStrikes;
+    }
+
+
+    boolean flies()
+    {
+        return flies;
+    }
+
+
+    boolean isNativeBramble()
+    {
+        return nativeBramble;
+    }
+
+
+    boolean isNativeDrift()
+    {
+        return nativeDrift;
+    }
+
+
+    boolean isNativeBog()
+    {
+        return nativeBog;
+    }
+
+
+    boolean isNativeSandDune()
+    {
+        return nativeSandDune;
+    }
+
+
+    boolean isNativeSlope()
+    {
+        return nativeSlope;
+    }
 }
