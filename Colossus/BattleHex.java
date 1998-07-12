ca691977a9289aa17c1cc94c0646c23568500d89
David Ripton <dripton@ripton.net>
1998-07-12 23:45:50 +0000

    Added creatures[] and related methods. Added findEntryCost() for movement.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@231 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/BattleHex.java
+++ Colossus/BattleHex.java
@@ -15,20 +15,32 @@ class Hex
     private int[] yVertex = new int[6];
     private Polygon p;
     private Rectangle rectBound;
-    private Creature creature;
+
+    private BattleMap map;
+
+    // Normal hexes hold only one chit, but entrances can hold up to 7.
+    int numChits = 0;
+    private BattleChit [] chits = new BattleChit[7];
+
+    // Valid elevations are 0, 1, and 2.
     private int elevation = 0;
 
     // p, r, s, t, o, v, d
     // plain, bramble, sand, tree, bog, volcano, drift
     private char terrain = 'p';
 
-    // d, c, s, w
-    // dune, cliff, slope, wall
+    // d, c, s, w, space
+    // dune, cliff, slope, wall, no obstacle
+    // The hexside is marked only in the higher hex.
     private char [] hexside = new char[6];
 
+    private Hex [] neighbors = new Hex[6];
 
-    Hex(int cx, int cy, int scale)
+
+    Hex(int cx, int cy, int scale, BattleMap map)
     {
+        this.map = map;
+
         selected = false;
 
         xVertex[0] = cx;
@@ -104,6 +116,13 @@ class Hex
     }
 
 
+    void repaint()
+    {
+        map.repaint(rectBound.x, rectBound.y, rectBound.width,
+            rectBound.height);
+    }
+
+
     boolean select(Point point)
     {
         if (p.contains(point))
@@ -112,6 +131,28 @@ class Hex
             return true;
         }
         return false;
+    }
+
+    void select()
+    {
+        selected = true;
+    }
+    
+    void unselect()
+    {
+        selected = false;
+    }
+
+    boolean isSelected()
+    {
+        return selected;
+    }
+
+
+    Point getCenter()
+    {
+        return new Point((xVertex[0] + xVertex[3]) / 2, 
+            (yVertex[0] + yVertex[3]) / 2);
     }
 
 
@@ -127,23 +168,92 @@ class Hex
     }
 
 
-    void setCreature(Creature creature)
+    int getNumChits()
     {
-        this.creature = creature;
+        return numChits;
     }
 
 
-    void removeCreature()
+    boolean isOccupied()
     {
-        this.creature = null;
+        return (numChits > 0);
     }
 
 
-    Creature getCreature()
+    void addChit(BattleChit chit)
     {
-        return creature;
+        if (numChits < 7)
+        {
+            chits[numChits] = chit;
+            numChits++;
+        }
+    }
+    
+    
+    void removeChit(int i)
+    {
+        for (int j = i; j < numChits - 1; j++)
+        {
+            chits[j] = chits[j + 1];
+        }
+        chits[numChits - 1] = null;
+        numChits--;
     }
 
+
+    BattleChit getChit()
+    {
+        if (numChits > 0)
+        {
+            return chits[0];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+    BattleChit getChit(int i)
+    {
+        if (numChits > i)
+        {
+            return chits[i];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+    void alignChits()
+    {
+        if (numChits == 0)
+        {
+            return;
+        }
+
+        int chitScale = chits[0].getBounds().width;
+        Point point = getCenter();
+
+        // Cascade chits diagonally.
+        point.x -= chitScale * (numChits / 2) / 4;
+        point.y -= chitScale * (numChits / 2) / 4;
+
+        for (int i = 0; i < numChits; i++)
+        {
+            chits[i].setLocationAbs(point);
+            point.x += chitScale / 4;
+            point.y += chitScale / 4;
+        }
+    }
+
+
+    void setTerrain(char terrain)
+    {
+        this.terrain = terrain;
+    }
 
     char getTerrain()
     {
@@ -151,8 +261,93 @@ class Hex
     }
 
 
+    void setHexside(int i, char hexside)
+    {
+        this.hexside[i] = hexside;
+    }
+
     char getHexside(int i)
     {
-        return hexside[i];
+        if (i < 0 || i > 5)
+        {
+            return ' ';
+        }
+        else
+        {
+            return hexside[i];
+        }
+    }
+
+
+    void setElevation (int elevation)
+    {
+        this.elevation = elevation;
+    }
+
+    int getElevation()
+    {
+        return elevation;
+    }
+
+
+    void setNeighbor(int i, Hex hex)
+    {
+        neighbors[i] = hex;
+    }
+
+    Hex getNeighbor(int i)
+    {
+        if (i < 0 || i > 6)
+        {
+            return null;
+        }
+        else
+        {
+            return neighbors[i];
+        }
+    }
+
+
+    // Return the number of movement points it costs to enter this hex.
+    // For fliers, this is the cost to land in this hex, not fly over it.
+    // If entry is illegal, just return a cost greater than the maximum
+    // possible number of movement points.
+    int getEntryCost(Creature creature, int cameFrom)
+    {
+        // Check to see if the hex is occupied or totally impassable.
+        if (isOccupied() || terrain == 't' || (terrain == 'v' && creature !=
+            Creature.dragon) || (terrain == 'o' && !creature.isNativeBog()))
+        {
+            return 5;
+        }
+
+        char hexside = getHexside(cameFrom);
+
+        // Non-fliers may not cross cliffs.
+        if (hexside == 'c' && !creature.flies())
+        {
+            return 5;
+        }
+
+        // Check for a slowing hexside.
+        if ((hexside == 'w' || (hexside == 's' && !creature.isNativeSlope())) 
+            && !creature.flies() && 
+            elevation > getNeighbor(cameFrom).getElevation())
+        {
+            // All hexes where this applies happen to have no 
+            // additional movement costs.
+            return 2;
+        }
+
+        // Bramble, drift, and sand slow non-natives.
+        if ((terrain == 'r' && !creature.isNativeBramble()) ||
+            (terrain == 'd' && !creature.isNativeDrift()) ||
+            (terrain == 's' && !creature.isNativeSandDune()))
+        {
+            return 2;
+        }
+
+        // Other hexes only cost 1.
+        return 1;
     }
 }
