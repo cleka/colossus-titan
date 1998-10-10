c8bea9a8ab8811d1e551518417c40b0b521b1612
David Ripton <dripton@ripton.net>
1998-10-10 22:21:10 +0000

    Initial revision
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@422 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/PickEntrySide.java
@@ -0,0 +1,568 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * Class PickEntrySide allows picking which side of a MasterBoard hex
+ * to enter.
+ * @version $Id$
+ * @author David Ripton
+ */
+
+class PickEntrySide extends JDialog implements ActionListener, 
+    WindowListener
+{
+    private BattleHex [][] h = new BattleHex[6][6];
+
+    private static final boolean[][] show =
+    {
+        {false,false,true,true,true,false},
+        {false,true,true,true,true,false},
+        {false,true,true,true,true,true},
+        {true,true,true,true,true,true},
+        {false,true,true,true,true,true},
+        {false,true,true,true,true,false}
+    };
+
+    private Image offImage;
+    private Graphics gBack;
+    private Dimension offDimension;
+
+    private static int scale = 30;
+
+    private MasterHex masterHex;
+    private char terrain;
+
+    private JButton button1;  // left
+    private JButton button2;  // bottom
+    private JButton button3;  // right
+
+    private boolean laidOut = false;
+
+
+    public PickEntrySide(JFrame parentFrame, MasterHex masterHex)
+    {
+        super(parentFrame, "Pick entry side", true);
+
+        this.masterHex = masterHex;
+        this.terrain = masterHex.getTerrain();
+
+        getContentPane().setLayout(null);
+
+        if (masterHex.canEnterViaSide(5))
+        {
+            button1 = new JButton("Left");
+            getContentPane().add(button1);
+            button1.addActionListener(this);
+        }
+        
+        if (masterHex.canEnterViaSide(3))
+        {
+            button2 = new JButton("Bottom");
+            getContentPane().add(button2);
+            button2.addActionListener(this);
+        }
+        
+        if (masterHex.canEnterViaSide(1))
+        {
+            button3 = new JButton("Right");
+            getContentPane().add(button3);
+            button3.addActionListener(this);
+        }
+
+        pack();
+        setSize(getPreferredSize());
+        setBackground(java.awt.Color.white);
+
+        addWindowListener(this);
+
+        validate();
+
+        setupHexes();
+
+        setVisible(true);
+
+        repaint();
+    }
+
+    
+    void setupHexes()
+    {
+        int cx = 6 * scale;
+        int cy = 3 * scale;
+
+        // Initialize hexes.
+        for (int i = 0; i < h.length; i++)
+        {
+            for (int j = 0; j < h[0].length; j++)
+            {
+                if (show[i][j])
+                {
+                    h[i][j] = new BattleHex
+                        ((int) Math.round(cx + 3 * i * scale),
+                        (int) Math.round(cy + (2 * j + (i & 1)) *
+                        BattleHex.SQRT3 * scale), scale, null, i, j);
+                }
+            }
+        }
+
+
+        // Add terrain, hexsides, elevation, and exits to hexes.
+        // Cliffs are bidirectional; other hexside obstacles are noted
+        // only on the high side, since they only interfere with
+        // uphill movement.
+        switch (terrain)
+        {
+            case 'P':
+                break;
+
+            case 'W':
+                h[0][2].setTerrain('t');
+                h[2][3].setTerrain('t');
+                h[3][5].setTerrain('t');
+                h[4][1].setTerrain('t');
+                h[4][3].setTerrain('t');
+
+                h[0][2].setElevation(1);
+                h[2][3].setElevation(1);
+                h[3][5].setElevation(1);
+                h[4][1].setElevation(1);
+                h[4][3].setElevation(1);
+                break;
+
+            case 'D':
+                h[0][3].setTerrain('s');
+                h[0][4].setTerrain('s');
+                h[1][3].setTerrain('s');
+                h[3][0].setTerrain('s');
+                h[3][1].setTerrain('s');
+                h[3][2].setTerrain('s');
+                h[3][5].setTerrain('s');
+                h[4][1].setTerrain('s');
+                h[4][2].setTerrain('s');
+                h[4][5].setTerrain('s');
+                h[5][1].setTerrain('s');
+
+                h[0][3].setHexside(0, 'd');
+                h[0][3].setHexside(1, 'd');
+                h[1][3].setHexside(0, 'd');
+                h[1][3].setHexside(1, 'd');
+                h[1][3].setHexside(2, 'd');
+                h[1][3].setHexside(3, 'c');
+                h[3][1].setHexside(4, 'd');
+                h[3][2].setHexside(2, 'd');
+                h[3][2].setHexside(3, 'c');
+                h[3][2].setHexside(4, 'c');
+                h[3][2].setHexside(5, 'd');
+                h[3][5].setHexside(0, 'd');
+                h[3][5].setHexside(5, 'd');
+                h[4][2].setHexside(2, 'd');
+                h[4][2].setHexside(3, 'd');
+                h[4][5].setHexside(0, 'c');
+                h[4][5].setHexside(1, 'd');
+                h[4][5].setHexside(5, 'd');
+                break;
+
+            case 'B':
+                h[0][2].setTerrain('r');
+                h[1][3].setTerrain('r');
+                h[2][2].setTerrain('r');
+                h[3][1].setTerrain('r');
+                h[3][4].setTerrain('r');
+                h[3][5].setTerrain('r');
+                h[4][3].setTerrain('r');
+                h[5][1].setTerrain('r');
+                break;
+
+            case 'J':
+                h[0][3].setTerrain('r');
+                h[1][1].setTerrain('t');
+                h[2][1].setTerrain('r');
+                h[2][3].setTerrain('r');
+                h[2][5].setTerrain('r');
+                h[3][2].setTerrain('r');
+                h[3][3].setTerrain('t');
+                h[4][4].setTerrain('r');
+                h[5][1].setTerrain('r');
+                h[5][2].setTerrain('t');
+
+                h[1][1].setElevation(1);
+                h[3][3].setElevation(1);
+                h[5][2].setElevation(1);
+                break;
+
+            case 'M':
+                h[0][2].setTerrain('o');
+                h[2][3].setTerrain('o');
+                h[2][4].setTerrain('o');
+                h[3][1].setTerrain('o');
+                h[4][3].setTerrain('o');
+                h[4][5].setTerrain('o');
+                break;
+
+            case 'S':
+                h[1][3].setTerrain('o');
+                h[2][1].setTerrain('o');
+                h[2][2].setTerrain('t');
+                h[2][4].setTerrain('t');
+                h[3][3].setTerrain('o');
+                h[3][5].setTerrain('o');
+                h[4][2].setTerrain('t');
+                h[5][3].setTerrain('o');
+
+                h[2][2].setElevation(1);
+                h[2][4].setElevation(1);
+                h[4][2].setElevation(1);
+                break;
+
+            case 'H':
+                h[2][2].setTerrain('t');
+                h[2][4].setTerrain('t');
+                h[5][3].setTerrain('t');
+
+                h[1][2].setElevation(1);
+                h[1][4].setElevation(1);
+                h[2][2].setElevation(1);
+                h[2][4].setElevation(1);
+                h[3][0].setElevation(1);
+                h[3][4].setElevation(1);
+                h[4][3].setElevation(1);
+                h[5][3].setElevation(1);
+
+                h[1][2].setHexside(0, 's');
+                h[1][2].setHexside(1, 's');
+                h[1][2].setHexside(2, 's');
+                h[1][2].setHexside(3, 's');
+                h[1][2].setHexside(4, 's');
+                h[1][2].setHexside(5, 's');
+                h[1][4].setHexside(0, 's');
+                h[1][4].setHexside(1, 's');
+                h[1][4].setHexside(2, 's');
+                h[1][4].setHexside(5, 's');
+                h[3][0].setHexside(2, 's');
+                h[3][0].setHexside(3, 's');
+                h[3][0].setHexside(4, 's');
+                h[3][4].setHexside(0, 's');
+                h[3][4].setHexside(1, 's');
+                h[3][4].setHexside(2, 's');
+                h[3][4].setHexside(3, 's');
+                h[3][4].setHexside(4, 's');
+                h[3][4].setHexside(5, 's');
+                h[4][3].setHexside(0, 's');
+                h[4][3].setHexside(1, 's');
+                h[4][3].setHexside(2, 's');
+                h[4][3].setHexside(3, 's');
+                h[4][3].setHexside(4, 's');
+                h[4][3].setHexside(5, 's');
+                break;
+
+            case 'm':
+                h[3][2].setTerrain('v');
+
+                h[0][4].setElevation(1);
+                h[1][1].setElevation(1);
+                h[1][3].setElevation(1);
+                h[1][4].setElevation(2);
+                h[2][1].setElevation(2);
+                h[2][2].setElevation(1);
+                h[2][5].setElevation(1);
+                h[3][0].setElevation(2);
+                h[3][1].setElevation(1);
+                h[3][2].setElevation(2);
+                h[3][3].setElevation(1);
+                h[4][1].setElevation(1);
+                h[4][2].setElevation(1);
+                h[4][3].setElevation(1);
+                h[5][1].setElevation(2);
+                h[5][2].setElevation(1);
+                h[5][3].setElevation(2);
+                h[5][4].setElevation(1);
+
+                h[0][4].setHexside(0, 's');
+                h[1][1].setHexside(3, 's');
+                h[1][1].setHexside(4, 's');
+                h[1][3].setHexside(0, 's');
+                h[1][3].setHexside(1, 's');
+                h[1][3].setHexside(2, 's');
+                h[1][3].setHexside(5, 's');
+                h[1][4].setHexside(0, 's');
+                h[1][4].setHexside(1, 'c');
+                h[1][4].setHexside(2, 's');
+                h[1][4].setHexside(5, 's');
+                h[2][1].setHexside(2, 's');
+                h[2][1].setHexside(3, 's');
+                h[2][1].setHexside(4, 's');
+                h[2][2].setHexside(3, 's');
+                h[2][2].setHexside(4, 's');
+                h[2][5].setHexside(0, 's');
+                h[2][5].setHexside(1, 's');
+                h[2][5].setHexside(2, 's');
+                h[3][0].setHexside(2, 's');
+                h[3][0].setHexside(3, 's');
+                h[3][2].setHexside(0, 's');
+                h[3][2].setHexside(1, 's');
+                h[3][2].setHexside(2, 's');
+                h[3][2].setHexside(3, 's');
+                h[3][2].setHexside(4, 'c');
+                h[3][2].setHexside(5, 's');
+                h[3][3].setHexside(2, 's');
+                h[3][3].setHexside(3, 's');
+                h[3][3].setHexside(4, 's');
+                h[3][3].setHexside(5, 's');
+                h[4][3].setHexside(3, 's');
+                h[5][1].setHexside(3, 's');
+                h[5][1].setHexside(4, 's');
+                h[5][1].setHexside(5, 's');
+                h[5][3].setHexside(0, 's');
+                h[5][3].setHexside(3, 's');
+                h[5][3].setHexside(4, 'c');
+                h[5][3].setHexside(5, 's');
+                h[5][4].setHexside(4, 's');
+                h[5][4].setHexside(5, 's');
+                break;
+
+            case 't':
+                h[0][4].setTerrain('d');
+                h[1][3].setTerrain('d');
+                h[2][1].setTerrain('d');
+                h[2][2].setTerrain('d');
+                h[2][4].setTerrain('d');
+                h[3][3].setTerrain('d');
+                h[4][2].setTerrain('d');
+                h[4][5].setTerrain('d');
+                h[5][3].setTerrain('d');
+                break;
+
+            case 'T':
+                h[2][2].setElevation(1);
+                h[2][3].setElevation(1);
+                h[3][1].setElevation(1);
+                h[3][2].setElevation(2);
+                h[3][3].setElevation(1);
+                h[4][2].setElevation(1);
+                h[4][3].setElevation(1);
+
+                h[2][2].setHexside(0, 'w');
+                h[2][2].setHexside(4, 'w');
+                h[2][2].setHexside(5, 'w');
+                h[2][3].setHexside(3, 'w');
+                h[2][3].setHexside(4, 'w');
+                h[2][3].setHexside(5, 'w');
+                h[3][1].setHexside(0, 'w');
+                h[3][1].setHexside(1, 'w');
+                h[3][1].setHexside(5, 'w');
+                h[3][2].setHexside(0, 'w');
+                h[3][2].setHexside(1, 'w');
+                h[3][2].setHexside(2, 'w');
+                h[3][2].setHexside(3, 'w');
+                h[3][2].setHexside(4, 'w');
+                h[3][2].setHexside(5, 'w');
+                h[3][3].setHexside(2, 'w');
+                h[3][3].setHexside(3, 'w');
+                h[3][3].setHexside(4, 'w');
+                h[4][2].setHexside(0, 'w');
+                h[4][2].setHexside(1, 'w');
+                h[4][2].setHexside(2, 'w');
+                h[4][3].setHexside(1, 'w');
+                h[4][3].setHexside(2, 'w');
+                h[4][3].setHexside(3, 'w');
+                break;
+        }
+
+
+        // Add references to neighbor hexes.
+        for (int i = 0; i < h.length; i++)
+        {
+            for (int j = 0; j < h[0].length; j++)
+            {
+                if (show[i][j])
+                {
+                    if (j > 0 && show[i][j - 1])
+                    {
+                        h[i][j].setNeighbor(0, h[i][j - 1]);
+                    }
+
+                    if (i < 5 && show[i + 1][j - ((i + 1) & 1)])
+                    {
+                        h[i][j].setNeighbor(1, h[i + 1][j - ((i + 1) & 1)]);;
+                    }
+
+                    if (i < 5 && j + (i & 1) < 6 && show[i + 1][j + (i & 1)])
+                    {
+                        h[i][j].setNeighbor(2, h[i + 1][j + (i & 1)]);
+                    }
+
+                    if (j < 5 && show[i][j + 1])
+                    {
+                        h[i][j].setNeighbor(3, h[i][j + 1]);
+                    }
+
+                    if (i > 0 && j + (i & 1) < 6 && show[i - 1][j + (i & 1)])
+                    {
+                        h[i][j].setNeighbor(4, h[i - 1][j + (i & 1)]);
+                    }
+
+                    if (i > 0 && show[i - 1][j - ((i + 1) & 1)])
+                    {
+                        h[i][j].setNeighbor(5, h[i - 1][j - ((i + 1) & 1)]);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void update(Graphics g)
+    {
+        Dimension d = getSize();
+        Rectangle rectClip = g.getClipBounds();
+        
+        // Create the back buffer only if we don't have a good one.
+        if (gBack == null || d.width != offDimension.width ||
+            d.height != offDimension.height)
+        {
+            offDimension = d;
+            offImage = createImage(2 * d.width, 2 * d.height);
+            gBack = offImage.getGraphics();
+        }
+
+        for (int i = 0; i < h.length; i++)
+        {
+            for (int j = 0; j < h[0].length; j++)
+            {
+                if (show[i][j] && rectClip.intersects(h[i][j].getBounds()))
+                {
+                    h[i][j].paint(gBack);
+                }
+            }
+        }
+
+        if (!laidOut)
+        {
+            if (button1 != null)
+            {
+                button1.setBounds(d.width / 7,
+                    7 * d.height / 8,
+                    d.width / 7, d.height / 16);
+            }
+            if (button2 != null)
+            {
+                button2.setBounds(3 * d.width / 7,
+                    7 * d.height / 8,
+                    d.width / 7, d.height / 16);
+            }
+            if (button3 != null)
+            {
+                button3.setBounds(5 * d.width / 7,
+                    7 * d.height / 8,
+                    d.width / 7, d.height / 16);
+            }
+
+            laidOut = true;
+        }
+
+        g.drawImage(offImage, 0, 0, this);
+
+        if (button1 != null)
+        {
+            button1.repaint();
+        }
+        if (button2 != null)
+        {
+            button2.repaint();
+        }
+        if (button3 != null)
+        {
+            button3.repaint();
+        }
+    }
+
+
+    public void paint(Graphics g)
+    {
+        // Double-buffer everything.
+        update(g);
+    }
+
+
+    public Dimension getMinimumSize()
+    {
+        return getPreferredSize();
+    }
+
+
+    public Dimension getPreferredSize()
+    {
+        return new Dimension(30 * scale, 30 * scale);
+    }
+
+
+    // Set hex's entry side to side, and then exit the dialog.  If side
+    // is -1, then do not set an entry side, which will abort the move.
+    private void cleanup(int side)
+    {
+        masterHex.clearAllEntrySides();
+
+        if (1 <= side && side <= 5)
+        {
+            masterHex.setEntrySide(side);
+        }
+        dispose();
+    }
+
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getActionCommand() == "Left")
+        {
+            cleanup(5);
+        }
+
+        else if (e.getActionCommand() == "Right")
+        {
+            cleanup(1);
+        }
+
+        else if (e.getActionCommand() == "Bottom")
+        {
+            cleanup(3);
+        }
+    }
+
+
+    public void windowActivated(WindowEvent e)
+    {
+    }
+
+
+    public void windowOpened(WindowEvent e)
+    {
+    }
+
+
+    public void windowClosed(WindowEvent e)
+    {
+    }
+
+
+    public void windowClosing(WindowEvent e)
+    {
+        // Abort the move.
+        cleanup(-1);
+    }
+
+
+    public void windowDeactivated(WindowEvent e)
+    {
+    }
+
+
+    public void windowIconified(WindowEvent e)
+    {
+    }
+
+
+    public void windowDeiconified(WindowEvent e)
+    {
+    }
+}
