96f1df33e4ab6b96567742bcd06acf91acb766cd
David Ripton <dripton@ripton.net>
1999-11-19 23:22:26 +0000

    Call Exception.toString() in catch blocks. Use e as the formal argument for all event handlers.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@620 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/PickRecruiter.java
+++ Colossus/PickRecruiter.java
@@ -97,7 +97,8 @@ class PickRecruiter extends Dialog implements MouseListener, WindowListener
         }
         catch (InterruptedException e)
         {
-            new MessageBox(parentFrame, "waitForAll was interrupted");
+            new MessageBox(parentFrame, e.toString() + 
+                " waitForAll was interrupted");
         }
         imagesLoaded = true;
 
@@ -190,38 +191,38 @@ class PickRecruiter extends Dialog implements MouseListener, WindowListener
     }
 
 
-    public void windowActivated(WindowEvent event)
+    public void windowActivated(WindowEvent e)
     {
     }
 
 
-    public void windowClosed(WindowEvent event)
+    public void windowClosed(WindowEvent e)
     {
     }
 
 
-    public void windowClosing(WindowEvent event)
+    public void windowClosing(WindowEvent e)
     {
         dispose();
     }
 
 
-    public void windowDeactivated(WindowEvent event)
+    public void windowDeactivated(WindowEvent e)
     {
     }
 
 
-    public void windowDeiconified(WindowEvent event)
+    public void windowDeiconified(WindowEvent e)
     {
     }
 
 
-    public void windowIconified(WindowEvent event)
+    public void windowIconified(WindowEvent e)
     {
     }
 
 
-    public void windowOpened(WindowEvent event)
+    public void windowOpened(WindowEvent e)
     {
     }
 }
