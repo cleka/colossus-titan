23eeb5ac62b3a46c7638f920ed3e87545953aee6
David Ripton <dripton@ripton.net>
2000-01-19 19:48:41 +0000

    Initial revision
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@632 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/PenaltyOption.java
@@ -0,0 +1,65 @@
+/**
+ * Class PenaltyOption holds the information needed to decide whether to take 
+ * a strike penalty, for one chit.
+ * @version $Id$
+ * @author David Ripton
+ */
+
+class PenaltyOption
+{
+    private BattleChit chit;
+    private int dice;
+    private int strikeNumber;
+
+
+    PenaltyOption(BattleChit chit, int dice, int strikeNumber)
+    {
+        this.chit = chit;
+        this.dice = dice;
+        this.strikeNumber = strikeNumber;
+    }
+
+
+    public BattleChit getChit()
+    {
+        return chit;
+    }
+    
+    
+    public int getDice()
+    {
+        return dice;
+    }
+    
+    
+    public int getStrikeNumber()
+    {
+        return strikeNumber;
+    }
+
+
+    // Sort first by ascending dice, then by descending strike number.
+    public int compareTo(PenaltyOption other)
+    {
+        if (dice < other.getDice())
+        {
+            return -1;
+        }
+        else if (dice > other.getDice())
+        {
+            return 1;
+        }
+        else if (strikeNumber > other.getStrikeNumber())
+        {
+            return -1;
+        }
+        else if (strikeNumber < other.getStrikeNumber())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
