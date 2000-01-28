5c7de63949b1e69486cf8efe2c7c42534053410e
David Ripton <dripton@ripton.net>
2000-01-28 00:46:53 +0000

    Add isApplet argument to constructor.  Load images using    the buggy-browser-friendly method (from David Barr)    if it's set, or the executable-jar-friendly method    if it's not.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@635 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/Chit.java
+++ Colossus/Chit.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.io.*;
 
 /**
  * Class Chit implements the GUI for a Titan chit representing
@@ -13,7 +14,6 @@ class Chit
     private boolean selected = false;
     private Rectangle rect;
     private static Container container;
-    public static final double DEG_TO_RAD = Math.PI / 180;
 
     // Offset of the mouse cursor within the chit.
     private int dx = 0;
@@ -24,13 +24,38 @@ class Chit
 
 
     Chit(int cx, int cy, int scale, String imageFilename,
-        Container container)
+        Container container, boolean isApplet)
     {
         rect = new Rectangle(cx, cy, scale, scale);
         this.container = container;
 
-        image = Toolkit.getDefaultToolkit().getImage(
-            getClass().getResource(imageFilename));
+        // The image-loading syntax that works correctly for applications
+        // packaged in executable jar files does not work correctly for 
+        // applets, and vice-versa.
+
+        if (isApplet)
+        {
+            InputStream in;
+            byte[] thanksToNetscape = null; 
+  
+            try
+            {
+                in = getClass().getResourceAsStream(imageFilename);
+                int length = in.available();
+                thanksToNetscape = new byte[length];
+                in.read(thanksToNetscape);
+                image = Toolkit.getDefaultToolkit().createImage(thanksToNetscape);
+            }
+            catch (Exception e)
+            {
+                System.out.println("couldn't load image " + imageFilename + "\n" + e);
+            }
+        }
+        else
+        {
+            image = Toolkit.getDefaultToolkit().getImage(
+                getClass().getResource(imageFilename));
+        }
     }
 
 
