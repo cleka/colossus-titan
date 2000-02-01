6cd8c7858f2459397e45130a2c6c237fcdac61cb
David Ripton <dripton@ripton.net>
2000-02-01 21:40:33 +0000

    Rename friend to ally, to avoid annoying C++ highlighting in gvim. Use GridBagLayout instead of absolute positioning.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@662 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/Concede.java
+++ Colossus/Concede.java
@@ -13,84 +13,91 @@ class Concede extends Dialog implements ActionListener
     private boolean imagesLoaded = false;
     private static final int scale = 60;
     private Frame parentFrame;
-    private Button button1;
-    private Button button2;
-    private boolean laidOut = false;
     private boolean flee;
-    private Legion friend;
+    private Legion ally;
     private Legion enemy;
-    private Chit [] friendChits;
+    private Chit [] allyChits;
     private Chit [] enemyChits;
-    private Chit friendMarker;
+    private Chit allyMarker;
     private Chit enemyMarker;
     private Graphics offGraphics;
     private Dimension offDimension;
     private Image offImage;
     private static Point location;
+    private GridBagLayout gridbag = new GridBagLayout();
+    private GridBagConstraints constraints = new GridBagConstraints();
 
 
-    Concede(Frame parentFrame, Legion friend, Legion enemy, boolean flee)
+    Concede(Frame parentFrame, Legion ally, Legion enemy, boolean flee)
     {
-        super(parentFrame, friend.getPlayer().getName() + ": " + (flee ?
-            "Flee" : "Concede") + " with Legion "  + friend.getMarkerId() +
-            " in " + friend.getCurrentHex().getTerrainName().toLowerCase() +
+        super(parentFrame, ally.getPlayer().getName() + ": " + (flee ?
+            "Flee" : "Concede") + " with Legion "  + ally.getMarkerId() +
+            " in " + ally.getCurrentHex().getTerrainName().toLowerCase() +
             "?", true);
 
-        setLayout(null);
+        setLayout(gridbag);
 
         this.parentFrame = parentFrame;
-        this.friend = friend;
+        this.ally = ally;
         this.enemy = enemy;
         this.flee = flee;
 
         pack();
 
         setBackground(Color.lightGray);
-        setSize(getPreferredSize());
         setResizable(false);
 
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 
-        if (location == null)
-        {
-            location = new Point(d.width / 2 - getSize().width / 2, d.height / 2
-                - getSize().height / 2);
-        }
-        setLocation(location);
+        allyMarker = new Marker(-1, -1, scale, ally.getImageName(), this, 
+            ally);
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        gridbag.setConstraints(allyMarker, constraints);
+        add(allyMarker);
 
         // Leave space for angels.
-        friendChits = new Chit[7];
-        for (int i = 0; i < friend.getHeight(); i++)
+        allyChits = new Chit[7];
+        for (int i = 0; i < ally.getHeight(); i++)
         {
-            friendChits[i] = new Chit((i + 1) * scale + (scale / 5), scale / 2,
-                scale, friend.getCritter(i).getImageName(), this);
+            allyChits[i] = new Chit(-1, -1, scale, 
+                ally.getCritter(i).getImageName(), this);
+            constraints.gridy = 0;
+            constraints.gridwidth = 1;
+            gridbag.setConstraints(allyChits[i], constraints);
+            add(allyChits[i]);
         }
+
+        enemyMarker = new Marker(-1, -1, scale, enemy.getImageName(), 
+            this, enemy);
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        gridbag.setConstraints(enemyMarker, constraints);
+        add(enemyMarker);
 
         // Leave space for angels.
         enemyChits = new Chit[7];
         for (int i = 0; i < enemy.getHeight(); i++)
         {
-            enemyChits[i] = new Chit((i + 1) * scale + (scale / 5),
-                2 * scale, scale, enemy.getCritter(i).getImageName(), this);
+            enemyChits[i] = new Chit(-1, -1, scale, 
+                enemy.getCritter(i).getImageName(), this);
+            constraints.gridy = 1;
+            constraints.gridwidth = 1;
+            gridbag.setConstraints(enemyChits[i], constraints);
+            add(enemyChits[i]);
         }
-
-        friendMarker = new Marker(scale / 5, scale / 2, scale,
-            friend.getImageName(), this, friend);
-
-        enemyMarker = new Marker(scale / 5, 2 * scale, scale,
-            enemy.getImageName(), this, enemy);
 
         tracker = new MediaTracker(this);
 
-        for (int i = 0; i < friend.getHeight(); i++)
+        for (int i = 0; i < ally.getHeight(); i++)
         {
-            tracker.addImage(friendChits[i].getImage(), 0);
+            tracker.addImage(allyChits[i].getImage(), 0);
         }
         for (int i = 0; i < enemy.getHeight(); i++)
         {
             tracker.addImage(enemyChits[i].getImage(), 0);
         }
-        tracker.addImage(friendMarker.getImage(), 0);
+        tracker.addImage(allyMarker.getImage(), 0);
         tracker.addImage(enemyMarker.getImage(), 0);
 
         try
@@ -104,12 +111,45 @@ class Concede extends Dialog implements ActionListener
         }
         imagesLoaded = true;
 
-        button1 = new Button(flee ? "Flee" : "Concede");
-        button2 = new Button(flee ? "Don't Flee" : "Don't Concede");
+        Button button1 = new Button(flee ? "Flee" : "Concede");
+        Button button2 = new Button(flee ? "Don't Flee" : "Don't Concede");
+
+        // Attempt to center the buttons.
+        int chitWidth = Math.max(ally.getHeight(), enemy.getHeight()) + 1;
+        if (chitWidth < 4)
+        {
+            constraints.gridwidth = 1;
+        }
+        else
+        {
+            constraints.gridwidth = 2;
+        }
+        int leadSpace = (chitWidth - 2 * constraints.gridwidth) / 2; 
+        if (leadSpace < 0)
+        {
+            leadSpace = 0;
+        }
+
+        constraints.gridy = 2;
+        constraints.gridx = leadSpace;
+        gridbag.setConstraints(button1, constraints);
         add(button1);
-        add(button2);
         button1.addActionListener(this);
+        constraints.gridx = leadSpace + constraints.gridwidth; 
+        gridbag.setConstraints(button2, constraints);
+        add(button2);
         button2.addActionListener(this);
+
+        pack();
+        
+        // Initially, center the dialog on the screen.  Save the
+        // location for future invocations.
+        if (location == null)
+        {
+            location = new Point(d.width / 2 - getSize().width / 2, 
+                d.height / 2 - getSize().height / 2);
+        }
+        setLocation(location);
 
         setVisible(true);
         repaint();
@@ -153,37 +193,6 @@ class Concede extends Dialog implements ActionListener
             offGraphics = offImage.getGraphics();
         }
 
-        friendMarker.paint(offGraphics);
-
-        enemyMarker.paint(offGraphics);
-
-        for (int i = friend.getHeight() - 1; i >= 0; i--)
-        {
-            // If the stack just acquired an angel, it won't be painted.
-            if (friendChits[i] != null)
-            {
-                friendChits[i].paint(offGraphics);
-            }
-        }
-        for (int i = enemy.getHeight() - 1; i >= 0; i--)
-        {
-            // If the stack just acquired an angel, it won't be painted.
-            if (enemyChits[i] != null)
-            {
-                enemyChits[i].paint(offGraphics);
-            }
-        }
-
-        if (!laidOut)
-        {
-            Insets insets = getInsets();
-            button1.setBounds(insets.left + d.width / 9, 13 * d.height / 16 -
-                insets.bottom, d.width / 3, d.height / 8);
-            button2.setBounds(5 * d.width / 9 - insets.right,
-                13 * d.height / 16 - insets.bottom, d.width / 3, d.height / 8);
-            laidOut = true;
-        }
-
         g.drawImage(offImage, 0, 0, this);
     }
 
@@ -201,14 +210,14 @@ class Concede extends Dialog implements ActionListener
             e.getActionCommand().equals("Concede"))
         {
             // Figure how many points the victor receives.
-            int points = friend.getPointValue();
+            int points = ally.getPointValue();
             if (flee)
             {
                 points /= 2;
             }
 
             // Remove the dead legion.
-            friend.removeLegion();
+            ally.removeLegion();
 
             // Add points, and angels if necessary.
             enemy.addPoints(points);
@@ -217,9 +226,9 @@ class Concede extends Dialog implements ActionListener
 
             // If this was the titan stack, its owner dies and gives half
             // points to the victor.
-            if (friend.numCreature(Creature.titan) == 1)
+            if (ally.numCreature(Creature.titan) == 1)
             {
-                friend.getPlayer().die(enemy.getPlayer(), true);
+                ally.getPlayer().die(enemy.getPlayer(), true);
             }
 
             // Exit this dialog.
@@ -235,17 +244,5 @@ class Concede extends Dialog implements ActionListener
         {
             cleanup();
         }
-    }
-
-
-    public Dimension getMinimumSize()
-    {
-        return getPreferredSize();
-    }
-
-    public Dimension getPreferredSize()
-    {
-        return new Dimension(scale * (Math.max(friend.getHeight(),
-            enemy.getHeight()) + 2), 4 * scale);
     }
 }
