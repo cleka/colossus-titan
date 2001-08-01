aad379b8c2d8b6b5db403bffa3bc7b10c0796cb4
Tom Fruchterman <tfruchterman@users.sf.net>
2001-08-01 05:34:31 +0000

    Utility class for holding GameListeners and firing events
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@892 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/net/sf/colossus/protocol/GameSource.java
@@ -0,0 +1,83 @@
+
+package net.sf.colossus.protocol;
+
+/**
+ * Utility class to manage event stuff
+ */
+
+import java.util.*;
+
+public class GameSource
+{
+	private ArrayList oListenerArrayList = new ArrayList();
+
+	public void addGameListener(GameListener oListener)
+		{
+			oListenerArrayList.add(oListener);
+		}
+
+	public void removeGameListener(GameListener oListener)
+		{
+			oListenerArrayList.remove(oListener);
+		}
+
+	public void fireEvent(GameEvent evt)
+		{
+			Iterator oListeners = oListenerArrayList.iterator();
+			while(oListeners.hasNext())
+			{
+				GameListener oListener = (GameListener) oListeners.next();
+				oListener.requestDenied(evt);
+			}
+		}
+
+	public void fireEvent(PlayerEvent evt)
+		{
+			Iterator oListeners = oListenerArrayList.iterator();
+			while(oListeners.hasNext())
+			{
+				GameListener oListener = (GameListener) oListeners.next();
+				oListener.playerChanged(evt);
+			}
+		}
+
+	public void fireEvent(LegionEvent evt)
+		{
+			Iterator oListeners = oListenerArrayList.iterator();
+			while(oListeners.hasNext())
+			{
+				GameListener oListener = (GameListener) oListeners.next();
+				oListener.legionChange(evt);
+			}
+		}
+	public void fireEvent(MasterBoardEvent evt)
+		{
+			Iterator oListeners = oListenerArrayList.iterator();
+			while(oListeners.hasNext())
+			{
+				GameListener oListener = (GameListener) oListeners.next();
+				oListener.masterBoardMove(evt);
+			}
+		}
+
+	public void fireEvent(BattleBoardEvent evt)
+		{
+			Iterator oListeners = oListenerArrayList.iterator();
+			while(oListeners.hasNext())
+			{
+				GameListener oListener = (GameListener) oListeners.next();
+				oListener.battleBoardMove(evt);
+			}
+		}
+
+	public void fireEvent(MessageEvent evt)
+		{
+			Iterator oListeners = oListenerArrayList.iterator();
+			while(oListeners.hasNext())
+			{
+				GameListener oListener = (GameListener) oListeners.next();
+				oListener.messageSent(evt);
+			}
+		}
+
+}
