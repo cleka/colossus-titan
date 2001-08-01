b2e9bdf41b13ec0b079473de3c13717564bfa336
Tom Fruchterman <tfruchterman@users.sf.net>
2001-08-01 05:36:16 +0000

    listener interface for game events
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@893 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/net/sf/colossus/protocol/GameListener.java
@@ -0,0 +1,12 @@
+
+package net.sf.colossus.protocol;
+
+public interface GameListener
+{
+	public void requestDenied(GameEvent evt);
+	public void playerChanged(PlayerEvent evt);
+	public void legionChange(LegionEvent evt);
+	public void masterBoardMove(MasterBoardEvent evt);
+	public void battleBoardMove(BattleBoardEvent evt);
+	public void messageSent(MessageEvent evt);
+}
