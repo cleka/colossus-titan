70acc898c94358a67b496eb4d0c9242967154a46
Tom Fruchterman <tfruchterman@users.sf.net>
2001-09-19 20:33:58 +0000

    Specify which hex terrain the battle will happen in. Still needs to set the entry sides.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@911 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/net/sf/colossus/battle/HexEditor.java
@@ -0,0 +1,93 @@
+
+package net.sf.colossus.battle;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import net.sf.colossus.*;
+import net.sf.colossus.masterboard.*;
+import java.util.*;
+
+
+class HexEditor extends JPanel
+{
+    static Hashtable s_oTerrainHash = new Hashtable();
+    static String[] s_strTerrainArray = 
+    {
+        "Brush",
+        "Desert",
+        "Hills",
+        "Mountains",
+        "Marsh",
+        "Plains",
+        "Swamp",
+        "Tower",
+        "Tundra",
+        "Woods",
+    };
+
+
+    static
+    {
+        s_oTerrainHash.put("Brush", "B");
+        s_oTerrainHash.put("Desert", "D");
+        s_oTerrainHash.put("Hills", "H");
+        s_oTerrainHash.put("Mountains", "m");
+        s_oTerrainHash.put("Marsh", "M");
+        s_oTerrainHash.put("Plains", "P");
+        s_oTerrainHash.put("Swamp", "S");
+        s_oTerrainHash.put("Tower", "T");
+        s_oTerrainHash.put("Tundra", "t");
+        s_oTerrainHash.put("Woods", "W");
+    }
+
+    JComboBox m_oTerrainChoice;
+    JLabel m_oHexLabel;
+
+    public char getTerrain()
+    {
+        String strTerrain = (String)
+            s_oTerrainHash.get(m_oTerrainChoice.getSelectedItem());
+        return strTerrain.charAt(0);
+    }
+
+    public String getLabel()
+    {
+        return m_oHexLabel.getText();
+    }
+
+    private void setLabel(String strLabel)
+    {
+        m_oHexLabel.setText(strLabel);
+    }
+
+    private void fixHexId()
+    {
+        String strTerrain = m_oTerrainChoice.getSelectedItem().toString();
+        char chTerrain = s_oTerrainHash.get(strTerrain).toString().charAt(0);
+        // HERE -- must have some way to get to the labels
+        MasterHexFixed oHex = MasterBoardFixed.getAnyHexWithTerrain(chTerrain);
+        String strHexLabel = oHex.getLabel();
+        setLabel(strHexLabel);
+    }
+    
+    HexEditor()
+    {
+        m_oTerrainChoice = new JComboBox(s_strTerrainArray);
+        add(m_oTerrainChoice);
+        m_oHexLabel = new JLabel("");
+        add(m_oHexLabel);
+
+        fixHexId();
+
+        m_oTerrainChoice.addItemListener(new ItemListener()
+            {
+                public void itemStateChanged(ItemEvent evt)
+                {
+                    fixHexId();
+                }
+            }
+            );
+    }
+}
