dc10b68baf0aa1d23219c2f0eff53e31b78fcfb0
Romain Dolbeau <romain@dolbeau.org>
2002-01-23 12:42:06 +0000

    credits
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1191 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/client/GUIBattleHex.java
+++ Colossus/net/sf/colossus/client/GUIBattleHex.java
@@ -14,6 +14,7 @@ import net.sf.colossus.util.Log;
  * Class GUIBattleHex holds GUI info for one battle hex.
  * @version $Id$
  * @author David Ripton
+ * @author Romain Dolbeau
  */
 
 public class GUIBattleHex extends BattleHex
