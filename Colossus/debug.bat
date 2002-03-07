606c475d5527f8e82738e720b52fb31974814014
David Ripton <dripton@ripton.net>
2002-03-07 19:43:36 +0000

    Handle up to 9 arguments instead of 5, since we're adding command-line options.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1496 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/debug.bat
+++ Colossus/debug.bat
@@ -1,2 +1,2 @@
 @echo off
-java com.bluemarsh.jswat.Main net.sf.colossus.server.Start %1 %2 %3 %4 %5
+java com.bluemarsh.jswat.Main net.sf.colossus.server.Start %1 %2 %3 %4 %5 %6 %7 %8 %9
--- Colossus/run.bat
+++ Colossus/run.bat
@@ -1,1 +1,2 @@
-java -jar Colossus.jar %1 %2 %3 %4 %5
+@echo off
+java -jar Colossus.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
