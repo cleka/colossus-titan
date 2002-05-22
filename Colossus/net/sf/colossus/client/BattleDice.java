815fe6940380b029ebd898163e01c009ac7f59bb
Romain Dolbeau <romain@dolbeau.org>
2002-05-22 15:19:31 +0000

    redo the BattleDice display, including colored boxes for 'average' results
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1704 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/client/BattleDice.java
+++ Colossus/net/sf/colossus/client/BattleDice.java
@@ -5,12 +5,14 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
+import javax.swing.border.*;
+import net.sf.colossus.util.HTMLColor;
 
 /**
  * Class BattleDice displays dice rolls during a battle.
  * @version $Id$
  * @author David Ripton
+ * @author Romain Dolbeau
  */
 
 
@@ -24,13 +26,33 @@ final class BattleDice extends JPanel
     private int hits = 0;
     private JLabel label1 = new JLabel();
     private Chit [] dice;
+    private int averageMiss = -1;
 
+    private JPanel diceBox, missBox, hitBox;
+    private TitledBorder diceBoxTitledBorder;
 
     BattleDice()
     {
         setVisible(false);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.lightGray);
+
+        diceBox = new JPanel();
+        diceBox.setLayout(new FlowLayout());
+        diceBoxTitledBorder = new TitledBorder("");
+        diceBoxTitledBorder.setTitleJustification(TitledBorder.CENTER);
+        diceBox.setBorder(diceBoxTitledBorder);
+        add(diceBox);
+        
+        missBox = new JPanel();
+        hitBox = new JPanel();
+        missBox.setLayout(new FlowLayout());
+        missBox.setBorder(new LineBorder(HTMLColor.blue));
+        hitBox.setBorder(new LineBorder(HTMLColor.red));
+        hitBox.setLayout(new FlowLayout());
+        diceBox.add(missBox);
+        diceBox.add(hitBox);
+
         showRoll();
     }
 
@@ -50,8 +72,12 @@ final class BattleDice extends JPanel
             Collections.sort(this.rolls);
         }
         numDice = this.rolls.size();
-    }
 
+        // for average miss number, let's assume 6-sided roll :-)
+        float floatAM = (numDice * (targetNumber - 1)) / 6;
+
+        averageMiss = Math.round(floatAM);
+    }
 
     private String getDieImageName(String rollString)
     {
@@ -83,33 +109,46 @@ final class BattleDice extends JPanel
     void showRoll()
     {
         setVisible(false);
-        removeAll();
+        hitBox.removeAll();
+        hitBox.setVisible(false);
+        missBox.removeAll();
+        missBox.setVisible(false);
 
         if (strikerDesc.equals(""))
         {
-            label1.setText("");
+            diceBoxTitledBorder.setTitle("Attack results");
         }
         else
         {
-            label1.setText(strikerDesc + " attacks " + targetDesc); 
+            diceBoxTitledBorder.setTitle(strikerDesc +
+                                         " attacks " +
+                                         targetDesc +
+                                         " (target number is " +
+                                         targetNumber + ")");
         }
-        label1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(label1);
 
-        JPanel diceBox = new JPanel();
-        diceBox.setLayout(new FlowLayout());
-        add(diceBox);
         dice = new Chit[numDice];
+
         for (int i = 0; i < numDice; i++)
         {
             String imageName = getDieImageName((String)rolls.get(i));
             if (imageName != null)
             {
                 dice[i] = new Chit(2 * Scale.get(), imageName, this);
-                diceBox.add(dice[i]);
+                if (averageMiss > i)
+                    missBox.add(dice[i]);
+                else
+                    hitBox.add(dice[i]);
             }
         }
 
+        if (numDice > 0)
+        {
+            hitBox.setVisible(true);
+            missBox.setVisible(true);
+        }
+
+        invalidate();
         setVisible(true);
         repaint();
     }
