b44fa24911f2fc20d35a86291ca29540ef572707
David Ripton <dripton@ripton.net>
2002-07-01 02:11:38 +0000

    Seed Dice.Random using /dev/random if present.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1750 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/server/Dice.java
+++ Colossus/net/sf/colossus/server/Dice.java
@@ -2,6 +2,9 @@ package net.sf.colossus.server;
 
 
 import java.util.*;
+import java.io.*;
+
+import net.sf.colossus.util.Log;
 
 
 /**
@@ -13,6 +16,10 @@ import java.util.*;
 public final class Dice
 {
     private static Random random = new Random();
+    /** If you're *really* paranoid, change to urandom. */
+    private static String randomFilename = "/dev/random";
+    private static File randomSource = new File(randomFilename);
+    private static FileInputStream randStream = null;
     
     
     /** Put all die rolling in one place, in case we decide to change random
@@ -29,7 +36,43 @@ public final class Dice
             Log.error("Looks like someone is trying to cheat");
             return 0;
         }
+        if (randomSource.exists())
+        {
+            if (randStream == null)
+            {
+                try
+                {
+                    randStream = new FileInputStream(randomSource);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Log.error("/dev/random disappeared!");
+                    System.exit(1);
+                }
+            }
+            byte [] bytes = new byte[8];
+            try
+            {
+                randStream.read(bytes);
+            }
+            catch (IOException ex)
+            {
+                Log.error("Problem reading from /dev/random");
+            }
+            random.setSeed(eightBytesToLong(bytes));
+        }
         return random.nextInt(size) + 1;
+    }
+
+    /** Generate a long from an array of eight bytes. */
+    private static long eightBytesToLong(byte [] bytes)
+    {
+        long val = 0L;
+        for (int i = 0; i < 8; i++)
+        {
+            val += bytes[i] << i;
+        }
+        return val;
     }
 }
 
