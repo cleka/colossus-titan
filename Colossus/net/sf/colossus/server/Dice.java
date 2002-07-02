00c048c40b055949acb112038c66a8b2b9a57252
Romain Dolbeau <romain@dolbeau.org>
2002-07-02 08:27:00 +0000

    update class Dice to use a custom Random, that will try to use an external random sources (first an optional custom one, then /dev/urandom, then /dev/random). If all else fails, fall back on the internal PRNG.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1755 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/server/Dice.java
+++ Colossus/net/sf/colossus/server/Dice.java
@@ -7,19 +7,104 @@ import java.io.*;
 import net.sf.colossus.util.Log;
 
 
+
+/**
+ * Class DevRandom generates random numbers from the "/dev/random" device, or any file or device supplying random binary data. If the source is unreachable it falls back on the regular java Random implementation.
+ * @version $Id$
+ * @author David Ripton
+ * @author Romain Dolbeau
+ */
+class DevRandom extends Random
+{
+    private final static String urandomFilename = "/dev/urandom";
+    private final static String randomFilename = "/dev/random";
+    private String source = urandomFilename;
+    private File randomSource = null;
+    private FileInputStream randStream = null;
+
+    public DevRandom()
+    {
+        init();
+    }
+    
+    public DevRandom(String sourcename)
+    {
+        source = sourcename;
+        init();
+    }
+    
+    private void init()
+    {
+        randomSource = new File(source);
+        if ((randomSource == null) || (!randomSource.exists()))
+        {
+            source = urandomFilename;
+            randomSource = new File(source);
+        }
+        if ((randomSource == null) || (!randomSource.exists()))
+        {
+            source = randomFilename;
+            randomSource = new File(source);
+        }
+        if ((randomSource != null) && (randomSource.exists()))
+        {
+            try
+            {
+                randStream = new FileInputStream(randomSource);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Log.error("Random source disappeared! " + ex);
+                System.exit(1);
+            }
+            Log.debug("Using " + source + " as the random source.");
+        }
+        else
+        {
+            Log.error("Random source unavailable ! Falling back on a PRNG");
+        }
+    }
+    
+    public int next(int bits)
+    {
+        if (randStream == null)
+        {
+//Log.debug("Error in the random source(s), falling back on the PRNG");
+            return super.next(bits);
+        }
+        if (bits > 32)
+            bits = 32;
+        if (bits < 1)
+            bits = 1;
+        int size = (bits + 7) / 8;
+        int mask = (1 << bits) - 1;
+        byte [] bytes = new byte[size];
+        try
+        {
+            randStream.read(bytes);
+        }
+        catch (IOException ex)
+        {
+            Log.error("Problem reading from random source");
+            return super.next(bits);
+        }
+        int result = 0;
+        for (int i = 0; i < size ; i++)
+        {
+            result |= ((long)(bytes[i])) << (i * 8);
+        }
+        return (result & mask);
+    }
+}
+
 /**
  * Class Dice handles die-rolling
  * @version $Id$
  * @author David Ripton
  */
-
 public final class Dice
 {
-    private static Random random = new Random();
-    private static String randomFilename = "/dev/urandom";
-    private static File randomSource = new File(randomFilename);
-    private static FileInputStream randStream = null;
-    
+    private static Random random = new DevRandom();
     
     /** Put all die rolling in one place, in case we decide to change random
      *  number algorithms, use an external dice server, etc. */
@@ -27,55 +112,9 @@ public final class Dice
     {
         return rollDie(6);
     }
-
+    
     public static int rollDie(int size)
     {
-        if (size > 10000)
-        {
-            Log.error("Looks like someone is trying to cheat");
-            return 0;
-        }
-        if (randomSource.exists())
-        {
-            if (randStream == null)
-            {
-                try
-                {
-                    randStream = new FileInputStream(randomSource);
-                }
-                catch (FileNotFoundException ex)
-                {
-                    Log.error("/dev/random disappeared!");
-                    System.exit(1);
-                }
-            }
-            byte [] bytes = new byte[8];
-            try
-            {
-                if (randStream.available() >= 8)
-                {
-                    randStream.read(bytes);
-                }
-            }
-            catch (IOException ex)
-            {
-                Log.error("Problem reading from /dev/random");
-                System.exit(1);
-            }
-            random.setSeed(eightBytesToLong(bytes));
-        }
         return random.nextInt(size) + 1;
     }
-
-    /** Generate a long from an array of eight bytes. */
-    private static long eightBytesToLong(byte [] bytes)
-    {
-        long val = 0L;
-        for (int i = 0; i < 8; i++)
-        {
-            val += bytes[i] << (8 * i);
-        }
-        return val;
-    }
 }
-
