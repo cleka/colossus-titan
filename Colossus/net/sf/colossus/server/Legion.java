4abb4be9de4fa89f19fb2974bbf98483ca0a8b47
David Ripton <dripton@ripton.net>
2002-08-05 05:13:02 +0000

    John Lum's new silver markers.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1834 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/server/Legion.java
+++ Colossus/net/sf/colossus/server/Legion.java
@@ -151,18 +151,18 @@ public final class Legion implements Comparable
         markerNames.put("Pu12", "Violet");
 
         /* Silver */
-        markerNames.put("Si01", "Axes2");
-        markerNames.put("Si02", "Eye2");
-        markerNames.put("Si03", "Die2");
-        markerNames.put("Si04", "Feather2");
-        markerNames.put("Si05", "Hand2");
-        markerNames.put("Si06", "Lightning2");
-        markerNames.put("Si07", "Pumpkin2");
-        markerNames.put("Si08", "Rose2");
-        markerNames.put("Si09", "Scorpion2");
-        markerNames.put("Si10", "Skull2");
-        markerNames.put("Si11", "Spearhead2");
-        markerNames.put("Si12", "Tombstone2");
+        markerNames.put("Si01", "Bomb");
+        markerNames.put("Si02", "Buckle");
+        markerNames.put("Si03", "Bullets");
+        markerNames.put("Si04", "Horseshoe");
+        markerNames.put("Si05", "Ingots");
+        markerNames.put("Si06", "Medal");
+        markerNames.put("Si07", "Mine");
+        markerNames.put("Si08", "Pin");
+        markerNames.put("Si09", "Pistol");
+        markerNames.put("Si10", "Seraph");
+        markerNames.put("Si11", "Shackles");
+        markerNames.put("Si12", "Spoons");
     }
 
 
