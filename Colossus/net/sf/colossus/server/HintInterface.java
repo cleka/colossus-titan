348a0371e3e48f11401a1f03bfa62d633661879f
Romain Dolbeau <romain@dolbeau.org>
2002-08-09 11:08:20 +0000

    add comments to HintInterface.java
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@1855 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/server/HintInterface.java
+++ Colossus/net/sf/colossus/server/HintInterface.java
@@ -10,13 +10,34 @@ import java.util.List;
  */
 public interface HintInterface
 {
+    /**
+     * Give the suggested recruit in this terrain for this legion.
+     * @param terrain Type of terrain
+     * @param legion The recruiting legion
+     * @param recruits List of all recruitable Creature (returned value must be the name of one of those)
+     * @param oracle An oracle that answers some questions about the situation of the legion
+     * @param section Array of AI section to be used (usually one or more of "AllAI:", "DefensiveAI:", "OffensiveAI:")
+     * @return The name os the suggested recruit
+     */
     public String getRecruitHint(char terrain,
                                  LegionInfo legion,
                                  List recruits,
                                  HintOracleInterface oracle,
                                  String[] section);
+    /**
+     * To obtain the list of creature to split on turn one.
+     * @param label Label of the starting Tower.
+     * @param section Array of AI section to be used (usually one or more of "AllAI:", "DefensiveAI:", "OffensiveAI:")
+     * @return The list of creature to split (listed by name)
+     */
     public List getInitialSplitHint(String label,
                                     String[] section);
+    /**
+     * Give an offset to apply to the creature Point Value before estimating it's recruitment value.
+     * @param name Name of the Creature
+     * @param section Array of AI section to be used (usually one or more of "AllAI:", "DefensiveAI:", "OffensiveAI:")
+     * @return An offset to the Point Value of the creature.
+     */
     public int getHintedRecruitmentValueOffset(String name,
                                                String[] section);    
 }
