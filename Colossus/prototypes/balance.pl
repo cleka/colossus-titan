aa080f5d6879d04f72973e28e8cc28fc2fbdefb3
David Ripton <dripton@ripton.net>
2003-01-08 14:37:13 +0000

    Check in the old balanced towers and titan hits prototypes.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2074 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/prototypes/balance.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl -w
+use strict;
+
+
+# Prototype to figure out balanced tower algorithm for Colossus
+# dripton 20020305
+
+# We use 0-based math everywhere to make mods work easily.
+# Convert at the end for output.
+
+
+
+# Convert from 0-based to 1-based then multiply by 100.
+sub pretty_tower_num($)
+{
+    my ($ord) = shift;
+    return 100 * ($ord + 1);
+}
+
+my $ntowers;
+my $nplayers;
+
+if (@ARGV >= 2)
+{
+    $ntowers = $ARGV[0];
+    $nplayers = $ARGV[1];
+}
+else
+{
+    print "Enter number of towers: ";
+    $ntowers = <STDIN>;
+    chomp $ntowers;
+
+    print "Enter number of players: ";
+    $nplayers = <STDIN>;
+    chomp $nplayers;
+}
+
+print "$ntowers towers and $nplayers players\n";
+
+if ($nplayers > $ntowers)
+{
+    die "More players than towers -- can't work.  Aborting.\n"
+}
+
+my $towers_per_player = $ntowers / $nplayers;
+
+print "towers / players = $towers_per_player\n";
+
+
+
+# Figure out the starting sequence of towers.
+my %sequence;
+my $ndone = 0;
+my $f = 0;
+# Prevent roundoff error
+my $epsilon = 0.000001;
+while ($ndone < $nplayers)
+{
+    $sequence{int $f + $epsilon} = 1;
+    $ndone++;
+    $f += $towers_per_player; 
+}
+
+print "starting sequence: ";
+foreach my $key (sort {$a <=> $b} keys %sequence)
+{
+    print $key . " "
+}
+print "\n";
+
+
+# Pick a random starting tower.
+my $offset = int rand $ntowers;
+print "random offset =    $offset\n"; 
+
+# Make the sequence relative to the starting tower.
+my %offset_sequence;
+foreach my $key (sort {$a <=> $b} keys %sequence)
+{
+    $offset_sequence{($key + $offset) % $ntowers} = 1;
+}
+my @towers = keys %offset_sequence;
+
+print "offset sequence:   ";
+foreach my $tower (sort {$a <=> $b} @towers)
+{
+    print &pretty_tower_num($tower) . " "
+}
+print "\n";
+
+
---
+++ Colossus/prototypes/titanhits.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+#titanhits.py   David Ripton 2002, converted from my old titanhits.pl
+
+""" 
+Calculate the probability of each number of hits in a Titan game attack.
+
+Roll N 6-sided dice.  s, the strike number, is an integer in the range
+of 2-6.  Each roll >= s is a hit.
+
+p, the probability of a hit on each die, is (7 - s) / 6 
+
+For each value h in the range 0-N, the probability of exactly h hits
+is (p)^h (1-p)^(N-h) (N choose h)
+where (N choose h) is N! / (h! * (N-h)!)
+"""
+
+
+def fact(arg):
+    """Calculate the factorial of a number."""
+    answer = 1.0;
+    for i in range (2, arg+1):
+        answer = answer * i
+    return answer
+
+def choose(a,b):
+    """Calculate the value of a choose b"""
+    return fact(a) / (fact(b) * fact(a - b));
+
+
+import sys
+if (len(sys.argv) == 3):
+    dice = sys.argv[1]
+    target = sys.argv[2]
+else:
+    dice = input("Enter number of dice: ")
+    target = input("Enter target number: ")
+
+dice = int(dice)
+target = int(target)
+
+print "dice is %d and target number is %d" % (dice, target)
+
+p = (7.0 - target) / 6
+antip = 1 - p
+
+print "p is %f and antip is %f" % (p, antip)
+
+cumulative_prob = 0
+
+print "        equal     or fewer   or greater"
+
+for h in range(0, dice+1):
+    prob = (p ** h) * (antip ** (dice - h)) * choose(dice, h)
+    cumulative_prob = cumulative_prob + prob
+    print "%2d  %10.5f  %10.5f  %10.5f" % (h, prob, cumulative_prob, \
+        1 - cumulative_prob + prob)
+
