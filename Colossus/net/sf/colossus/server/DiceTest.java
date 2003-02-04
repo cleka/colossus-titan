82b711666c63dc47cae94ebc91b6f78cc744dbfc
David Ripton <dripton@ripton.net>
2003-02-04 17:47:42 +0000

    Rewrite runs test.  It works now. Integrate my bugfixes with Romain's.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2130 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/server/DiceTest.java
+++ Colossus/net/sf/colossus/server/DiceTest.java
@@ -1,5 +1,7 @@
 package net.sf.colossus.server;
 
+import net.sf.colossus.util.SpecialFunction;
+
 import java.util.*;
 import junit.framework.*;
 
@@ -11,7 +13,7 @@ import junit.framework.*;
  */
 public class DiceTest extends TestCase
 {
-    private int trials = 10000;
+    private int trials = 5000;
     double epsilon = 0.000001;
 
     public DiceTest(String name)
@@ -19,18 +21,18 @@ public class DiceTest extends TestCase
         super(name);
     }
 
-    // Commented out because it's so slow.
-    /*
+    
+    /* Commented out because it's slow.
     public void testDevRandom()
     {
-        System.out.println("testDevRandom()");
+        System.out.println("---testDevRandom()---");
         int [] rolls = new int[trials];
         Dice.init("/dev/random");
         for (int i = 0; i < trials; i++)
         {
             rolls[i] = Dice.rollDie();
         }
-        run4Tests(rolls, true);
+        runAllTests(rolls, true);
     }
     */
 
@@ -38,7 +40,7 @@ public class DiceTest extends TestCase
     /*
     public void testTmpRandom()
     {
-        System.out.println("testTmpRandom()");
+        System.out.println("---testTmpRandom()---");
         int [] rolls = new int[trials];
         Dice.init("/tmp/random");
         for (int i = 0; i < trials; i++)
@@ -51,40 +53,40 @@ public class DiceTest extends TestCase
     
     public void testDevUrandom()
     {
-        System.out.println("testDevUrandom()");
+        System.out.println("---testDevUrandom()---");
         int [] rolls = new int[trials];
         Dice.init("/dev/urandom");
         for (int i = 0; i < trials; i++)
         {
             rolls[i] = Dice.rollDie();
         }
-        run4Tests(rolls, true);
+        runAllTests(rolls, true);
     }
     
     public void testPRNG()
     {
-        System.out.println("testPRNG()");
+        System.out.println("---testPRNG()---");
         int [] rolls = new int[trials];
         Dice.init("PRNG");
         for (int i = 0; i < trials; i++)
         {
             rolls[i] = Dice.rollDie();
         }
-        run4Tests(rolls, true);
+        runAllTests(rolls, true);
     }
 
     public void testNonRandomDice()
     {
-        System.out.println("testNonRandomDice() [some should fail]");
+        System.out.println("---testNonRandomDice()--- [some should fail]");
         int [] rolls = new int[trials];
         for (int i = 0; i < trials; i++)
         {
             rolls[i] = Dice.rollDieNonRandom();
         }
-        run4Tests(rolls, false);
+        runAllTests(rolls, false);
     }
 
-    void run4Tests(int [] rolls, boolean random)
+    void runAllTests(int [] rolls, boolean random)
     {
         mTest(rolls, random);
         signTest(rolls, random);
@@ -92,8 +94,9 @@ public class DiceTest extends TestCase
         runsTest(rolls, random);
     }
 
-    // Source of tests: http://www.stat.unc.edu/faculty/rs/s102/lec17a.pdf
-    // http://www2.sunysuffolk.edu/wrightj/MA24/Misc/RunsTest.pdf
+
+    // Most tests at http://www.stat.unc.edu/faculty/rs/s102/lec17a.pdf
+    // Runs test http://www2.sunysuffolk.edu/wrightj/MA24/Misc/RunsTest.pdf
 
     /** Recode each sample as 0 if <= sample median, 1 if > sample median
         M is number of runs of consecutive 0s and 1s.
@@ -114,8 +117,8 @@ public class DiceTest extends TestCase
         double meanM = (2.0 * r * (n - r) / n) + 1.;
         double varianceM = ((2.0 * r) * (n - r) / n / n * 
                 ((2. * r) * (n - r) - n)) / (n - 1.);
-        System.out.println("r=" + r + " M=" + M + " mean=" + meanM + " var=" +
-            varianceM);
+        System.out.println("M test: r=" + r + " M=" + M + " mean=" + meanM + 
+            " var=" + varianceM);
         failIfAbnormal(M, meanM, varianceM, random); 
     }
 
@@ -128,19 +131,23 @@ public class DiceTest extends TestCase
         double m = (double)countNonZeroDiffs(rolls);
         double meanP = m / 2.;
         double varianceP = m / 12.;
-        System.out.println("P=" + P + " m=" + m + " mean=" + meanP + " var=" +
-            varianceP);
+        System.out.println("Sign test: P=" + P + " m=" + m + " mean=" + 
+            meanP + " var=" + varianceP);
         failIfAbnormal(P, meanP, varianceP, random); 
     }
 
     void runsTest(int [] rolls, boolean random)
     {
-        double R = (double)countRunsExcludingZeros(rolls);
-        double m = (double)countNonZeroDiffs(rolls);
-        double meanR = ((2. * m) + 1.) / 3.;
-        double varianceR = ((16. * m) - 13.) / 90.;
-        System.out.println("R=" + R + " m=" + m + " mean=" + meanR + " var=" +
-            varianceR);
+        int [] trimmed = trimZeroRuns(rolls);
+        int m = trimmed.length;
+        int pos = countPositiveDiffs(trimmed);
+        int neg = m - pos;
+        double R = pos;
+        double meanR = 1.0 + (2 * pos * neg) / (pos + neg);
+        double varianceR = ((2.0 * pos * neg) * (2 * pos * neg - pos - neg)) /
+                ((pos + neg) * (pos + neg) * (pos + neg - 1));
+        System.out.println("Runs test: R=" + R + " m=" + m + " mean=" + 
+            meanR + " var=" + varianceR);
         failIfAbnormal(R, meanR, varianceR, random); 
     }
 
@@ -158,10 +165,22 @@ public class DiceTest extends TestCase
         }
         double meanS = 0.;
         double varianceS = (n / 18.) * (n - 1.) * (2. * n + 5.);
-        System.out.println("S=" + S + " mean=" + meanS + " var=" + varianceS);
+        System.out.println("M-K test: S=" + S + " mean=" + meanS + 
+            " var=" + varianceS);
         failIfAbnormal(S, meanS, varianceS, random); 
     }
-    
+
+
+    double findMean(int [] rolls)
+    {
+        double sum = 0.0;
+        for (int i = 0; i < rolls.length; i++)
+        {
+            sum += rolls[i];
+        }
+        return sum / rolls.length;
+    }
+
     double findMedian(int [] rolls)
     {
         int [] copy = (int [])rolls.clone();
@@ -247,61 +266,32 @@ public class DiceTest extends TestCase
         return count;
     }
 
-    int countRunsExcludingZeros(int [] rolls)
+    /** Return the list with runs of identical rolls reduced to just one */
+    int [] trimZeroRuns(int [] rolls)
     {
-        int count = 0;
-        // states: 1 = positive, -1 = negative
-        int state = 0;
-        for (int i = 1; i < rolls.length; i++)
+        List li = new ArrayList();
+        int lastroll = -1;
+        for (int i = 0; i < rolls.length; i++)
         {
-            if (rolls[i] > rolls[i - 1])
+            if (rolls[i] != lastroll)
             {
-                 if (state != 1)
-                 {
-                     count++;
-                 }
-                 state = 1;
-            }
-            else if (rolls[i] < rolls[i - 1])
-            {
-                 if (state != -1)
-                 {
-                     count++;
-                 }
-                 state = -1;
+                lastroll = rolls[i];
+                li.add(new Integer(lastroll));
             }
         }
-        return count;
+        int [] results = new int[li.size()];
+        for (int i = 0; i < results.length; i++)
+        {
+            results[i] = ((Integer)li.get(i)).intValue();
+        }
+        return results;
     }
+
 
     /** Return the standard normal probability p(z) */
     double normalDistribution(double z)
     {
         return 1/Math.sqrt(2*Math.PI) * Math.exp(-z*z/2);
-    }
-
-
-    /** Fail if an expected random result is outside the normal range. */
-    void failIfAbnormal(double val, double mean, double variance, 
-        boolean random)
-    {
-        double sd = Math.sqrt(variance);
-        double z = (val - mean) / sd;
-        System.out.print("sd=" + sd + " z=" + z);
-        // fail if variance negative, as sd & z are NaN
-        if (random && ((Math.abs(z) > 3) || (variance < 0.)))
-        {
-            System.out.println(" (FAIL)");
-            fail();
-        }
-        else if (!random && ((Math.abs(z) > 3) || (variance < 0.)))
-        {
-            System.out.println(" (EXPECTED FAIL)");
-        }
-        else
-        {
-            System.out.println(" (SUCCESS)");
-        }
     }
 
 
@@ -317,5 +307,39 @@ public class DiceTest extends TestCase
             return -1;
         }
         return 0;
+    }
+
+
+    /** Fail if an expected random result is outside the normal range. */
+    void failIfAbnormal(double val, double mean, double variance, 
+        boolean random)
+    {
+        double sd;
+        double z;
+        // Avoid division by zero when we hit spot-on.
+        if (Math.abs(variance) < epsilon) 
+        {
+            sd = 0;
+            z = 0;
+        }
+        else
+        {
+            sd = Math.sqrt(variance);
+            z = (val - mean) / sd;
+        }
+        System.out.print("sd=" + sd + " z=" + z);
+        if (random && (Math.abs(z) > 3))
+        {
+            System.out.println(" (FAILURE)");
+            fail();
+        }
+        else if (!random && ((Math.abs(z) > 3)))
+        {
+            System.out.println(" (expected failure)");
+        }
+        else
+        {
+            System.out.println(" (success)");
+        }
     }
 }
