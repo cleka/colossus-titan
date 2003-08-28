26fc04ab1816d4c18433e2b92b476884a7598845
David Ripton <dripton@ripton.net>
2003-08-28 00:48:18 +0000

    Use absolute value to fix bug in findMedian()
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2270 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/server/DiceTest.java
+++ Colossus/net/sf/colossus/server/DiceTest.java
@@ -1,5 +1,6 @@
 package net.sf.colossus.server;
 
+
 import java.util.*;
 import junit.framework.*;
 
@@ -19,40 +20,39 @@ public class DiceTest extends TestCase
         super(name);
     }
 
-    
     /* Commented out because it's slow.
-    public void testDevRandom()
-    {
-        System.out.println("---testDevRandom()---");
-        int [] rolls = new int[trials];
-        Dice.init("/dev/random");
-        for (int i = 0; i < trials; i++)
-        {
-            rolls[i] = Dice.rollDie();
-        }
-        runAllTests(rolls, true);
-    }
-    */
+     public void testDevRandom()
+     {
+     System.out.println("---testDevRandom()---");
+     int [] rolls = new int[trials];
+     Dice.init("/dev/random");
+     for (int i = 0; i < trials; i++)
+     {
+     rolls[i] = Dice.rollDie();
+     }
+     runAllTests(rolls, true);
+     }
+     */
 
     // Comented out because not everyone has a /tmp/random :-)
     /*
-    public void testTmpRandom()
-    {
-        System.out.println("---testTmpRandom()---");
-        int [] rolls = new int[trials];
-        Dice.init("/tmp/random");
-        for (int i = 0; i < trials; i++)
-        {
-            rolls[i] = Dice.rollDie();
-        }
-        run4Tests(rolls, true);
-    }
-    */
-    
+     public void testTmpRandom()
+     {
+     System.out.println("---testTmpRandom()---");
+     int [] rolls = new int[trials];
+     Dice.init("/tmp/random");
+     for (int i = 0; i < trials; i++)
+     {
+     rolls[i] = Dice.rollDie();
+     }
+     run4Tests(rolls, true);
+     }
+     */
+
     public void testDevUrandom()
     {
         System.out.println("---testDevUrandom()---");
-        int [] rolls = new int[trials];
+        int[] rolls = new int[trials];
         Dice.init("/dev/urandom");
         for (int i = 0; i < trials; i++)
         {
@@ -60,11 +60,11 @@ public class DiceTest extends TestCase
         }
         runAllTests(rolls, true);
     }
-    
+
     public void testPRNG()
     {
         System.out.println("---testPRNG()---");
-        int [] rolls = new int[trials];
+        int[] rolls = new int[trials];
         Dice.init("PRNG");
         for (int i = 0; i < trials; i++)
         {
@@ -76,7 +76,7 @@ public class DiceTest extends TestCase
     public void testNonRandomDice()
     {
         System.out.println("---testNonRandomDice()--- [some should fail]");
-        int [] rolls = new int[trials];
+        int[] rolls = new int[trials];
         for (int i = 0; i < trials; i++)
         {
             rolls[i] = Dice.rollDieNonRandom();
@@ -84,7 +84,7 @@ public class DiceTest extends TestCase
         runAllTests(rolls, false);
     }
 
-    void runAllTests(int [] rolls, boolean random)
+    void runAllTests(int[] rolls, boolean random)
     {
         chiSquareTest(rolls, random);
         mTest(rolls, random);
@@ -98,69 +98,68 @@ public class DiceTest extends TestCase
 
 
     // XXX Need to use chi-square distribution not normal distribution.
-    void chiSquareTest(int [] rolls, boolean random)
+    void chiSquareTest(int[] rolls, boolean random)
     {
         // We know our non-random dice will ace this test, so use them
         // to find expected mean and variance.
         int divisibleBy6 = 6 * trials / 6;
-        int [] fixedRolls = new int[divisibleBy6];
+        int[] fixedRolls = new int[divisibleBy6];
         for (int i = 0; i < fixedRolls.length; i++)
         {
             fixedRolls[i] = Dice.rollDieNonRandom();
         }
         final double meanDieRoll = 3.5;
         double expVariance = findExpectedVariance(fixedRolls, meanDieRoll);
-        double expMean = findChiSquare(fixedRolls, meanDieRoll) / 
-            fixedRolls.length;
+        double expMean = findChiSquare(fixedRolls, meanDieRoll) /
+                fixedRolls.length;
 
         double chisquare = findChiSquare(rolls, meanDieRoll) / rolls.length;
-        System.out.println("chi-square test: chi-square=" + chisquare + 
-            " mean=" + expMean + " var=" + expVariance);
-        failIfAbnormal(chisquare, expMean, expVariance, random); 
+        System.out.println("chi-square test: chi-square=" + chisquare +
+                " mean=" + expMean + " var=" + expVariance);
+        failIfAbnormal(chisquare, expMean, expVariance, random);
     }
 
-
     /** Recode each sample as 0 if <= sample median, 1 if > sample median
-        M is number of runs of consecutive 0s and 1s.
-        r is number of 0s.
-        null hypothesis, mean and variance of M in n observations are about
-        meanM = 2*r*(n-r)/n + 1
-        varianceM = 2*r*(n-r)*(2*r*(n-r)-n)/(n*n*(n-1))
-        for large samples Zm = (M - meanM) / standardDevM is standard normal
-        prob (M <= val) = Pr((M-meanM)/sdM = Pr(Z)
+     M is number of runs of consecutive 0s and 1s.
+     r is number of 0s.
+     null hypothesis, mean and variance of M in n observations are about
+     meanM = 2*r*(n-r)/n + 1
+     varianceM = 2*r*(n-r)*(2*r*(n-r)-n)/(n*n*(n-1))
+     for large samples Zm = (M - meanM) / standardDevM is standard normal
+     prob (M <= val) = Pr((M-meanM)/sdM = Pr(Z)
      */
-    void mTest(int [] rolls, boolean random)
+    void mTest(int[] rolls, boolean random)
     {
         double median = findMedian(rolls);
-        int [] ms = convertToBinary(rolls, median);
+        int[] ms = convertToBinary(rolls, median);
         int r = countZeros(ms);
         int M = countRuns(ms);
         int n = trials;
         double meanM = (2.0 * r * (n - r) / n) + 1.;
-        double varianceM = ((2.0 * r) * (n - r) / n / n * 
+        double varianceM = ((2.0 * r) * (n - r) / n / n *
                 ((2. * r) * (n - r) - n)) / (n - 1.);
-        System.out.println("M test: r=" + r + " M=" + M + " mean=" + meanM + 
-            " var=" + varianceM);
-        failIfAbnormal(M, meanM, varianceM, random); 
+        System.out.println("M test: r=" + r + " M=" + M + " mean=" + meanM +
+                " var=" + varianceM);
+        failIfAbnormal(M, meanM, varianceM, random);
     }
 
     /** P is number of positive signs among x2-x1, x3-x2, etc. (not zeros)
-        If m non-zero values of xi - x(i-1), meanP is m/2, varianceP is m/12
+     If m non-zero values of xi - x(i-1), meanP is m/2, varianceP is m/12
      */
-    void signTest(int [] rolls, boolean random)
+    void signTest(int[] rolls, boolean random)
     {
         double P = (double)countPositiveDiffs(rolls);
         double m = (double)countNonZeroDiffs(rolls);
         double meanP = m / 2.;
         double varianceP = m / 12.;
-        System.out.println("Sign test: P=" + P + " m=" + m + " mean=" + 
-            meanP + " var=" + varianceP);
-        failIfAbnormal(P, meanP, varianceP, random); 
+        System.out.println("Sign test: P=" + P + " m=" + m + " mean=" +
+                meanP + " var=" + varianceP);
+        failIfAbnormal(P, meanP, varianceP, random);
     }
 
-    void runsTest(int [] rolls, boolean random)
+    void runsTest(int[] rolls, boolean random)
     {
-        int [] trimmed = trimZeroRuns(rolls);
+        int[] trimmed = trimZeroRuns(rolls);
         int m = trimmed.length;
         int pos = countPositiveDiffs(trimmed);
         int neg = m - pos;
@@ -168,12 +167,12 @@ public class DiceTest extends TestCase
         double meanR = 1. + (2 * pos * neg) / (pos + neg);
         double varianceR = ((2.0 * pos * neg) * (2 * pos * neg - pos - neg)) /
                 ((pos + neg) * (pos + neg) * (pos + neg - 1));
-        System.out.println("Runs test: R=" + R + " m=" + m + " mean=" + 
-            meanR + " var=" + varianceR);
-        failIfAbnormal(R, meanR, varianceR, random); 
+        System.out.println("Runs test: R=" + R + " m=" + m + " mean=" +
+                meanR + " var=" + varianceR);
+        failIfAbnormal(R, meanR, varianceR, random);
     }
 
-    void mannKendallTest(int [] rolls, boolean random)
+    void mannKendallTest(int[] rolls, boolean random)
     {
         int S = 0;
         int n = rolls.length;
@@ -187,13 +186,12 @@ public class DiceTest extends TestCase
         }
         double meanS = 0.;
         double varianceS = (n / 18.) * (n - 1.) * (2. * n + 5.);
-        System.out.println("Mann-Kendall test: S=" + S + " mean=" + meanS + 
-            " var=" + varianceS);
-        failIfAbnormal(S, meanS, varianceS, random); 
+        System.out.println("Mann-Kendall test: S=" + S + " mean=" + meanS +
+                " var=" + varianceS);
+        failIfAbnormal(S, meanS, varianceS, random);
     }
 
-
-    double findMean(int [] rolls)
+    double findMean(int[] rolls)
     {
         double sum = 0.0;
         for (int i = 0; i < rolls.length; i++)
@@ -203,25 +201,25 @@ public class DiceTest extends TestCase
         return sum / rolls.length;
     }
 
-    double findMedian(int [] rolls)
+    double findMedian(int[] rolls)
     {
-        int [] copy = (int [])rolls.clone();
+        int[] copy = (int[])rolls.clone();
         Arrays.sort(copy);
         double midpoint = (rolls.length - 1) / 2.0;
-        if (midpoint - Math.round(midpoint) < epsilon)
+        if (Math.abs(midpoint - Math.round(midpoint)) < epsilon)
         {
             return copy[(int)Math.round(midpoint)];
         }
         else
         {
-            return (copy[(int)Math.round(midpoint - 0.5)] + 
+            return (copy[(int)Math.round(midpoint - 0.5)] +
                     copy[(int)Math.round(midpoint + 0.5)]) / 2.0;
         }
     }
 
-    int [] convertToBinary(int [] rolls, double median)
+    int[] convertToBinary(int[] rolls, double median)
     {
-        int [] ms = new int[rolls.length];
+        int[] ms = new int[rolls.length];
         for (int i = 0; i < rolls.length; i++)
         {
             if (rolls[i] <= median)
@@ -236,7 +234,7 @@ public class DiceTest extends TestCase
         return ms;
     }
 
-    int countZeros(int [] rolls)
+    int countZeros(int[] rolls)
     {
         int count = 0;
         for (int i = 0; i < rolls.length; i++)
@@ -249,7 +247,7 @@ public class DiceTest extends TestCase
         return count;
     }
 
-    int countRuns(int [] rolls)
+    int countRuns(int[] rolls)
     {
         int count = 0;
         for (int i = 0; i < rolls.length; i++)
@@ -262,34 +260,34 @@ public class DiceTest extends TestCase
         return count;
     }
 
-    int countPositiveDiffs(int [] rolls)
+    int countPositiveDiffs(int[] rolls)
     {
         int count = 0;
         for (int i = 0; i < rolls.length - 1; i++)
         {
             if (rolls[i + 1] > rolls[i])
             {
-                 count++;
+                count++;
             }
         }
         return count;
     }
 
-    int countNonZeroDiffs(int [] rolls)
+    int countNonZeroDiffs(int[] rolls)
     {
         int count = 0;
-        for (int i = 1; i < rolls.length ; i++)
+        for (int i = 1; i < rolls.length; i++)
         {
             if (rolls[i] != rolls[i - 1])
             {
-                 count++;
+                count++;
             }
         }
         return count;
     }
 
     /** Return the list with runs of identical rolls reduced to just one */
-    int [] trimZeroRuns(int [] rolls)
+    int[] trimZeroRuns(int[] rolls)
     {
         List li = new ArrayList();
         int lastroll = -1;
@@ -301,7 +299,7 @@ public class DiceTest extends TestCase
                 li.add(new Integer(lastroll));
             }
         }
-        int [] results = new int[li.size()];
+        int[] results = new int[li.size()];
         for (int i = 0; i < results.length; i++)
         {
             results[i] = ((Integer)li.get(i)).intValue();
@@ -309,13 +307,11 @@ public class DiceTest extends TestCase
         return results;
     }
 
-
     /** Return the standard normal probability p(z) */
     double normalDistribution(double z)
     {
-        return 1/Math.sqrt(2*Math.PI) * Math.exp(-z*z/2);
+        return 1 / Math.sqrt(2 * Math.PI) * Math.exp(-z * z / 2);
     }
-
 
     /** Return 1 if positive, 0 if zero, -1 if negative. */
     int sign(int num)
@@ -331,8 +327,7 @@ public class DiceTest extends TestCase
         return 0;
     }
 
-
-    double findChiSquare(int [] rolls, double mean)
+    double findChiSquare(int[] rolls, double mean)
     {
         double sum = 0.;
         for (int i = 0; i < rolls.length; i++)
@@ -343,7 +338,7 @@ public class DiceTest extends TestCase
         return sum / mean;
     }
 
-    double findExpectedVariance(int [] rolls, double mean)
+    double findExpectedVariance(int[] rolls, double mean)
     {
         double sum = 0.;
         int n = rolls.length;
@@ -355,15 +350,14 @@ public class DiceTest extends TestCase
         return sum / n;
     }
 
-
     /** Fail if an expected random result is outside the normal range. */
-    void failIfAbnormal(double val, double mean, double variance, 
-        boolean random)
+    void failIfAbnormal(double val, double mean, double variance,
+            boolean random)
     {
         double sd;
         double z;
         // Avoid division by zero when we hit spot-on.
-        if (Math.abs(variance) < epsilon) 
+        if (Math.abs(variance) < epsilon)
         {
             sd = 0;
             z = 0;
