9de3ba104ffb10682ecb8926da2b18c792d294e2
David Ripton <dripton@ripton.net>
2004-01-22 03:02:07 +0000

    Beelzebub variant, by Chris Byler and Jay Delanoy
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2396 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/Beelzebub/BeelzebubHint.java
@@ -0,0 +1,345 @@
+package Beelzebub;
+
+import net.sf.colossus.client.LegionInfo;
+import net.sf.colossus.server.HintOracleInterface;
+import net.sf.colossus.server.Creature;
+import net.sf.colossus.server.Constants;
+import net.sf.colossus.util.DevRandom;
+import java.util.*;
+
+public class BeelzebubHint implements net.sf.colossus.server.HintInterface
+{
+        private DevRandom rnd = new DevRandom();
+
+        public String getRecruitHint(String terrain,
+                LegionInfo legion,
+                List recruits,
+                HintOracleInterface oracle,
+                String[] section)
+        {
+                List sect = Arrays.asList(section);
+
+                if (terrain.equals("Plains"))
+                {
+                        if(recruits.contains("Centaur") &&
+                                !legion.contains("Warbear") &&
+                                !legion.contains("Ent") &&
+                                !legion.contains("Unicorn") &&
+                                !legion.contains("Ranger") &&
+                                !legion.contains("Knight") &&
+                                !oracle.canRecruit("Ranger") &&
+                                legion.getHeight() != 6 &&
+                                legion.numCreature("Centaur") == 2 &&
+                                oracle.canReach("Woods") &&
+                                oracle.creatureAvailable("Warbear") >= 2
+                                )
+                        {
+                                return "Centaur";
+                        }
+                        if(recruits.contains("Ranger") &&
+                                !legion.contains("Basilisk") &&
+                                legion.getHeight() != 6 &&
+                                legion.numCreature("Ranger") == 2 &&
+                                oracle.canReach("Marsh") &&
+                                oracle.creatureAvailable("Basilisk") >= 1
+                                )
+                        {
+                                return "Ranger";
+                        }
+
+                }
+                else if (terrain.equals("Marsh"))
+                {
+                        if(recruits.contains("Goblin") &&
+                                !legion.contains("Minotaur") &&
+                                !legion.contains("Djinn") &&
+                                !legion.contains("Manticore") &&
+                                !legion.contains("Ranger") &&
+                                !legion.contains("Basilisk") &&
+                                !oracle.canRecruit("Ranger") &&
+                                legion.numCreature("Goblin") == 2 &&
+                                legion.getHeight() != 6 &&
+                                oracle.canReach("Hills") &&
+                                oracle.creatureAvailable("Minotaur") >= 2
+                                )
+                        {
+                                return "Goblin";
+                        }
+                }
+                else if (terrain.equals("Ruins"))
+                {
+                        if(recruits.contains("Skeleton") &&
+                                !legion.contains("Zombie") &&
+                                !legion.contains("Ghost") &&
+                                !legion.contains("Vampire") &&
+                                !oracle.canRecruit("Vampire") &&
+                                !legion.contains("Lich") &&
+                                !legion.contains("Death") &&
+                                !legion.contains("Mummy") &&
+                                legion.numCreature("Skeleton") == 2 &&
+                                legion.getHeight() != 6 &&
+                                oracle.canReach("Tombs") &&
+                                oracle.creatureAvailable("Zombie") >= 2
+                                )
+                        {
+                                return "Skeleton";
+                        }
+                        if (recruits.contains("Shade") &&
+                                !legion.contains("Ghost") &&
+                                !legion.contains("Wraith") &&
+                                !legion.contains("Beelzebub") &&
+                                !legion.contains("Lich") &&
+                                !oracle.canRecruit("Lich") &&
+                                !legion.contains("Death") &&
+                                legion.numCreature("Shade") == 2 &&
+                                legion.getHeight() != 6 &&
+                                oracle.canReach("Circle") &&
+                                oracle.creatureAvailable("Ghost") >= 2
+                                )
+                        {
+                                return "Shade";
+                        }
+                }
+                else if (terrain.equals("Tombs"))
+                {
+                        if(recruits.contains("Ghost") &&
+                                !legion.contains("Wraith") &&
+                                !legion.contains("Beelzebub") &&
+                                !legion.contains("Death") &&
+                                !oracle.canRecruit("Death") &&
+                                legion.numCreature("Ghost") == 2 &&
+                                legion.getHeight() != 6 &&
+                                oracle.canReach("Circle") &&
+                                oracle.creatureAvailable("Wraith") >= 2
+                                )
+                        {
+                                return "Ghost";
+                        }
+                }
+                else if (terrain.equals("Desert"))
+                {
+                        if(recruits.contains("Djinn") &&
+                                !legion.contains("Manticore") &&
+                                legion.numCreature("Djinn") == 2 &&
+                                legion.getHeight() != 6 &&
+                                oracle.canReach("Hills") &&
+                                oracle.creatureAvailable("Manticore") >= 1
+                                )
+                        {
+                                return "Djinn";
+                        }
+                }
+                else if (terrain.equals("Swamp"))
+                {
+                        if(recruits.contains("Ent") &&
+                                !legion.contains("Unicorn") &&
+                                legion.numCreature("Ent") == 2 &&
+                                legion.getHeight() != 6 &&
+                                oracle.canReach("Woods") &&
+                                oracle.creatureAvailable("Unicorn") >= 1
+                                )
+                        {
+                                return "Ent";
+                        }
+                }
+                else if (terrain.equals("Tower"))
+                {
+                        if(recruits.contains("Skeleton") &&
+                                legion.contains("Skeleton") &&
+                                legion.numCreature("Skeleton") == 2 &&
+                                !oracle.canRecruit("Golem") &&
+                                (legion.getHeight() != 6 ||
+                                (oracle.canReach("Tombs")
+                                && !legion.contains("Zombie")
+                                && oracle.creatureAvailable("Zombie") >= 2)
+                                ))
+                        {
+                                return "Skeleton";
+                        }
+                        if (recruits.contains("Skeleton") &&
+                                legion.contains("Skeleton") &&
+                                legion.numCreature("Skeleton") == 1 &&
+                                !legion.contains("Shade") &&
+                                !oracle.canRecruit("Golem") &&
+                                oracle.creatureAvailable("Shade") >= 2
+                                )
+                        {
+                                return "Skeleton";
+                        }
+                        if(recruits.contains("Skeleton") &&
+                                !legion.contains("Skeleton") &&
+                                !legion.contains("Shade") &&
+                                legion.getHeight() != 6 &&
+                                oracle.creatureAvailable("Shade") >= 6 &&
+                                !oracle.canRecruit("Golem")
+                                )
+                        {
+                                return "Skeleton";
+                        }
+                        if(recruits.contains("Goblin") &&
+                                legion.contains("Goblin") &&
+                                legion.numCreature("Goblin") == 2 &&
+                                !oracle.canRecruit("Golem") &&
+                                (legion.getHeight() != 6 ||
+                                        (oracle.canReach("Hills") &&
+                                        !legion.contains("Minotaur") &&
+                                        oracle.creatureAvailable("Minotaur")
+                                        >= 2))
+                                )
+                        {
+                                return "Goblin";
+                        }
+                        if(recruits.contains("Goblin") &&
+                                legion.contains("Goblin") &&
+                                legion.numCreature("Goblin") == 1 &&
+                                !legion.contains("Naga") &&
+                                !oracle.canRecruit("Golem") &&
+                                oracle.creatureAvailable("Naga") >= 2
+                                )
+                        {
+                                return "Goblin";
+                        }
+                        if(recruits.contains("Goblin") &&
+                                !legion.contains("Goblin") &&
+                                !legion.contains("Naga") &&
+                                legion.getHeight() != 6 &&
+                                oracle.creatureAvailable("Naga") >= 6 &&
+                                !oracle.canRecruit("Golem")
+                                )
+                        {
+                                return "Goblin";
+                        }
+                        if (recruits.contains("Centaur") &&
+                                legion.contains("Centaur") &&
+                                legion.numCreature("Centaur") == 2 &&
+                                !oracle.canRecruit("Golem") &&
+                                (legion.getHeight() != 6 ||
+                                        (oracle.canReach("Woods") &&
+                                                !legion.contains("Warbear") &&
+                                                oracle.creatureAvailable
+                                                        ("Warbear") >= 2))
+                                )
+                        {
+                                return "Centaur";
+                        }
+                        if (recruits.contains("Centaur") &&
+                                legion.contains("Centaur") &&
+                                legion.numCreature("Centaur") == 1 &&
+                                !legion.contains("Lion") &&
+                                !oracle.canRecruit("Golem") &&
+                                oracle.creatureAvailable("Lion") >= 2
+                                )
+                        {
+                                return "Centaur";
+                        }
+                        if (recruits.contains("Centaur") &&
+                                !legion.contains("Centaur") &&
+                                !legion.contains("Lion") &&
+                                legion.getHeight() != 6 &&
+                                oracle.creatureAvailable("Lion") >= 6 &&
+                                !oracle.canRecruit("Golem")
+                                )
+                        {
+                                return "Centaur";
+                        }
+                }
+                return ((Creature)recruits.get(recruits.size() - 1)).getName();
+        }
+
+        public List getInitialSplitHint(String label, String[] section)
+        {
+                List li = new ArrayList();
+                if (label.equals("100"))
+                {
+                        if(rnd.nextFloat() < 0.5)
+                        {
+                                li.add("Titan");
+                                li.add("Skeleton");
+                                li.add("Centaur");
+                                li.add("Centaur");
+                        }
+                        else
+                        {
+                                li.add("Titan");
+                                li.add("Skeleton");
+                                li.add("Skeleton");
+                                li.add("Goblin");
+                        }
+                }
+                else if (label.equals("200"))
+                {
+                        li.add("Titan");
+                        li.add("Goblin");
+                        li.add("Goblin");
+                        li.add("Skeleton");
+                }
+                else if (label.equals("300"))
+                {
+                        if(rnd.nextFloat() < 0.5)
+                        {
+                                li.add("Titan");
+                                li.add("Skeleton");
+                                li.add("Centaur");
+                                li.add("Centaur");
+                        }
+                        else
+                        {
+                                li.add("Titan");
+                                li.add("Skeleton");
+                                li.add("Skeleton");
+                                li.add("Goblin");
+                        }
+                }
+                else if (label.equals("400"))
+                {
+                        li.add("Titan");
+                        li.add("Goblin");
+                        li.add("Goblin");
+                        li.add("Skeleton");
+
+                }
+                else if (label.equals("500"))
+                {
+                        if(rnd.nextFloat() < 0.5)
+                        {
+                                li.add("Titan");
+                                li.add("Skeleton");
+                                li.add("Centaur");
+                                li.add("Centaur");
+                        }
+                        else
+                        {
+                                li.add("Titan");
+                                li.add("Skeleton");
+                                li.add("Skeleton");
+                                li.add("Goblin");
+                        }
+                }
+                else if (label.equals("600"))
+                {
+                        li.add("Titan");
+                        li.add("Goblin");
+                        li.add("Goblin");
+                        li.add("Skeleton");
+                }
+                else
+                {
+                        throw new RuntimeException("Bad hex label " + label);
+                }
+                return li;
+}
+
+        public int getHintedRecruitmentValueOffset(String name,
+                String[] section)
+        {
+                if(name.equals("Imp"))
+                {
+                        return -5;
+                }
+                else if(name.equals("Devil"))
+                {
+                        return 10;
+                }
+                return 0;
+        }
+}
---
+++ Colossus/Beelzebub/README.txt
@@ -0,0 +1,72 @@
+README for Beelzebub v0.2 alpha 2
+A variant by Chris Byler and Jay Delanoy
+
+This variant is an attempt to combine the best features of the
+Abyssal, Extended and Undead variants, with a careful eye on
+the balance of the recruiting trees.
+
+Major changes:
+1. Brush/Jungle track replaced with undead.  Tombs and Circle battlelands 
+now have Stones (tombstones) instead of Trees.  Shades, Ghosts, Spectres, 
+Phantoms and Wraiths can enter Stones and have a native bonus there - 
+non-native attackers lose a Skill factor.  Both tracks lead up to Death, 
+who is a unique 16-3 flying summonable lord.  Death can recruit down in 
+two terrains (to Liches and Mummies), but since he is unique he cannot
+recruit more of himself.  Liches and Wraiths are the only recruitable 
+magic rangestrikers (except for Beelzebub, see below).
+
+2. New creatures added to the tops of most recruiting trees.  Unicorns
+and Hydras moved further up their trees and now only native to one
+terrain, but their abilities have been improved (greatly so for
+Unicorns).  Colossi now only native to mountains - 2 Giants in Tundra 
+now get a Mammoth (24-2).
+
+3. Masterboard considerably rearranged.  Some Hills/Woods moved to the
+outer ring ("bus").
+
+4. Some creature stats added, and recruiting trees modified.
+
+5. New terrain type: Hellmouth.  Any legion on a Hellmouth can recruit
+an Imp.  Otherwise the recruiting ladder is 2 Imps, 3 Devils, 2 Fiends, 
+Beelzebub.  Beelzebub is a unique summonable 13-4 flying magic 
+rangestriking lord.  If killed he can be recruited again.  The Hellmouth 
+is an unpleasant place to defend, although not quite as harsh as in 
+Abyssal.  Note that Devils can also be earned on points for battles won
+in Tombs, Ruins or Hellmouth, and Devils, Fiends and Beelzebub are all
+summonable.  Beelzebub can also be recruited by 2 Wraiths in a Circle.
+
+6. Many new earned creatures - elementals, spectres, devils and
+archdevils, mages, archmages and phantoms.  Elementals, spectres and
+phantoms get native bonuses in their respective terrains.  Devils, 
+archdevils, archmages and phantoms are summonable, but elementals, 
+spectres and mages are not.  Archdevils, archmages and phantoms are 
+also lords (they can tower teleport and are immune to normal 
+rangestrikes).
+
+Terrain             Earned (100pts)         Earned(500pts)
+
+Plains              Angel, AirElemental     Archangel
+Woods               Angel, EarthElemental   Archangel
+Desert              Angel, FireElemental    Archangel
+Marsh               Angel, WaterElemental   Archangel
+Hills               Angel, EarthElemental   Archangel
+Swamp               Angel, WaterElemental   Archangel
+Mountains           Angel, FireElemental    Archangel
+Tundra              Angel, AirElemental     Archangel
+Ruins               Devil, Spectre          Archdevil
+Tombs               Devil, Spectre          Phantom
+Tower               Angel, Mage             Archangel, Archmage
+Hellmouth           Devil, Mage             Archdevil, Archmage
+Circle              Spectre, Mage           Phantom, Archmage
+
+7. Titans are skill 4, gain power every 100 points and Titan Teleport
+at 600 points.
+
+8. Warlocks have been eliminated.  Mages and Archmages must be earned 
+on points for battles fought in Towers, Hellmouths or Circles.  However, 
+they are both more powerful than Warlocks.  Titans no longer get any 
+special recruit.
+
+9. Alpha 2 changes: Wraiths 12*!3, Mummies 18-2, 3 Ghost->Wraith,
+2 Wraith->Beelzebub, replaced Efreet with Djinn (6*|4, native to sand,
+dunes and slopes), fixed some errors in the readme.
