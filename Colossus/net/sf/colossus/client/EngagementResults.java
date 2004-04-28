12ce0434d1af9cc885f144da900ce1c5af598688
Peter Becker <peter@peterbecker.de>
2004-04-28 08:39:18 +0000

    Some EngagementResults enhancements.
    - dialog remembers position between session
    - dialog remembers all engagements
    - first unacknowledged result will be shown on popup, bnrowsing backwards and
      forwards is possible.
    
    
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2507 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/net/sf/colossus/client/EngagementResults.java
+++ Colossus/net/sf/colossus/client/EngagementResults.java
@@ -7,16 +7,16 @@ import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -39,10 +39,13 @@ import net.sf.colossus.util.Options;
 
 final class EngagementResults
     extends KDialog
-    implements ActionListener, WindowListener
 {
     private IOracle oracle;
     private IOptions options;
+    private int current = -1;
+    private int lastSeen = -1;
+    private ArrayList engagementLog = new ArrayList();
+	private SaveWindow saveWindow;
 
     /** 
      * inits the diaolog, not opens it.
@@ -57,12 +60,12 @@ final class EngagementResults
         this.oracle = oracle;
         this.options = options;
         setBackground(Color.lightGray);
-        addWindowListener(this);
+        setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+        
+        this.saveWindow = new SaveWindow(options, "EngagementResultView");
+        saveWindow.restore(this, new Point(0,0));
     }
 
-
-    private int current = -1;
-    private ArrayList engagementLog = new ArrayList();
     /** adds a log record to the list of logged engagements.
      *
      * TODO: see if xxxStartingCertainities can somehow get values
@@ -88,128 +91,47 @@ final class EngagementResults
         List defenderStartingCertainities
         )
     {
-        EngagementLogEntry elog = new EngagementLogEntry();
-
-        elog.hexLabel = oracle.getBattleSite();
-        elog.attackerId = oracle.getAttackerMarkerId();
-        elog.defenderId = oracle.getDefenderMarkerId();
-        elog.battleTurn = turns;
-        elog.gameTurn = oracle.getTurnNumber();
-
-        elog.attackerStartingContents = attackerStartingContents;
-        elog.defenderStartingContents = defenderStartingContents;
-        elog.attackerStartingCertainities = attackerStartingCertainities;
-        elog.defenderStartingCertainities = defenderStartingCertainities;
-
-        elog.attackerEndingContents =
-            oracle.getLegionImageNames(elog.attackerId);
-        elog.defenderEndingContents =
-            oracle.getLegionImageNames(elog.defenderId);
-        elog.attackerEndingCertainities =
-            oracle.getLegionCreatureCertainties(elog.attackerId);
-        elog.defenderEndingCertainities =
-            oracle.getLegionCreatureCertainties(elog.defenderId);
-
-        elog.setWinnerId(winnerId);
-        elog.setMethod(method);
-        elog.points = points;
-
+        Engagement result = new Engagement(winnerId,
+                method,
+                points,
+                turns,
+                attackerStartingContents,
+                defenderStartingContents,
+                attackerStartingCertainities,
+                defenderStartingCertainities,
+                oracle
+        );       
+        EngagementLogEntry elog = new EngagementLogEntry(result);
         engagementLog.add(elog);
-        current = engagementLog.size() - 1;
+        
+        if(this.current == -1)
+        {
+        	this.current = 0;
+        }
 
         showCurrent();
     }
 
 
     /** one log entry. can show itself into a swing container.
-     * TODO: make fields private and write a constructor with many args.
+     * @todo the model part has been extracted into ths Engagement class,
+     * this class should disappear now in favour of doing the GUI layout only once
+     * in the outer class.
      */
     private class EngagementLogEntry
     {
-        int gameTurn;
-        String attackerId;
-        String defenderId;
-        String hexLabel;
-        int battleTurn;
-        String winnerId;
-        String loserId;
-        String method;
-        String _result; // long string set with method
-        int points;
-        // image names        
-        List attackerStartingContents; 
-        List defenderStartingContents;
-        List attackerEndingContents;
-        List defenderEndingContents;
-        // certainities
-        List attackerStartingCertainities; 
-        List defenderStartingCertainities;
-        List attackerEndingCertainities;
-        List defenderEndingCertainities;
+        private Engagement result;
 
-        /** by setting the winnerId the loserId is also set. */
-        void setWinnerId(String aWinnerId)
-        {
-            loserId = null;
-            winnerId = aWinnerId;
-            if (winnerId != null)
-            {
-                if (winnerId.equals(attackerId))
-                {
-                    loserId = defenderId;
-                }
-                else if (winnerId.equals(defenderId))
-                {
-                    loserId = attackerId;
-                }
-                else
-                {
-                    winnerId=null;
-                }
-            }
-        }
-
-        /** also calculates the '_result_ string for display. */
-        void setMethod(String aMethod)
-        {
-            this.method = aMethod;
-            this._result = "bogus method";
-            if (method.equals("flee"))
-            {
-                _result = winnerId + " won when " + loserId + " fled";
-            }
-            else if (method.equals("concede"))
-            {
-                _result = winnerId + " won when " + loserId + " conceded";
-            }
-            else if (method.equals("negotiate"))
-            {
-                if (winnerId != null)
-                {
-                    _result = winnerId + " won a negotiated settlement";
-                }
-                else
-                {
-                    _result = "Negotiated mutual elimination";
-                }
-            }
-            else if (method.equals("fight"))
-            {
-                if (winnerId != null)
-                {
-                    _result = winnerId + " won the battle in "
-                        + battleTurn +  " turns";
-                }
-                else
-                {
-                    _result = "Mutual elimination in "
-                        + battleTurn + " turns";
-                }
-            }
-        }
+		public EngagementLogEntry(Engagement result) {
+			this.result = result;
+		}
 
         /** make some of the background colors more distiguishable */
         private final static boolean COLORFUL = true;
+		private JButton firstButton;
+		private JButton prevButton;
+		private JButton nextButton;
+		private JButton lastButton;
         /** like toString into a swing component.
          * the current rough layout is:
          * <pre>
@@ -229,12 +151,12 @@ final class EngagementResults
          *  # +---------------+  %  +---------------------+ #
          *  #===============================================#
          *  # +-South:FlowLayout(left)--------------------+ #
-         *  # |  -prev-  -next-  -dismiss-                | #
+         *  # |  -buttons-                                | #
          *  # +-------------------------------------------+ #
          *  #################################################
          * </pre>
          */
-        void show(Container cnt, ActionListener al)
+        void show(Container cnt)
         {
             // setup the containers
             cnt.removeAll();
@@ -272,65 +194,102 @@ final class EngagementResults
             // add elements
             //
             //  north
-            panelNorth.add(new JLabel("Turn " + gameTurn));
-            panelNorth.add(new JLabel(attackerId + " attacked " + defenderId
+            panelNorth.add(new JLabel("Turn " + result.gameTurn));
+            panelNorth.add(new JLabel(result.attackerId + " attacked " + result.defenderId
                 + " in "
-                + MasterBoard.getHexByLabel(hexLabel).getDescription()));
-            panelNorth.add(new JLabel(_result));
-            if(winnerId!=null)
+                + MasterBoard.getHexByLabel(result.hexLabel).getDescription()));
+            panelNorth.add(new JLabel(result.getResultText()));
+            if(result.winnerId!=null)
             {
-                panelNorth.add(new JLabel(winnerId + " earned "
-                    + points + " points"));
+                panelNorth.add(new JLabel(result.winnerId + " earned "
+                    + result.points + " points"));
             }
 
             // west
             panelWest.add(new JLabel("Atacker"));
-            JLabel l = new JLabel(attackerId);
+            JLabel l = new JLabel(result.attackerId);
             l.setForeground(Color.BLUE);
             panelWest.add(l);
             panelWest.add(new JLabel("Defender"));
-            l = new JLabel(defenderId);
+            l = new JLabel(result.defenderId);
             l.setForeground(Color.BLUE);
             panelWest.add(l);
             panelWest.add(new JLabel("Winner"));
 
 
             //  center
-            _legion(attackerId,
-                attackerStartingContents, attackerStartingCertainities,
+            _legion(result.attackerId,
+                result.attackerStartingContents, result.attackerStartingCertainities,
                 panelCenter, true, false, false);
 
-            _legion(defenderId,
-                defenderStartingContents, defenderStartingCertainities,
+            _legion(result.defenderId,
+                result.defenderStartingContents, result.defenderStartingCertainities,
                 panelCenter, true, true, false);
-            if (attackerId.equals(winnerId))
+            if (result.attackerId.equals(result.winnerId))
             {
-                _legion(attackerId,
-                    attackerEndingContents, attackerEndingCertainities,
+                _legion(result.attackerId,
+                    result.attackerEndingContents, result.attackerEndingCertainties,
                     panelCenter, false, false, false);
             }
-            else if (defenderId.equals(winnerId))
+            else if (result.defenderId.equals(result.winnerId))
             {
-                _legion(defenderId,
-                defenderEndingContents, defenderEndingCertainities,
+                _legion(result.defenderId,
+                result.defenderEndingContents, result.defenderEndingCertainties,
                 panelCenter, false, true, false);
             }
  
             //  south
-            JButton prevButton = new JButton(PREV);
-            prevButton.setActionCommand(PREV);
-            prevButton.addActionListener(al);
-            panelSouth.add(prevButton);
+            this.firstButton = new JButton("First");
+            firstButton.addActionListener(new ActionListener(){
+            	public void actionPerformed(ActionEvent e){
+                    current = 0;
+                    showCurrent();
+            	}    
+            });
+            panelSouth.add(firstButton);
+            this.firstButton.setEnabled(current != 0);
 
-            JButton nextButton = new JButton(NEXT);
-            nextButton.addActionListener(al);
-            nextButton.setActionCommand(NEXT);
+            this.prevButton = new JButton("Previous");
+            prevButton.addActionListener(new ActionListener() {
+            	public void actionPerformed(ActionEvent e) {
+                    current--;
+                    showCurrent();
+            	}   
+            });
+            panelSouth.add(prevButton);
+            this.prevButton.setEnabled(current != 0);
+
+            if(current != engagementLog.size()-1)
+            {
+                this.nextButton = new JButton("Next");
+                nextButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        current++;
+                        showCurrent();
+        			}   
+                });
+            }
+            else
+            {
+                this.nextButton = new JButton("Hide");
+                nextButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        current++;
+                        hide();
+                    }   
+                });
+            }   
             panelSouth.add(nextButton);
 
-            JButton dropButton = new JButton(DROP);
-            dropButton.addActionListener(al);
-            dropButton.setActionCommand(DROP);
-            panelSouth.add(dropButton);
+            this.lastButton = new JButton("Last");
+            lastButton.addActionListener(new ActionListener() {
+            	public void actionPerformed(ActionEvent e) {
+                    current = engagementLog.size() - 1;
+                    showCurrent();
+				}
+            });
+            panelSouth.add(lastButton);
+            this.lastButton.setEnabled(current != engagementLog.size()-1);
 
             // finish
         }
@@ -404,9 +363,10 @@ final class EngagementResults
             EngagementLogEntry elog =
                 (EngagementLogEntry) engagementLog.get(current);
             Container contentPane = getContentPane();
-            elog.show(contentPane, this);
+            elog.show(contentPane);
             this.setTitle("Engagement " + (current + 1)
                 + " of " + engagementLog.size());
+            this.lastSeen = Math.max(this.lastSeen, this.current);
         }
         maybeShow();
     }
@@ -431,46 +391,136 @@ final class EngagementResults
         }
     }
 
-
-    // button actions and labels
-    private final static String NEXT = "Next";
-    private final static String PREV = "Previous";
-    private final static String DROP = "Drop";
-
-    /** handler for buttons */
-    public void actionPerformed(ActionEvent e)
+    public void dispose()
     {
-        String cmd = e.getActionCommand();
-        if (cmd.equals(NEXT))
+        saveWindow.save(this);
+        super.dispose();
+    }
+
+    public void hide()
+    {
+        saveWindow.save(this);
+        super.hide();
+    }
+
+    /**
+     * Stores information about an engagement.
+     * 
+     * @todo: it might be useful to have this as a proper toplevel class
+     */
+    private class Engagement
+    {
+        String winnerId; // null on mutual elim, flee, concede, negotiate
+        String loserId;
+		String attackerId;
+        String defenderId;
+        String method;
+        int points;
+        int turns;
+        List attackerStartingContents;
+        List defenderStartingContents;
+        List attackerStartingCertainities;
+        List defenderStartingCertainities;
+		String hexLabel;
+		int gameTurn;
+		List attackerEndingContents;
+		List defenderEndingContents;
+		List attackerEndingCertainties;
+		List defenderEndingCertainties;
+        
+		public Engagement(
+		    String winnerId, 
+            String method, 
+            int points,
+			int turns, 
+            List attackerStartingContents,
+			List defenderStartingContents,
+			List attackerStartingCertainities,
+			List defenderStartingCertainities,
+            IOracle oracle
+            )
         {
-            current += 1;
-        }
-        else if (cmd.equals(PREV))
+			this.winnerId = winnerId;
+			this.method = method;
+			this.points = points;
+			this.turns = turns;
+			this.attackerStartingContents = attackerStartingContents;
+			this.defenderStartingContents = defenderStartingContents;
+			this.attackerStartingCertainities = attackerStartingCertainities;
+			this.defenderStartingCertainities = defenderStartingCertainities;
+            this.hexLabel = oracle.getBattleSite();
+            this.attackerId = oracle.getAttackerMarkerId();
+            this.defenderId = oracle.getDefenderMarkerId();
+            this.gameTurn = oracle.getTurnNumber();
+
+            this.attackerEndingContents =
+                oracle.getLegionImageNames(this.attackerId);
+            this.defenderEndingContents =
+                oracle.getLegionImageNames(this.defenderId);
+            this.attackerEndingCertainties =
+                oracle.getLegionCreatureCertainties(this.attackerId);
+            this.defenderEndingCertainties =
+                oracle.getLegionCreatureCertainties(this.defenderId);
+
+            this.setWinnerAndLoserId();
+		}
+
+        private void setWinnerAndLoserId()
         {
-            current -= 1;
-        }
-        else if (cmd.equals(DROP))
-        {
-            if (engagementLog.size() >= 1)
+            this.loserId = null;
+            if (this.winnerId != null)
             {
-                engagementLog.remove(current);
-                current -= 1;
+                if (this.winnerId.equals(this.attackerId))
+                {
+                    this.loserId = this.defenderId;
+                }
+                else if (this.winnerId.equals(this.defenderId))
+                {
+                    this.loserId = this.attackerId;
+                }
+                else
+                {
+                    this.winnerId=null; // @todo is this case possible at all? What does it mean?
+                }
             }
         }
-        // show
-        current = between(0, current, engagementLog.size() - 1); // sanity
-        showCurrent();
-    }
 
-    /** min max cascade */
-    private final int between(int min, int val, int max)
-    {
-        return Math.min(Math.max(min, val), max);
-    }
-
-    /** just close it */
-    public void windowClosing(WindowEvent e)
-    {
-        options.setOption(Options.showEngagementResults, false);
+        public String getResultText()
+        {
+            String result = "bogus method";
+            if (method.equals("flee"))
+            {
+                result = winnerId + " won when " + loserId + " fled";
+            }
+            else if (method.equals("concede"))
+            {
+                result = winnerId + " won when " + loserId + " conceded";
+            }
+            else if (method.equals("negotiate"))
+            {
+                if (winnerId != null)
+                {
+                    result = winnerId + " won a negotiated settlement";
+                }
+                else
+                {
+                    result = "Negotiated mutual elimination";
+                }
+            }
+            else if (method.equals("fight"))
+            {
+                if (winnerId != null)
+                {
+                    result = winnerId + " won the battle in "
+                        + this.turns +  " turns";
+                }
+                else
+                {
+                    result = "Mutual elimination in "
+                        + this.turns + " turns";
+                }
+            }
+            return result;
+        }
     }
 }
--- Colossus/net/sf/colossus/client/SaveWindow.java
+++ Colossus/net/sf/colossus/client/SaveWindow.java
@@ -4,6 +4,7 @@ package net.sf.colossus.client;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.Window;
 
 import net.sf.colossus.util.Options;
 
@@ -60,6 +61,27 @@ public final class SaveWindow
     {
         options.setOption(name + Options.locX, location.x);
         options.setOption(name + Options.locY, location.y);
+    }
+    
+    public void save(Window window ) {
+        saveLocation(window.getLocation());
+        saveSize(window.getSize());
+    }
+    
+    public void restore(Window window, Point defaultLocation) {
+        Point location = loadLocation();
+        if (location == null)
+        {
+            location = defaultLocation;
+        }
+        window.setLocation(location);
+
+        Dimension size = loadSize();
+        if (size == null)
+        {
+            size = window.getPreferredSize();
+        }
+        window.setSize(size);
     }
 }
 
