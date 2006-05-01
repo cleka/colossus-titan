8029abe654157ed0090773ee420be97745d3474b
Peter Becker <peter@peterbecker.de>
2006-05-01 13:27:47 +0000

    Added missing files.
    I forgot to add the ticks for the new files on the last commit.
    
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2555 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/net/sf/colossus/server/RemoteLogHandler.java
@@ -0,0 +1,42 @@
+package net.sf.colossus.server;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+
+/**
+ * A java.util.logging Handler that appends to a <code>Server</code> via <code>allLog</code>.
+ */
+public class RemoteLogHandler extends Handler
+{
+    private Server server = null;
+
+    public void setServer(Server server)
+    {
+        this.server = server;
+    }
+
+    public RemoteLogHandler()
+    {
+        super();
+    }
+
+    public boolean requiresLayout()
+    {
+        return true;
+    }
+
+    public void close()
+    {
+        server = null;
+    }
+
+	public void publish(LogRecord record) {
+        if (server != null) {
+            server.allLog(record.toString());
+        }
+	}
+
+	public void flush() {
+	}
+}
---
+++ Colossus/net/sf/colossus/util/SwingDocumentLogHandler.java
@@ -0,0 +1,58 @@
+package net.sf.colossus.util;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+/**
+ * A java.util.logging Handler that appends to a <code>javax.swing.text.Document</code>.
+ *
+ * @author Barrie Treloar
+ * @author Peter Becker
+ */
+public class SwingDocumentLogHandler extends Handler
+{
+	private static final Logger LOGGER = Logger.getLogger(SwingDocumentLogHandler.class.getName());
+	
+    private Document document = new PlainDocument();
+
+    public SwingDocumentLogHandler()
+    {
+        super();
+    }
+
+    public Document getDocument()
+    {
+        return document;
+    }
+
+	public void publish(LogRecord record) {
+        try
+        {
+            document.insertString(document.getLength(), record.getMessage() + "\n", null);
+        } catch (BadLocationException e)
+        {
+            LOGGER.log(Level.SEVERE, "append() call failed on document.", e);
+        }
+	}
+
+	public void flush() {
+		// nothing to do, just making the method concrete
+	}
+
+	public void close() throws SecurityException {
+        try
+        {
+            document.remove(0, document.getLength());
+            document = null;
+        } catch (BadLocationException e)
+        {
+            LOGGER.log(Level.SEVERE, "close() call failed on document.", e);
+        }
+	}
+}
