eac99e8c15676de5ec9966511b22eba6b2f7ad54
Peter Becker <peter@peterbecker.de>
2006-05-06 09:58:04 +0000

    Added log configuration to rotate 3 files a 1MB in the temp dir.
    This configuration is used by the start scripts now.
    
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2561 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/run.bat
+++ Colossus/run.bat
@@ -1,1 +1,1 @@
-java -Xmx128M -jar Colossus.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djava.util.logging.config.file=logging.properties -Xmx128M -jar Colossus.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
--- Colossus/runclient.bat
+++ Colossus/runclient.bat
@@ -1,1 +1,1 @@
-java net.sf.colossus.server.Start -c %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djava.util.logging.config.file=logging.properties net.sf.colossus.server.Start -c %1 %2 %3 %4 %5 %6 %7 %8 %9
