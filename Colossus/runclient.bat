3553f19d335f83e3a4bc06812303440c053b8e14
Clemens Katzer <github-cleka@cleka.net>
2007-05-04 19:34:08 +0000

    Changed to use also the -jar ... form instead of net.sf....
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2676 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/runclient.bat
+++ Colossus/runclient.bat
@@ -1,1 +1,1 @@
-java -Djava.util.logging.config.file=logging.properties net.sf.colossus.server.Start -c %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djava.util.logging.config.file=logging.properties -Xmx128M -jar Colossus.jar -c %1 %2 %3 %4 %5 %6 %7 %8 %9
