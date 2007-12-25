e0a17b5a52ee1ecd04ab8615cc2d49a9f9870df2
Peter Becker <peter@peterbecker.de>
2007-12-25 22:31:34 +0000

    Added a little formatter class to make logging messages more brief.
    It pretty much reduces logging to what you normally would do with System.out -- just that you can configure it instead of having to print to stdout directly from your code.
    
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2916 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/core/src/main/java/net/sf/colossus/util/VerySimpleFormatter.java
@@ -0,0 +1,53 @@
+package net.sf.colossus.util;
+
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+
+/**
+ * This is a logging formatter doing not much more than the bare minimum.
+ * 
+ * We don't log the source class/method, we log the level only if WARNING
+ * or SEVERE. Exceptions are logged if given, but otherwise it is more like
+ * printing to stdout, just that it is configurable and it is possible to have
+ * a more verbose log in parallel. 
+ */
+public class VerySimpleFormatter extends Formatter
+{
+    public static final String LINE_SEPARATOR = System
+        .getProperty("line.separator");
+
+    public String format(LogRecord record)
+    {
+        StringBuffer sb = new StringBuffer();
+        String message = formatMessage(record);
+        if ((record.getLevel() == Level.SEVERE)
+            || (record.getLevel() == Level.WARNING))
+        {
+            sb.append(record.getLevel().getLocalizedName());
+            sb.append(": ");
+        }
+        sb.append(message);
+        sb.append(LINE_SEPARATOR);
+        if (record.getThrown() != null)
+        {
+            try
+            {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                record.getThrown().printStackTrace(pw);
+                pw.close();
+                sb.append(sw.toString());
+            }
+            catch (Exception ex)
+            {
+                // really should work
+            }
+        }
+        return sb.toString();
+    }
+}
