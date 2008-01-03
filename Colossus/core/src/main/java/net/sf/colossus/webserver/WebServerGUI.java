d5519d8772597531673aaee1dea83a029996ca9f
Clemens Katzer <github-cleka@cleka.net>
2008-01-03 07:31:45 +0000

    Removed unused import.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@2954 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/webserver/WebServerGUI.java
+++ Colossus/core/src/main/java/net/sf/colossus/webserver/WebServerGUI.java
@@ -11,7 +11,6 @@ import java.awt.event.WindowListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
