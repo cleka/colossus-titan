9c8568b30209f18222f2c7f34936cf530c74184e
Peter Becker <peter@peterbecker.de>
2008-01-09 13:19:25 +0000

    Removed unused member, updated some JavaDoc.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@3001 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/variant/HazardTerrain.java
+++ Colossus/core/src/main/java/net/sf/colossus/variant/HazardTerrain.java
@@ -18,15 +18,6 @@ public class HazardTerrain
     private final String name;
 
     /**
-     * A token used to represent the terrain.
-     * 
-     * Must be unique over all hazard terrains.
-     * 
-     * TODO: do we need this?
-     */
-    private final Character token;
-
-    /**
      * Determines if native characters get a bonus in this terrain.
      */
     private final boolean isNativeBonusTerrain;
@@ -63,17 +54,14 @@ public class HazardTerrain
 
     /**
      * A map from the serialization string of a terrain to the instances.
-     * 
-     * This is a Map<String,HazardTerrain>.
      */
     private final static Map<String, HazardTerrain> TERRAIN_MAP = new HashMap<String, HazardTerrain>();
 
-    public HazardTerrain(String name, char token, boolean nativeBonus,
+    public HazardTerrain(String name, boolean nativeBonus,
         boolean nonNativePenalty, boolean nativeOnly, boolean flyerOnly,
         boolean nativeFlyerOnly, boolean slowsNonNative, boolean blocksFlying)
     {
         this.name = name;
-        this.token = new Character(token);
         this.isNativeBonusTerrain = nativeBonus;
         this.isNonNativePenaltyTerrain = nonNativePenalty;
         this.isNativeOnly = nativeOnly;
@@ -81,18 +69,12 @@ public class HazardTerrain
         this.isNativeFlyersOnly = nativeFlyerOnly;
         this.slowsNonNative = slowsNonNative;
         this.blocksFlying = blocksFlying;
-        assert TERRAIN_MAP.get(this.token) == null : "Duplicate terrain token not allowed";
         TERRAIN_MAP.put(name, this);
     }
 
     public String getName()
     {
         return name;
-    }
-
-    public char getToken()
-    {
-        return token.charValue();
     }
 
     public boolean isNativeBonusTerrain()
@@ -135,38 +117,46 @@ public class HazardTerrain
         return TERRAIN_MAP.get(name);
     }
 
+    /**
+     * Returns all available hazard terrains.
+     * 
+     * This is not variant-specific, any terrain known to the program is listed even
+     * if it is not available in the current variant.
+     * 
+     * TODO this should really be a question to ask a variant instance
+     */
     public static final Collection<HazardTerrain> getAllHazardTerrains()
     {
         return TERRAIN_MAP.values();
     }
 
     public static final HazardTerrain PLAINS = new HazardTerrain("Plains",
-        'p', false, false, false, false, false, false, false);
+        false, false, false, false, false, false, false);
 
     public static final HazardTerrain BRAMBLES = new HazardTerrain("Brambles",
-        'r', true, true, false, false, false, false, false);
+        true, true, false, false, false, false, false);
 
-    public static final HazardTerrain SAND = new HazardTerrain("Sand", 's',
-        false, false, false, false, false, false, false);
+    public static final HazardTerrain SAND = new HazardTerrain("Sand", false,
+        false, false, false, false, false, false);
 
-    public static final HazardTerrain TREE = new HazardTerrain("Tree", 't',
-        false, false, true, false, false, false, false);
+    public static final HazardTerrain TREE = new HazardTerrain("Tree", false,
+        false, true, false, false, false, false);
 
-    public static final HazardTerrain BOG = new HazardTerrain("Bog", 'o',
-        false, false, true, false, false, false, false);
+    public static final HazardTerrain BOG = new HazardTerrain("Bog", false,
+        false, true, false, false, false, false);
 
     public static final HazardTerrain VOLCANO = new HazardTerrain("Volcano",
-        'v', true, false, true, false, true, false, false);
+        true, false, true, false, true, false, false);
 
-    public static final HazardTerrain DRIFT = new HazardTerrain("Drift", 'd',
+    public static final HazardTerrain DRIFT = new HazardTerrain("Drift",
         false, true, false, false, false, false, false);
 
-    public static final HazardTerrain TOWER = new HazardTerrain("Tower", 'w',
+    public static final HazardTerrain TOWER = new HazardTerrain("Tower",
         false, false, false, false, false, false, false);
 
-    public static final HazardTerrain LAKE = new HazardTerrain("Lake", 'l',
-        false, false, true, false, false, false, false);
+    public static final HazardTerrain LAKE = new HazardTerrain("Lake", false,
+        false, true, false, false, false, false);
 
-    public static final HazardTerrain STONE = new HazardTerrain("Stone", 'n',
+    public static final HazardTerrain STONE = new HazardTerrain("Stone",
         false, false, true, false, false, false, true);
 }
