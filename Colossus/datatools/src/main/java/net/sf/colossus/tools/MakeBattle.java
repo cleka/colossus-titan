85e3946e6839f5e056af8c11e34210a41aeaa2fc
Romain Dolbeau <romain@dolbeau.org>
2009-02-26 17:09:35 +0000

    add the tools/ files
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@3646 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/datatools/src/main/java/net/sf/colossus/tools/MakeBattle.java
@@ -0,0 +1,170 @@
+package net.sf.colossus.tools;
+
+
+// DOM classes.
+import org.w3c.dom.*;
+//JAXP 1.1
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import javax.xml.transform.dom.*;
+
+public class MakeBattle {
+
+    static void addCreature(Document doc, String legion, String creature) {
+        Element root = doc.getDocumentElement();
+        NodeList l;
+        int n;
+
+        /* take from caretaker */
+        l = root.getElementsByTagName("Caretaker");
+        n = l.getLength();
+        for (int i = 0; i < n; i++) {
+            Element c = (Element) l.item(i);
+
+            NodeList l2 = c.getElementsByTagName("Creature");
+            int n2 = l2.getLength();
+
+            for (int i2 = 0; i2 < n2; i2++) {
+                Element C = (Element) l2.item(i2);
+                if (C.getAttribute("name").equals(creature)) {
+                    //System.err.println("Taking one " + C.getAttribute("name"));
+                    String R = C.getAttribute("remaining");
+                    int r = Integer.parseInt(R);
+                    C.setAttribute("remaining", "" + (r - 1));
+                } else {
+                    //System.err.println("Ignoring " + C.getAttribute("name"));
+                }
+            }
+        }
+        /* put in Player's Legion */
+        l = root.getElementsByTagName("Player");
+        n = l.getLength();
+        for (int i = 0; i < n; i++) {
+            Element c = (Element) l.item(i);
+
+            NodeList l2 = c.getElementsByTagName("Legion");
+            int n2 = l2.getLength();
+
+            for (int i2 = 0; i2 < n2; i2++) {
+                Element C = (Element) l2.item(i2);
+                if (C.getAttribute("name").equals(legion)) {
+                    Element nc = doc.createElement("Creature");
+                    nc.setAttribute("name", creature);
+                    C.appendChild(nc);
+                } else {
+                }
+            }
+        }
+        /* put in History */
+        l = root.getElementsByTagName("History");
+        n = l.getLength();
+        for (int i = 0; i < n; i++) {
+            Element c = (Element) l.item(i);
+
+            NodeList l2 = c.getElementsByTagName("Reveal");
+            int n2 = l2.getLength();
+
+            for (int i2 = 0; i2 < n2; i2++) {
+                Element C = (Element) l2.item(i2);
+                if (C.getAttribute("markerId").equals(legion)) {
+                    Element nc = doc.createElement("creature");
+                    Element C2 = (Element) C.getElementsByTagName("creatures").item(0);
+                    C2.appendChild(nc);
+                    nc.setTextContent(creature);
+                } else {
+                }
+            }
+        }
+    }
+
+    static void addCreaturesToLegions(Document doc, String al, String[] aca, String dl, String[] dca) {
+
+        for (int i = 0; i < aca.length; i++) {
+            addCreature(doc, al, aca[i]);
+        }
+        for (int i = 0; i < dca.length; i++) {
+            addCreature(doc, dl, dca[i]);
+        }
+    }
+
+    static void replaceAIs(Document doc, String ap, String aAI, String dp, String dAI) {
+        Element root = doc.getDocumentElement();
+        NodeList l;
+        int n;
+
+        /* take from caretaker */
+        l = root.getElementsByTagName("Player");
+        n = l.getLength();
+        for (int i = 0; i < n; i++) {
+            Element c = (Element) l.item(i);
+            if (c.getAttribute("name").equals(ap)) {
+                c.setAttribute("type", "net.sf.colossus.ai." + aAI);
+            }
+            if (c.getAttribute("name").equals(dp)) {
+                c.setAttribute("type", "net.sf.colossus.ai." + dAI);
+            }
+        }
+    }
+
+    public static void main(String[] arg) {
+        Document doc = null;
+        String[] aca = null;
+        String[] dca = null;
+        String aAI = "SimpleAI";
+        String dAI = "SimpleAI";
+
+        for (int i = 0; i < arg.length; i++) {
+
+            if (arg[i].startsWith("--alist=")) {
+                aca = arg[i].substring(8).split(",");
+            } else if (arg[i].startsWith("--dlist=")) {
+                dca = arg[i].substring(8).split(",");
+            } else if (arg[i].startsWith("--aAI=")) {
+                aAI = arg[i].substring(6);
+            } else if (arg[i].startsWith("--dAI=")) {
+                dAI = arg[i].substring(6);
+            } else {
+                System.err.println("usage:\n\t <whatever_to_launch>" +
+                        "--alist=<comma separated list of attacking creature> " +
+                        "--dlist=<comma separated list of defending creature> " +
+                        "--aAI=<attacking AI name> " +
+                        "--dAI=<defending AI name> ");
+                System.exit(-1);
+            }
+        }
+
+        try {
+            javax.xml.parsers.DocumentBuilder db =
+                    javax.xml.parsers.DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+            doc = db.parse("MakeBattle.xml");
+        } catch (java.io.FileNotFoundException fnfe) {
+            System.err.println("Open Failed (FileNotFoundException): " + fnfe);
+        } catch (java.io.IOException ie) {
+            System.err.println("Open failed (IOException): " + ie);
+        } catch (javax.xml.parsers.ParserConfigurationException pce) {
+            System.err.println("Open failed (ParserConfigurationException): " + pce);
+        } catch (org.xml.sax.SAXException se) {
+            System.err.println("Open failed (SAXException): " + se);
+        }
+
+        addCreaturesToLegions(doc, "Rd01", aca, "Br01", dca);
+        replaceAIs(doc, "Red1", aAI, "Brown", dAI);
+
+
+        DOMSource domSource = new DOMSource(doc);
+        StreamResult streamResult = new StreamResult(System.out);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        try {
+            Transformer serializer = tf.newTransformer();
+            //        serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
+            //        serializer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,"users.dtd");
+            serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+            serializer.transform(domSource, streamResult);
+        } catch (javax.xml.transform.TransformerConfigurationException tce) {
+            System.err.println("Write failed (TransformerConfigurationException): " + tce);
+        } catch (javax.xml.transform.TransformerException te) {
+            System.err.println("Write failed (TransformerException): " + te);
+        }
+    }
+}
---
+++ Colossus/datatools/src/main/java/net/sf/colossus/tools/ShowBuilderHexMap.java
@@ -0,0 +1,611 @@
+package net.sf.colossus.tools;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.print.Book;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import javax.swing.*;
+
+import net.sf.colossus.variant.BattleHex;
+import net.sf.colossus.client.GUIBattleHex;
+import net.sf.colossus.variant.HazardTerrain;
+import net.sf.colossus.xmlparser.BattlelandLoader;
+import net.sf.colossus.parser.BattlelandRandomizerLoader;
+
+/**
+ * Class ShowBuilderHexMap displays a battle map.
+ * @version $Id$
+ * @author David Ripton
+ * @author Romain Dolbeau
+ */
+final class ShowBuilderHexMap extends BuilderHexMap implements WindowListener,
+        MouseListener, Printable
+{
+
+    private JDialog dialog;
+    private JPopupMenu popupMenuTerrain;
+    private JPopupMenu popupMenuBorder;
+    private Point lastPoint;
+    private Component lastComponent;
+    private int lastSide;
+    private JCheckBoxMenuItem towerItem;
+    private AbstractAction towerAction;
+    private AbstractAction clearStartListAction;
+
+    class rndFileFilter extends javax.swing.filechooser.FileFilter
+    {
+
+        public boolean accept(java.io.File f)
+        {
+            if (f.isDirectory())
+            {
+                return (true);
+            }
+            if (f.getName().endsWith(".rnd"))
+            {
+                return (true);
+            }
+            return (false);
+        }
+
+        public String getDescription()
+        {
+            return ("Colossus RaNDom generator files");
+        }
+    }
+
+    class xmlFileFilter extends javax.swing.filechooser.FileFilter
+    {
+
+        public boolean accept(java.io.File f)
+        {
+            if (f.isDirectory())
+            {
+                return (true);
+            }
+            if (f.getName().endsWith(".xml"))
+            {
+                return (true);
+            }
+            return (false);
+        }
+
+        public String getDescription()
+        {
+            return ("XML files");
+        }
+    }
+
+    
+    private void doLoadRandom(BattleHex[][] h)
+    {
+        javax.swing.JFileChooser loadFileChooser = new JFileChooser(".");
+        loadFileChooser.setFileFilter(new rndFileFilter());
+        loadFileChooser.setDialogTitle(
+                "Choose the RaNDom file to open (or cancel for nothing)");
+        int returnVal = loadFileChooser.showOpenDialog(loadFileChooser);
+        if (returnVal == javax.swing.JFileChooser.APPROVE_OPTION)
+        {
+            java.io.File rndFile = loadFileChooser.getSelectedFile();
+            String tempRndName = rndFile.getName();
+            String tempRndDirectory = rndFile.getParentFile().getAbsolutePath();
+            List directories = new java.util.ArrayList();
+            directories.add(tempRndDirectory);
+            java.io.InputStream inputFile =
+                    net.sf.colossus.util.ResourceLoader.getInputStream(
+                    tempRndName,
+                    directories);
+            if (inputFile != null)
+            {
+                BattlelandRandomizerLoader parser =
+                        new BattlelandRandomizerLoader( inputFile);
+                try
+                {
+                    while (parser.oneArea(h) >= 0)
+                    {
+                    }
+                    parser.resolveAllHexsides(h);
+                    towerItem.setState(parser.isTower());
+                    List startList = parser.getStartList();
+                    if (startList != null)
+                    {
+                        selectHexesByLabels(new java.util.HashSet(startList));
+                    }
+                } catch (Exception e)
+                {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+     
+    private void doLoadFile(BattleHex[][] h)
+    {
+        JFileChooser loadFileChooser = new JFileChooser();
+        loadFileChooser.setFileFilter(new xmlFileFilter());
+        loadFileChooser.setDialogTitle(
+                "Choose the battleland file to open (or cancel for nothing)");
+        int returnVal = loadFileChooser.showOpenDialog(loadFileChooser);
+        if (returnVal == javax.swing.JFileChooser.APPROVE_OPTION)
+        {
+            File loadFileFile = loadFileChooser.getSelectedFile();
+            String temploadFileName = loadFileFile.getName();
+            String temploadFileDirectory = loadFileFile.getParentFile().getAbsolutePath();
+            List<String> directories = new ArrayList<String>();
+            directories.add(temploadFileDirectory);
+            InputStream inputFile =
+                    net.sf.colossus.util.ResourceLoader.getInputStream(
+                    temploadFileName,
+                    directories);
+            if (inputFile != null)
+            {
+                BattlelandLoader parser =
+                        new BattlelandLoader(inputFile, h);
+                try
+                {
+                    towerItem.setState(parser.isTower());
+                    isTower = parser.isTower();
+                    List<String> startList = parser.getStartList();
+                    if (startList != null)
+                    {
+                        selectHexesByLabels(new HashSet<String>(startList));
+                    }
+                    subtitle = parser.getSubtitle();
+                    displayName = temploadFileName.replaceAll(".xml", "");
+                    basicName = temploadFileName.replaceAll(".xml", "");
+                } catch (Exception e)
+                {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+
+    private void doSaveFile()
+    {
+        javax.swing.JFileChooser saveFileChooser = new JFileChooser();
+        saveFileChooser.setDialogTitle(
+                "Choose the battleland file to save (or cancel for nothing)");
+        int returnVal = saveFileChooser.showSaveDialog(saveFileChooser);
+        if (returnVal == javax.swing.JFileChooser.APPROVE_OPTION)
+        {
+            java.io.File saveFileFile = saveFileChooser.getSelectedFile();
+            String tempsaveFileName = saveFileFile.getName();
+            String tempsaveFileDirectory = saveFileFile.getParentFile().getAbsolutePath();
+            List<String> directories = new ArrayList<String>();
+            directories.add(tempsaveFileDirectory);
+            OutputStream outputFile =
+                    net.sf.colossus.util.ResourceLoader.getOutputStream(
+                    tempsaveFileName,
+                    directories);
+            if (outputFile != null)
+            {
+                String outStr = dumpAsString();
+                try
+                {
+                    outputFile.write(outStr.getBytes());
+                    outputFile.flush();
+                    outputFile.close();
+                } catch (Exception e)
+                {
+                    System.err.println(e);
+                }
+            }
+        }
+    }
+
+    private void doPrintBattleland()
+    {
+        PrinterJob printerJob = PrinterJob.getPrinterJob();
+        Book book = new Book();
+        book.append(this, new PageFormat());
+        printerJob.setPageable(book);
+        boolean doPrint = printerJob.printDialog();
+        if (doPrint)
+        {
+            try
+            {
+                printerJob.print();
+            } catch (PrinterException exception)
+            {
+                System.err.println("Printing error: " + exception);
+            }
+        }
+    }
+
+    /* Printable */
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
+    {
+        if (pageIndex >= 1)
+        {
+            return Printable.NO_SUCH_PAGE;
+        }
+
+        Graphics2D g2 = (Graphics2D) graphics;
+        g2.scale(.5, .5);
+
+        g2.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+        paint(g2);
+
+        return Printable.PAGE_EXISTS;
+    }
+
+    private void doFillSlope(BattleHex[][] h)
+    {
+        for (int i = 0; i < h.length; i++)
+        {
+            for (int j = 0; j < h[i].length; j++)
+            {
+                if (h[i][j] != null)
+                {
+                    for (int k = 0; k < 6; k++)
+                    {
+                        if ((h[i][j].getHexside(k) == ' ') &&
+                                (h[i][j].getOppositeHexside(k) == ' '))
+                        {
+                            BattleHex n = h[i][j].getNeighbor(k);
+                            if (n != null)
+                            {
+                                if (h[i][j].getElevation() >
+                                        n.getElevation())
+                                {
+                                    h[i][j].setHexside(k, 's');
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class TerrainAction extends AbstractAction
+    {
+
+        HazardTerrain te;
+
+        TerrainAction(String t, HazardTerrain te)
+        {
+            super(t);
+            this.te = te;
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            GUIBattleHex h = getHexContainingPoint(lastPoint);
+            h.getHexModel().setTerrain(te);
+            h.repaint();
+        }
+    }
+
+    class ElevationAction extends AbstractAction
+    {
+
+        int el;
+
+        ElevationAction(String t, int el)
+        {
+            super(t);
+            this.el = el;
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            GUIBattleHex h = getHexContainingPoint(lastPoint);
+            h.getHexModel().setElevation(el);
+            h.repaint();
+        }
+    }
+
+    class HexsideAction extends AbstractAction
+    {
+
+        char c;
+
+        HexsideAction(String t, char c)
+        {
+            super(t);
+            this.c = c;
+        }
+
+        public void actionPerformed(ActionEvent e)
+        {
+            GUIBattleHex h = getHexContainingPoint(lastPoint);
+            h.getHexModel().setHexside(lastSide, c);
+            h.repaint();
+            ((GUIBattleHex) h.getNeighbor(lastSide)).repaint();
+        }
+    }
+    private AbstractAction showBattlelandAction;
+    private AbstractAction saveBattlelandAsAction;
+    private AbstractAction printBattlelandAction;
+    private AbstractAction quitAction;
+    private AbstractAction eraseAction;
+    private AbstractAction randomizeAction;
+    private AbstractAction fillWithSlopeAction;
+    private AbstractAction loadFileAction;
+    JMenuBar menuBar;
+
+    ShowBuilderHexMap()
+    {
+        super();
+
+        menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+        JMenuItem mi;
+
+        showBattlelandAction = new AbstractAction("Show Battleland")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.print(dumpAsString());
+            }
+        };
+
+        saveBattlelandAsAction = new AbstractAction("Save Battleland As...")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                doSaveFile();
+            }
+        };
+
+        printBattlelandAction = new AbstractAction("Print Battleland...")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                doPrintBattleland();
+            }
+        };
+
+        quitAction = new AbstractAction("Quit")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        };
+
+        eraseAction = new AbstractAction("Erase Map")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                eraseMap();
+                repaint();
+            }
+        };
+        /*
+        randomizeAction = new AbstractAction("Randomize Map (from file)")
+        {
+        public void actionPerformed(ActionEvent e)
+        {
+        doLoadRandom(getBattleHexArray());
+        repaint();
+        }
+        };
+         */
+        fillWithSlopeAction = new AbstractAction("Fill Edge With Slope")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                doFillSlope(getBattleHexArray());
+                repaint();
+            }
+        };
+
+        loadFileAction = new AbstractAction("Load Map (from file)")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                doLoadFile(getBattleHexArray());
+                repaint();
+            }
+        };
+
+        mi = fileMenu.add(loadFileAction);
+        mi.setMnemonic(KeyEvent.VK_O);
+        mi = fileMenu.add(saveBattlelandAsAction);
+        mi.setMnemonic(KeyEvent.VK_S);
+        mi = fileMenu.add(printBattlelandAction);
+        mi = fileMenu.add(showBattlelandAction);
+        mi = fileMenu.add(eraseAction);
+        //mi = fileMenu.add(randomizeAction);
+        mi = fileMenu.add(fillWithSlopeAction);
+        mi = fileMenu.add(quitAction);
+        mi.setMnemonic(KeyEvent.VK_Q);
+
+        JMenu specialMenu = new JMenu("Special");
+        menuBar.add(specialMenu);
+
+        towerAction = new AbstractAction("Terrain is a Tower")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                JCheckBoxMenuItem mi = (JCheckBoxMenuItem) e.getSource();
+                isTower = !isTower;
+                mi.setState(isTower);
+            }
+        };
+        towerItem = (JCheckBoxMenuItem) specialMenu.add(new JCheckBoxMenuItem(
+                towerAction));
+        clearStartListAction = new AbstractAction("Remove StartList")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                for (int i = 0; i < h.length; i++)
+                {
+                    for (int j = 0; j < h[0].length; j++)
+                    {
+                        if ((h[i][j] != null) &&
+                                (h[i][j].isSelected()))
+                        {
+                            h[i][j].unselect();
+                            h[i][j].repaint();
+                        }
+                    }
+                }
+            }
+        };
+        mi = specialMenu.add(clearStartListAction);
+
+        dialog = new JDialog();
+
+        Container contentPane = dialog.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add(menuBar, BorderLayout.NORTH);
+
+        addMouseListener(this);
+        dialog.addWindowListener(this);
+
+        contentPane.add(this, BorderLayout.CENTER);
+        dialog.pack();
+        dialog.setVisible(true);
+
+        popupMenuTerrain = new JPopupMenu("Choose Terrain");
+        contentPane.add(popupMenuTerrain);
+        Collection<HazardTerrain> terrains =
+                HazardTerrain.getAllHazardTerrains();
+        GUIBattleHex tempH = new GUIBattleHex(0, 0, 1, this, 0, 0);
+
+        for (HazardTerrain terrain : terrains)
+        {
+            tempH.getHexModel().setTerrain(terrain);
+            mi = popupMenuTerrain.add(new TerrainAction(terrain.getName(),
+                    terrain));
+        }
+        popupMenuTerrain.addSeparator();
+        for (int i = 0; i < 4; i++)
+        {
+            mi = popupMenuTerrain.add(new ElevationAction(
+                    "Set Elevation to: " + i,
+                    i));
+        }
+        popupMenuTerrain.addSeparator();
+        AbstractAction select = new AbstractAction("Select/Unselect (StartList)")
+        {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                GUIBattleHex h = getHexContainingPoint(lastPoint);
+                if (h.isSelected())
+                {
+                    h.unselect();
+                }
+                else
+                {
+                    h.select();
+                }
+                h.repaint();
+            }
+        };
+        mi = popupMenuTerrain.add(select);
+
+        popupMenuBorder = new JPopupMenu("Choose Border");
+        contentPane.add(popupMenuBorder);
+        char[] hexsides = BattleHex.getHexsides();
+
+        for (int i = 0; i < hexsides.length; i++)
+        {
+            tempH.getHexModel().setHexside(0, hexsides[i]);
+            mi = popupMenuBorder.add(new HexsideAction(tempH.getHexModel().
+                    getHexsideName(0),
+                    hexsides[i]));
+        }
+
+        lastPoint = new Point(0, 0);
+        lastComponent = contentPane;
+        lastSide = 0;
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        lastPoint = e.getPoint();
+        lastComponent = e.getComponent();
+        GUIBattleHex h = getHexContainingPoint(lastPoint);
+        if (h != null)
+        {
+            Point c = h.findCenter();
+            if (c.y >= lastPoint.y)
+            { // uppper half
+                if (lastPoint.x >=
+                        ((c.x) + (h.getBounds().x + h.getBounds().width)) / 2)
+                {
+                    lastSide = 1;
+                }
+                else if (lastPoint.x <= ((c.x) + (h.getBounds().x)) / 2)
+                {
+                    lastSide = 5;
+                }
+                else
+                {
+                    lastSide = 0;
+                }
+            }
+            else
+            { // lower half
+                if (lastPoint.x >=
+                        ((c.x) + (h.getBounds().x + h.getBounds().width)) / 2)
+                {
+                    lastSide = 2;
+                }
+                else if (lastPoint.x <= ((c.x) + (h.getBounds().x)) / 2)
+                {
+                    lastSide = 4;
+                }
+                else
+                {
+                    lastSide = 3;
+                }
+            }
+            if (h.innerContains(lastPoint) ||
+                    (h.getNeighbor(lastSide) == null))
+            { // change content
+                popupMenuTerrain.show(e.getComponent(),
+                        lastPoint.x,
+                        lastPoint.y);
+            }
+            else
+            { // change border
+                popupMenuBorder.show(e.getComponent(),
+                        lastPoint.x,
+                        lastPoint.y);
+            }
+        }
+    }
+
+    public void windowClosing(WindowEvent e)
+    {
+        System.exit(0);
+    }
+}
