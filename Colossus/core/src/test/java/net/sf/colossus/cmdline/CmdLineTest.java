e1547b987e683e29210104a01e98fd00a42028b6
David Ripton <dripton@ripton.net>
2009-03-04 01:31:50 +0000

    Add unit tests to cover the rest of the cmdline API.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@3683 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/test/java/net/sf/colossus/cmdline/CmdLineTest.java
+++ Colossus/core/src/test/java/net/sf/colossus/cmdline/CmdLineTest.java
@@ -6,7 +6,7 @@ import net.sf.colossus.cmdline.Opt;
 import net.sf.colossus.cmdline.Opts;
 
 
-/** 
+/**
  *  JUnit test for cmdline package.
  *  @version $Id$
  *  @author David Ripton
@@ -25,16 +25,41 @@ public class CmdLineTest extends TestCase
         Opts opts = new Opts();
         opts.addOption('v', "verbose", false, "log more stuff");
         assertTrue(opts.getOptions().size() == 1);
+
         Opt opt = opts.getOpt('v');
         assertEquals(opt.getOptch(), 'v');
         assertEquals(opt.getName(), "verbose");
         assertEquals(opt.hasArg(), false);
         assertEquals(opt.getHelp(), "log more stuff");
+        assertEquals(opt.toString(), "-v, --verbose, log more stuff");
+
         opt = opts.getOpt("verbose");
         assertEquals(opt.getOptch(), 'v');
         assertEquals(opt.getName(), "verbose");
         assertEquals(opt.hasArg(), false);
         assertEquals(opt.getHelp(), "log more stuff");
+        assertEquals(opt.toString(), "-v, --verbose, log more stuff");
+    }
+
+    public void testAddOptionWithArg()
+    {
+        Opts opts = new Opts();
+        opts.addOption('m', "myname", true, "My player name");
+        assertTrue(opts.getOptions().size() == 1);
+
+        Opt opt = opts.getOpt('m');
+        assertEquals(opt.getOptch(), 'm');
+        assertEquals(opt.getName(), "myname");
+        assertEquals(opt.hasArg(), true);
+        assertEquals(opt.getHelp(), "My player name");
+        assertEquals(opt.toString(), "-m, --myname, + ARG, My player name");
+
+        opt = opts.getOpt("myname");
+        assertEquals(opt.getOptch(), 'm');
+        assertEquals(opt.getName(), "myname");
+        assertEquals(opt.hasArg(), true);
+        assertEquals(opt.getHelp(), "My player name");
+        assertEquals(opt.toString(), "-m, --myname, + ARG, My player name");
     }
 
     public void testParse()
@@ -68,5 +93,23 @@ public class CmdLineTest extends TestCase
         assertTrue(cl.optIsSet("verbose"));
         assertEquals(cl.getOptValue('p'), "1234");
         assertEquals(cl.getOptValue("port"), "1234");
+    }
+
+    public void testLeftovers()
+    {
+        Opts opts = new Opts();
+        opts.addOption('v', "verbose", false, "log more stuff");
+        opts.addOption('p', "port", true, "server port number");
+        String [] args = {"-p", "1234", "-v", "hi", "mom"};
+        CmdLine cl = opts.parse(args);
+        assertTrue(cl.optIsSet('p'));
+        assertTrue(cl.optIsSet("port"));
+        assertTrue(cl.optIsSet('v'));
+        assertTrue(cl.optIsSet("verbose"));
+        assertEquals(cl.getOptValue('p'), "1234");
+        assertEquals(cl.getOptValue("port"), "1234");
+        assertEquals(cl.getLeftovers().size(), 2);
+        assertEquals(cl.getLeftovers().get(0), "hi");
+        assertEquals(cl.getLeftovers().get(1), "mom");
     }
 }
