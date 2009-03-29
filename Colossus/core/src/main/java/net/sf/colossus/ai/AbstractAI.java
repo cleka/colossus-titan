5ac22cc5519985155bd2fd1074ef6bdc10b30c3b
Romain Dolbeau <romain@dolbeau.org>
2009-03-29 12:54:43 +0000

    Javadoc
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@3885 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/ai/AbstractAI.java
+++ Colossus/core/src/main/java/net/sf/colossus/ai/AbstractAI.java
@@ -842,12 +842,22 @@ abstract public class AbstractAI implements AI
         }
     }
 
+    /** An integer value, along with a detailed record of how and why
+     * the value has the value it has.
+     * @author Romain Dolbeau
+     */
     protected class ValueRecorder
     {
 
+        /** The current value */
         private int value = 0;
+        /** All the explanations and value changes */
         private final StringBuffer why = new StringBuffer();
 
+        /** Augment the value.
+         * @param v By how much the value change.
+         * @param r The reason of the change.
+         */
         public void add(int v, String r)
         {
             if (why.toString().equals("") || v < 0)
@@ -862,6 +872,10 @@ abstract public class AbstractAI implements AI
             value += v;
         }
 
+        /** Reset the value to a specific value.
+         * @param v The new value to use.
+         * @param r The reason of the change.
+         */
         public void resetTo(int v, String r)
         {
             why.append(" | " + v);
@@ -869,11 +883,17 @@ abstract public class AbstractAI implements AI
             value = v;
         }
 
+        /** Get the value.
+         * @return The current value.
+         */
         public int getValue()
         {
             return value;
         }
 
+        /** Get the detailed explanations and final value as String.
+         * @return The detailed explanations and final value.
+         */
         @Override
         public String toString()
         {
