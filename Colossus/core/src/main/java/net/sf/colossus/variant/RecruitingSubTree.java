d798f391605f09ed9e2012b7765c4565a61701e8
Romain Dolbeau <romain@dolbeau.org>
2009-04-02 14:27:26 +0000

    Starting on TRL replacement. Instead of the big, String-based, graph-using convoluted logic that anwser a lot of questions nobody ever asked (see RecruitGraph and the number of unused functions... very convenient they are for AIs, except AIs don't use them), simply use as many object as needed. The only thing this version lack is CustomRecruitBase support, because the semantic is so ill-defined I'm not sure whether it applies to recruiters or recruits (keep in mind I wrote that code... years ago).
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@3962 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/core/src/main/java/net/sf/colossus/variant/RecruitingSubTree.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sf.colossus.variant;
+
+import java.util.HashMap;
+import java.util.Map;
+import net.sf.colossus.common.Constants;
+
+/**
+ * The recruiting sub-tree in a terrain (or several terrains)
+ * @author Romain Dolbeau
+ */
+public class RecruitingSubTree
+{
+
+    private class RecruiterAndRecruit
+    {
+
+        final private CreatureType recruiter;
+        final private CreatureType recruit;
+
+        RecruiterAndRecruit(CreatureType recruiter, CreatureType recruit)
+        {
+            this.recruiter = recruiter;
+            this.recruit = recruit;
+        }
+
+        public boolean equals(RecruiterAndRecruit rar)
+        {
+            return this.recruiter.equals(rar.recruiter) &&
+                    this.recruit.equals(rar.recruit);
+        }
+    }
+    private final Map<RecruiterAndRecruit, Integer> regular =
+            new HashMap<RecruiterAndRecruit, Integer>();
+    private final Map<CreatureType, Integer> any =
+            new HashMap<CreatureType, Integer>();
+    private final Map<CreatureType, Integer> anyNonLord =
+            new HashMap<CreatureType, Integer>();
+    private final Map<CreatureType, Integer> anyLord =
+            new HashMap<CreatureType, Integer>();
+    private final Map<CreatureType, Integer> anyDemiLord =
+            new HashMap<CreatureType, Integer>();
+
+    public void addRegular(CreatureType recruiter, CreatureType recruit,
+            int number)
+    {
+        regular.put(new RecruiterAndRecruit(recruiter, recruit), new Integer(
+                number));
+    }
+
+    public void addAny(CreatureType recruit, int number)
+    {
+        any.put(recruit, number);
+    }
+
+    public void addNonLord(CreatureType recruit, int number)
+    {
+        anyNonLord.put(recruit, number);
+    }
+
+    public void addLord(CreatureType recruit, int number)
+    {
+        anyLord.put(recruit, number);
+    }
+
+    public void addDemiLord(CreatureType recruit, int number)
+    {
+        anyDemiLord.put(recruit, number);
+    }
+
+    public int numberOfRecruiterNeeded(CreatureType recruiter,
+            CreatureType recruit, MasterBoardTerrain terrain, MasterHex hex)
+    {
+        int number = Constants.BIGNUM;
+        RecruiterAndRecruit rar = new RecruiterAndRecruit(recruiter, recruit);
+        if (regular.keySet().contains(rar))
+        {
+            number = Math.min(number, regular.get(rar).intValue());
+        }
+        if (any.keySet().contains(recruit))
+        {
+            number = Math.min(number, any.get(recruit).intValue());
+        }
+        if (!recruiter.isLord() && !recruiter.isDemiLord())
+        {
+            if (anyNonLord.keySet().contains(recruit))
+            {
+                number = Math.min(number, anyNonLord.get(recruit).intValue());
+            }
+        }
+        if (recruiter.isLord())
+        {
+            if (anyLord.keySet().contains(recruit))
+            {
+                number = Math.min(number, anyLord.get(recruit).intValue());
+            }
+        }
+        if (recruiter.isDemiLord())
+        {
+            if (anyDemiLord.keySet().contains(recruit))
+            {
+                number = Math.min(number, anyDemiLord.get(recruit).intValue());
+            }
+        }
+        return number;
+    }
+}
