97007ad7f915003b7aa8b55c8eef3609ce8b9574
Peter Becker <peter@peterbecker.de>
2009-05-06 09:53:49 +0000

    Reduced visibility of loop variable.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4173 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/gui/EventViewer.java
+++ Colossus/core/src/main/java/net/sf/colossus/gui/EventViewer.java
@@ -419,8 +419,7 @@ final class EventViewer extends KDialog
         int maxVal = this.expireTurns == -1 ? 1000 : this.expireTurns;
 
         List<String> alChoices = new ArrayList<String>();
-        int i;
-        for (i = 1; i <= maxVal; i++)
+        for (int i = 1; i <= maxVal; i++)
         {
             // 1, 2, 3, 4, 5,
             if (i <= 5 || i == maxVal)
