c87f2ed5e9c106d71d1d8a9be4d1d46db800718f
Clemens Katzer <github-cleka@cleka.net>
2009-05-22 10:20:42 +0000

    Additional manual beautification of source formatting (for the AI constants array ;-)
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4234 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/common/Constants.java
+++ Colossus/core/src/main/java/net/sf/colossus/common/Constants.java
@@ -139,8 +139,10 @@ public final class Constants
     public static final String anyAI = "A Random AI";
     public static final String defaultAI = anyAI;
     public static final String[] aiArray = { "SimpleAI", "CowardSimpleAI",
-        "RationalAI", "HumanHaterRationalAI", "MilvangAI", "ExperimentalAI" };
-    // , "ParallelEvaluatorAI"};
+        "RationalAI", "HumanHaterRationalAI", "MilvangAI", "ExperimentalAI"
+        // , "ParallelEvaluatorAI"
+    };
+
     // Auto play for human players uses SimpleAI:
     public static final String autoplayAI = aiArray[0];
 
