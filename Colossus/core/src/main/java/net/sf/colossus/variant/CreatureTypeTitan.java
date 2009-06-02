c6c99cfc4af1425d17df43be409335a46016f743
Clemens Katzer <github-cleka@cleka.net>
2009-06-02 19:15:28 +0000

    Using in javadoc link to PlayerServerSide the fully qualified name, so that variant does not need to import PlayerServerSide from server package (looks confusing if it imports something from server but in fact does not really depend on it.)
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4296 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/variant/CreatureTypeTitan.java
+++ Colossus/core/src/main/java/net/sf/colossus/variant/CreatureTypeTitan.java
@@ -6,22 +6,22 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.sf.colossus.common.Constants;
-import net.sf.colossus.server.PlayerServerSide;
 
 
 /**
  * Class CreatureTitan represent the CONSTANT information about a
  * Titan (the game) Titan (the creature).
- * 
+ *
  * Game related info is in Critter.  Counts of
  * recruited/available/dead are in Caretaker.
- * 
- * TODO this class doesn't really fulfill the whole {@link CreatureType} interface
- * since it can't handle getPower() at the moment (and thus some other things don't
- * work). The solution could be to consider each Titan a type of creature of his
- * own, distinguished by the Player owning them, which then could be stored as
- * member in the class, delegating {@link #getPower()} to {@link PlayerServerSide#getTitanPower()}.
- * 
+ *
+ * TODO this class doesn't really fulfill the whole {@link CreatureType}
+ * interface since it can't handle getPower() at the moment (and thus some
+ * other things don't work). The solution could be to consider each Titan a
+ * type of creature of his own, distinguished by the Player owning them, which
+ * then could be stored as member in the class, delegating {@link #getPower()}
+ * to {@link net.sf.colossus.server.PlayerServerSide#getTitanPower()}.
+ *
  * @version $Id$
  * @author Romain Dolbeau
  */
@@ -64,7 +64,7 @@ public class CreatureTypeTitan extends CreatureType
     @Override
     public int getPointValue()
     {
-        // Log.warn("Calling getPointValue() on Titan Creature"); 
+        // Log.warn("Calling getPointValue() on Titan Creature");
         // XXX This is wrong, but 24 is better than -4.
         int val = 6 * getSkill();
         return val;
