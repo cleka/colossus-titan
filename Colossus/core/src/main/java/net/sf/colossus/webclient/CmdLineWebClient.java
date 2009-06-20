4fe9b0483d2ff2305b1f47b8268a41c99a3bb18b
Clemens Katzer <github-cleka@cleka.net>
2009-06-20 19:26:56 +0000

    A first start to create a cmdline WebClient, for example for automated testing or for shutting down the server. Still "Work in Progress", just committing it to get my work copy clean...
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4388 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/core/src/main/java/net/sf/colossus/webclient/CmdLineWebClient.java
@@ -0,0 +1,33 @@
+package net.sf.colossus.webclient;
+
+import java.util.logging.Logger;
+
+
+/**
+ * A first start to create a cmdline WebClient. For example for automated
+ * testing or for shutting down the server.
+ *
+ * Still "Work in Progress", just committing it to get my work copy clean...
+ *
+ */
+public class CmdLineWebClient
+{
+    private static final Logger LOGGER = Logger
+        .getLogger(CmdLineWebClient.class.getName());
+
+
+    public CmdLineWebClient()
+    {
+        LOGGER.info("Cmdline WebClient instantiated.");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
