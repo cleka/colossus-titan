13de5df16ada137e69d5d4506746492301ae5636
Romain Dolbeau <romain@dolbeau.org>
2009-08-04 14:03:39 +0000

    Missing file from previous commit ; thank you Hudson. [Redo ValueRecorder with a list, so we can have a much prettier, indented output, including recursive ValueRecorder. Use ValueRecorder in the Objectives, so we can track them in the log. Allow to scale ValueRecorder so that we can store the priority in it. Also, use empty _Strike and _Rangestrike evaluatr in ExperimentalAI (objectives are supposed to deal wit those).]
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4475 c5679a2a-0e0e-0410-bba2-8642b794b8c1

---
+++ Colossus/core/src/main/java/net/sf/colossus/util/IValueRecorderItem.java
@@ -0,0 +1,24 @@
+/*
+ */
+
+package net.sf.colossus.util;
+
+/**
+ *
+ * @author dolbeau
+ */
+public interface IValueRecorderItem {
+
+    /**
+     * Get the value.
+     * @return The current value.
+     */
+    int getValue();
+
+    String getWhy(String prefix);
+
+    String getFull(String prefix);
+
+    boolean isReset();
+
+}
