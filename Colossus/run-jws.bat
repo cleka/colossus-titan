1bea57a55908d5479287847a6586a9b4566f3b1e
Barrie Treloar <baerrach@gmail.com>
2010-05-23 12:30:49 +0000

    Added cd to cwd via %~dp0 to all bat files so they can be run from any location
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4832 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/run-jws.bat
+++ Colossus/run-jws.bat
@@ -1,4 +1,5 @@
 rem Make sure javaws.exe is in your path.
 rem Change the codebase in win.jnlp to match your config.
 rem You need a *signed* Colossus.jar in this directory.
+cd "%~dp0"
 javaws win.jnlp
--- Colossus/runwebserver.bat
+++ Colossus/runwebserver.bat
@@ -1,1 +1,2 @@
+cd "%~dp0"
 java -Djava.util.logging.config.file=logging.properties -jar ColossusWeb.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
