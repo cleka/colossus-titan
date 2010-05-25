334cd6f0631ddc90011d1e69392ca7b0d6e5151b
Clemens Katzer <github-cleka@cleka.net>
2010-05-25 19:50:42 +0000

    Make the RemoteLogHandler not send anything to any remote clients for now, since it does not work as one would expect anyway.
    git-svn-id: svn://localhost/tmp/s2g/colossus.svn/trunk@4845 c5679a2a-0e0e-0410-bba2-8642b794b8c1

--- Colossus/core/src/main/java/net/sf/colossus/server/RemoteLogHandler.java
+++ Colossus/core/src/main/java/net/sf/colossus/server/RemoteLogHandler.java
@@ -6,10 +6,25 @@ import java.util.logging.LogRecord;
 
 
 /**
- * A java.util.logging Handler that appends to a <code>Server</code> via <code>allLog</code>.
+ * A java.util.logging Handler that appends to a <code>Server</code> via
+ * <code>allLog</code>.
+ *
+ * Clemens: this is totally odd. Above it says it appends to a server, but
+ * in practice it is used in a way that any thing logged on server is
+ * transmitted to all remote clients, and even in wrong way; see below...
  */
 public class RemoteLogHandler extends Handler
 {
+    // Right now taken out of use, because it does not work anyway
+    // 1) if there is 3 remote clients, each remote logger sends to all three
+    //    remote clients
+    // 2) serialization does not work (any more?) - remote client shows merely
+    //    LogRecord@123456 instead of something meaningful
+    // 3) When client connection gets lost, hassle with the isGone and logging
+    //    of related information (still tried to send to client).
+
+    private final static boolean PUBLISH_TO_REMOTE_LOGGERS = false;
+
     private Server server = null;
 
     public RemoteLogHandler(Server server)
@@ -33,9 +48,12 @@ public class RemoteLogHandler extends Handler
     @Override
     public void publish(LogRecord record)
     {
-        if (server != null)
+        if (PUBLISH_TO_REMOTE_LOGGERS)
         {
-            server.allLog(record.toString());
+            if (server != null)
+            {
+                server.allLog(record.toString());
+            }
         }
     }
 
