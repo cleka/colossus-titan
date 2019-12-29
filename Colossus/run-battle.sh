9d92bf30505658ee080841a8cf2faa38efd4461d
Clemens Katzer <clemens@cleka.net>
2019-12-29 18:02:22 +0200

    Made run-battle.sh a lot more flexible

--- Colossus/run-battle.sh
+++ Colossus/run-battle.sh
@@ -1,16 +1,37 @@
+#! /bin/sh
 
-# BATTLE_XML=./Battle-stuff/battle.xml
-BATTLE_XML=./Battle-stuff/battle-7v7.xml
-BATTLE_XML=attacked.xml
+[ -e ~/.colossus/jre.SH ] && source ~/.colossus/jre.SH
+
+BATTLE_XML=./Battle-stuff/battle.xml
+
+FORCE_BOARD=false
+END_AFTER_BATTLE=true
+LOG_PROPERTIES=logging.properties
 
 # -P: probability based battle hits
-pbbh="-P"
+PROB_HITS="-P"
 
-source /lib/jvm-private/jdk1.7.0_09.SH
+#
+# You can override any of the above by putting it to a file and then
+# use it e.g. by:
+#  ./run-battle.sh ./battle1.cf
+#
 
+if [ $# -gt 0 ]
+then
+    if [ -e $1 ]
+    then
+	echo "Sourcing battle settings '$1'..."
+	source $1
+	shift
+    fi
+fi
 
-java -Dnet.sf.colossus.forceViewBoard=true \
-     -Dnet.sf.colossus.endAfterFirstBattle=true \
-     -Djava.util.logging.config.file=logging.properties -Xmx256M -jar Colossus.jar $1 $2 $3 $4 $5 $6 $7 $8 $9 --load $BATTLE_XML $pbbh
+echo "Starting Colossus for $BATTLE_XML"
 
+java -Dnet.sf.colossus.forceViewBoard=$FORCE_BOARD           \
+     -Dnet.sf.colossus.endAfterFirstBattle=$END_AFTER_BATTLE \
+     -Djava.util.logging.config.file=$LOG_PROPERTIES         \
+     -Xmx256M -jar Colossus.jar                              \
+     --load $BATTLE_XML $PROB_HITS
 
