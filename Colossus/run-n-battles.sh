8bbb7f4ad60859fff0a57084effbe015a51f831c
Clemens Katzer <clemens@cleka.net>
2020-01-26 18:39:22 +0200

    run-n-battles.sh - pass through given arg to run-battle.sh

--- Colossus/run-n-battles.sh
+++ Colossus/run-n-battles.sh
@@ -1,21 +1,24 @@
 #! /bin/sh
 
 N=10
-[ $# -gt 0 ] && N=$1
+[ $# -gt 0 ] && N=$1 && shift
 
 touch Battle-stuff/basenr.txt
 basenr=`cat Battle-stuff/basenr.txt`
 [ -z "$basenr" ] && basenr=0
 
 i=1
-while [ $i -le $N ]
+while [ $i -le $N -a ! -e /tmp/stop.n.flag ]
 do
     nr=`expr $basenr + $i`
-    echo running battle nr $nr
-    ./run-battle.sh
+    echo
+    echo -e "running battle nr $nr - \c"
+    ./run-battle.sh $@
     mkdir -p out/$nr
     mv /tmp/Colossus*.log ./out/$nr/.
     i=`expr $i + 1`
 done
+
+rm -f /tmp/stop.n.flag
 
 echo $nr > Battle-stuff/basenr.txt
